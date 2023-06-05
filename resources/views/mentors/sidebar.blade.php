231f74b99d49cce53a7716ac151a3f1fd7dc75af
nihala-pp <nihala-pp@pransainvestment.com>
2023-06-05 12:09:42 +0400

    signout

--- resources/views/mentors/sidebar.blade.php
+++ resources/views/mentors/sidebar.blade.php
@@ -69,6 +69,14 @@
                     <span class="nav-link-text ms-1">My Availability</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="{{ route('mentor.dashboard.availability') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Sign Out</span>
+                </a>
+            </li>
             <!-- <li class="nav-item">
                     <a class="nav-link text-white " href="{{ route('admin.mentors.sign-in') }}">
                         <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
