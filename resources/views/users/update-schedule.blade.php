e20fa33067262069152619cdfc238478998c1965
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-16 01:47:53 +0400

    user profilw

--- resources/views/users/update-schedule.blade.php
+++ resources/views/users/update-schedule.blade.php
@@ -42,7 +42,7 @@
             <div class="container-fluid py-1 px-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">User</a></li>
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="{{ route('user.')}}">User</a></li>
                         <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
                     </ol>
                     <h6 class="font-weight-bolder mb-0">Dashboard</h6>
@@ -197,7 +197,7 @@
                                         <select class="form-control" id="educationDate" name="utc" required>
                                             <option value="">UTC</option>
                                                 @foreach($utc as $key => $ut)
-                                                   <option value="{{ $ut }}" {{ $ut == $scheduled_call->ut ? "selected" : "" }}>{{ $key }}</option>
+                                                   <option value="{{ $ut }}" {{ $ut == $scheduled_call->utc ? "selected" : "" }}>{{ $key }}</option>
                                                 @endforeach
                                         </select>
                                     </div>
