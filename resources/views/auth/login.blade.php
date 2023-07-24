1262cd794afcd47c4b34a4ed098f17c3398a6c80
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-24 19:18:35 +0400

    sdfdsf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,13 +33,13 @@
                                 <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
                                     <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">{{ __('Login') }}
                                     </h4>
-                                    <div class="row mt-3 social">
+                                    <div class="row mt-3">
                                         <!-- <div class="col-2 text-center ms-auto"> -->
                                         <!-- <a class="btn btn-link px-3" href="javascript:;">
                         <i class="fa fa-facebook text-white text-lg"></i>
                       </a> -->
                                         <!-- </div> -->
-                                        <div class="col-3 text-center ms-auto">
+                                        <div class="col-3 text-center ms-auto linkedin">
                                             <a class="btn btn-link px-2" href="{{ route('auth.linkedin') }}">
                                                 <img src="{{ asset('public/assets/img/linkedin.png') }}"
                                                     class="avatar avatar-sm me-3" alt="linkedin">
@@ -47,7 +47,7 @@
                                                 <!-- <i class="fa fa-linkedin text-dark-blue text-lg"></i> -->
                                             </a>
                                         </div>
-                                        <div class="col-3 text-center me-auto">
+                                        <div class="col-3 text-center me-auto google">
                                             <a class="btn btn-link px-2" href="{{ route('auth.google') }}">
                                                 <img src="{{ asset('public/assets/img/google.png') }}"
                                                     class="avatar avatar-sm me-3" alt="google">
@@ -141,7 +141,11 @@
     padding-left: 750px;
 }
 
-.social {
+.linkedin {
+    padding-right: 50px;
+}
+
+.google {
     padding-right: 50px;
 }
 </style>
