fdd127a7d2f12d2ec86ac6f35657e4b7238a2981
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-24 15:02:09 +0400

    sasdsadf

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
   <!-- Nucleo Icons -->
   <link href="{{ asset('public/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-  <link href="{{ asset('public/assets/css/material-dashboard.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('public/assets/css/material-dashboard.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ asset('public/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
   <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
