eac8890cd051215efaa330e72a8472b5b333b843
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-24 13:26:11 +0400

    sdsfsfzx

--- resources/views/mentors/edit-availability.blade.php
+++ resources/views/mentors/edit-availability.blade.php
@@ -17,12 +17,12 @@
                     <label class="form-label">Date</label>
                     <input type="text" name="date" class="form-control date" value="{{ $availability->date }}" required>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <label class="form-label">Start Time</label>
                     <input type="time" name="start_time" class="form-control" value="{{ $availability->start_time }}"
                         required>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <label class="form-label">End Time</label>
                     <input type="time" name="end_time" class="form-control" value="{{ $availability->end_time }}"
                         required>
