8fdb304bfbd1c8e0616aef4d889bdb00135240ae
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-24 17:15:53 +0400

    sdsdfds

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
   </div> -->
     <main class="main-content  mt-0">
         <div class="page-header align-items-start min-vh-100"
-            style="background-image: url('https://www.freepik.com/free-photo/businessman-logging-his-tablet_15474932.htm#page=3&query=login%20security&position=6&from_view=search&track=ais');">
+            style="background-image: url('https://img.freepik.com/free-photo/businessman-logging-his-tablet_53876-102029.jpg');">
             <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container my-auto">
                 <div class="row">
