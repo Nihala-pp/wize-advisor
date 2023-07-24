a3659e56ebe2c0cdf968e372574839975249fb11
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-24 11:00:32 +0400

    dsfsfd

--- resources/views/mentors/availability.blade.php
+++ resources/views/mentors/availability.blade.php
@@ -294,7 +294,7 @@
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title font-weight-normal" id="editAvailabilityLabel">Edit Availability</h5>
+                        <h5 class="modal-title font-weight-normal" id="editAvailabilityLabel"></h5>
                         <button type="button" class="btn-close text-dark" data-bs-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
