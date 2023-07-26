cd57818cc9d2effa9b410aa7789052d9db2bf327
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-26 15:54:52 +0400

    sds

--- resources/views/mentors/sessions.blade.php
+++ resources/views/mentors/sessions.blade.php
@@ -227,7 +227,7 @@
         <div class="container-fluid py-4">
             <div class="row">
                 <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
-                    <div class="card my-4">
+                <div class="card">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
                                 <h6 class="text-white text-capitalize ps-3">Requested Sessions</h6>
