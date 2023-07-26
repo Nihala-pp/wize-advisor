6e93db048ada665c5d020c85613a601bed660ddc
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-26 14:32:58 +0400

    navbar

--- resources/views/mentors/index.blade.php
+++ resources/views/mentors/index.blade.php
@@ -126,9 +126,9 @@
     </aside>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <!-- Navbar -->
-        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
             navbar-scroll="true">
-            <div class="container-fluid py-1 px-3 card">
+            <div class="container-fluid py-1 px-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                         <!-- <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark"
