5a0a460a39bdce8609faa1f8b885629852daf170
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 13:50:24 +0400

    sdsf

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -38,14 +38,6 @@
         <nav class="navbar navbar-main navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky" id="navbarBlur" data-scroll="true">
             <div class="container-fluid py-1 px-3">
             <a class="navbar-brand" href="#">Navbar</a>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <!-- <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark"
-                                href="javascript:;"></a></li>
-                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page"></li> -->
-                    </ol>
-                    <!-- <h5 class="font-weight-bolder mb-0">Dashboard</h5> -->
-                </nav>
                 <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                     <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                         <div class="input-group input-group-outline">
