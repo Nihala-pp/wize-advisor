47fff12e6421bd20c8688fc98476a6521bf444f4
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 13:35:48 +0400

    dsffd

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -49,7 +49,7 @@
     <!-- </aside> -->
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <!-- Navbar -->
-        <nav class="navbar navbar-main bg-gradient-light navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky"
+        <nav class="navbar navbar-main bg-gradient-white navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky"
             id="navbarBlur" data-scroll="true">
             <div class="container-fluid py-1 px-3">
                 <nav aria-label="breadcrumb">
