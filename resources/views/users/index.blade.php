909c85d9f897960b096316145a3b8a372b66c187
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 19:28:26 +0400

    dscsfds

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -37,7 +37,7 @@
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <!-- Navbar -->
         <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl navbar-blur mt-3 mb-3"
-            id="navbarBlur" navbar-scroll="true">
+            id="navbarBlur">
             <div class="container-fluid py-1 px-3">
                 <a class="navbar-brand" href="{{ route('user.dashboard') }}">
                     <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.png" alt="wiseAdvizor"
