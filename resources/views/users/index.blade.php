02d3f3dd62920b6d79f70fc279667882236556d0
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 13:34:06 +0400

    adsdfs

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -48,7 +48,26 @@
     </div>
     <!-- </aside> -->
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-        <div class="col-lg-8">
+        <!-- Navbar -->
+        <nav class="navbar navbar-main bg-gradient-light navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky"
+            id="navbarBlur" data-scroll="true">
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <!-- <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark"
+                                href="javascript:;"></a></li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page"></li> -->
+                    </ol>
+                    <!-- <h5 class="font-weight-bolder mb-0">Dashboard</h5> -->
+                </nav>
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                        <div class="input-group input-group-outline">
+                            <!-- <label class="form-label">Type here...</label>
+                            <input type="text" class="form-control"> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
             <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                 <li class="nav-item active">
                     <a href="{{ route('user.dashboard') }}" class="nav-link text-muted active" target="_blank">Dashboard
@@ -72,32 +91,12 @@
                         <li><a class="dropdown-item" href="{{ route('blogs') }}">BLOGS</a></li>
                     </ul>
                 </li>
+                <li class="nav-item">
+                    <a href="{{ route('browseMentor') }}" class="nav-link text-muted" target="_blank">Browse
+                        Mentor</a>
+                </li>
             </ul>
-            <form class="d-flex">
-                <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
-                <button class="btn btn-outline-success" type="submit" href="{{ route('browseMentor') }}">Browse
-                    Mentor</button>
-            </form>
         </div>
-        <!-- Navbar -->
-        <nav class="navbar navbar-main bg-gradient-light navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky"
-            id="navbarBlur" data-scroll="true">
-            <div class="container-fluid py-1 px-3">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <!-- <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark"
-                                href="javascript:;"></a></li>
-                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page"></li> -->
-                    </ol>
-                    <!-- <h5 class="font-weight-bolder mb-0">Dashboard</h5> -->
-                </nav>
-                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                        <div class="input-group input-group-outline">
-                            <!-- <label class="form-label">Type here...</label>
-                            <input type="text" class="form-control"> -->
-                        </div>
-                    </div>
                     <ul class="navbar-nav  justify-content-end">
                         <li class="nav-item d-flex align-items-center">
                             <a href="{{ route('user.profile') }}" class="nav-link text-body font-weight-bold px-0">
