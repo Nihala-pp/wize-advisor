8d3cb828affcad8214dd4f41e476035a258db088
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 18:22:20 +0400

    wdewfewf

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -198,7 +198,7 @@
         <!--====================================================
                            Upcoming Sessions
     ======================================================-->
-        <div class="page-header" style="background-image: url('{{ asset('public/assets/img/user-dashboard.png') }}')">
+        <div class="row" style="background-image: url('{{ asset('public/assets/img/user-dashboard.png') }}')">
             <div class="container-fluid py-4">
                 <div class="row">
                     <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
