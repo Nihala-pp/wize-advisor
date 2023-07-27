30f7f0e4212de3b34a062186638195dd048126d1
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 14:16:44 +0400

    fsdfdg

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -44,7 +44,7 @@
                         width="150;" height="75px;" style="object-fit:contain;">
                 </a>
                 <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-                    <div class="ms-md-auto pe-md-3 d-flex align-items-cente">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                         <div class="input-group input-group-outline">
                             <!-- <label class="form-label">Type here...</label> -->
                             <!-- <input type="text" class="form-control">  -->
