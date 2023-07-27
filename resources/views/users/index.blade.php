41acbbab4945d93893b5b1cd03f263234016f278
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-27 18:28:24 +0400

    eefe

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -201,7 +201,7 @@
         <div class="row" style="background-image: url('{{ asset('public/assets/img/user-dashboard.png') }}')">
             <div class="container-fluid py-4 req">
                 <div class="row">
-                    <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
+                    <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
                         <div class="card my-4">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                                 <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
@@ -289,7 +289,7 @@
     ======================================================-->
             <div class="container-fluid py-4 req">
                 <div class="row">
-                    <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
+                    <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
                         <div class="card my-4">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                                 <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
@@ -381,7 +381,7 @@
     ======================================================-->
             <div class="container-fluid py-4 req">
                 <div class="row">
-                    <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
+                    <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
                         <div class="card">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                                 <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
@@ -464,7 +464,7 @@
     ======================================================-->
             <div class="container-fluid py-4 req">
                 <div class="row">
-                    <div class="col-lg-12 col-md-6 mb-md-0 mb-4">
+                    <div class="col-lg-8 col-md-6 mb-md-0 mb-4">
                         <div class="card">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                                 <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
