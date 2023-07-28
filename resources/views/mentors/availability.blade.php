a7eecc68dcf9d0e46db7f7708506cd2628ae925c
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-28 12:04:08 +0400

    dfds

--- resources/views/mentors/availability.blade.php
+++ resources/views/mentors/availability.blade.php
@@ -243,7 +243,7 @@
                                         </select>
                                     </div>
                                     <div class="row">
-                                        <div class="col-md-4 mt-3">
+                                        <div class="col-md-4 mt-4">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="checkbox" value=""
                                                     id="fcustomCheck1">
