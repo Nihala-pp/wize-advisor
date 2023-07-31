ac4f1d1b25dd4acbeb88d7adec62f1a033a65aa9
nihala-pp <nihala-pp@pransainvestment.com>
2023-07-31 17:58:26 +0400

    frrgf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -251,6 +251,7 @@ window.location.href = "' + custom_location + " / " + Id + '";
         $mentor = $request->mentor;
         $timezone = $request->timezone ? $request->timezone : Auth::user()->metaData->timezone;
         $nmonth = date("m", strtotime($request->month));
+        dd($nmonth);
         $date = $request->year.'-'.$nmonth.'-'.$request->day;
         // dd($date);
         $availability = AvailableSchedule::where('mentor_id', $mentor)->where('date', $date)->get();
