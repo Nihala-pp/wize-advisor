dd6914209dd32fa04dce08d440c1cb5a95a7b56c
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-02 14:32:09 +0400

    cdscd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -206,8 +206,8 @@ window.location.href = "' + custom_location + " / " + Id + '";
       $end_time = Carbon::parse($data['time'])->addMinutes($data['duration']);
       $finish_time = $end_time->toTimeString();
 
-      $mentor_timezone = AvailableSchedule::where('mentor_id', $data['mentor'])->where('date', $date)->first();
-      dd($data['mentor'], $date);
+      $mentor_timezone = AvailableSchedule::where('mentor_id', $data['mentor'])->where('date', Carbon::parse($date)->format('Y-m-d'))->first();
+      dd($mentor_timezone);
 
       $user_timezone = new \DateTime($date . ' ' . $data['time'], new \DateTimeZone($data['timezone']));
     
