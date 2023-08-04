b11e2b53a77981df92cfa46b4b291c7d58b3ad3f
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-04 12:02:03 +0400

    sdcdv

--- resources/views/users/update-schedule.blade.php
+++ resources/views/users/update-schedule.blade.php
@@ -288,7 +288,7 @@
     </main>
     </div>
     <script type="text/javascript">
-      var enableDays = "{{ $dateAvailability }}";
+      var enableDays = <?php echo json_encode($dateAvailability); ?>
 
     function enableAllTheseDays(date) {
         var sdate = $.datepicker.formatDate('d-m-yy', date)
