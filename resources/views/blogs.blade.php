03e91ebdf045a107553779f2ebd84b46f5702272
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-21 17:45:56 +0400

    asadsd

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1739,9 +1739,9 @@
                             class="elementor-section elementor-inner-section elementor-element elementor-element-ef970f2 elementor-section-full_width elementor-section-height-default elementor-section-height-default"
                             data-id="ef970f2" data-element_type="section">
                             <div class="elementor-container elementor-column-gap-default">
+                                @foreach($blogs as $blog)
                                 <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-fd27b31"
                                     data-id="fd27b31" data-element_type="column">
-                                    @foreach($blogs as $blog)
                                     <div class="elementor-widget-wrap elementor-element-populated">
                                         <div class="elementor-element elementor-element-bff2ac9 elementor-widget elementor-widget-image"
                                             data-id="bff2ac9" data-element_type="widget"
@@ -1832,8 +1832,8 @@
                                             </div>
                                         </div>
                                     </div>
-                                    @endforeach
                                 </div>
+                                @endforeach
                             </div>
                         </section>
                     </div>
