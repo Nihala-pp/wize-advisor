b07637945e30c1a8695addbbd85fec7ed9fe823e
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-22 18:25:10 +0400

    wdwd

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -58,7 +58,7 @@
                                         @csrf
                                         <input type="hidden" name="token" value="{{ $token ?: 'Null' }}">
                                         <input type="hidden" name="mentor_id" value="{{ $id ?: 'Null' }}">
-                                        <div class="input-group input-group-static mb-0">
+                                        <div class="input-group input-group-static mb-3">
                                             <label class="">Name</label>
                                             <input id="name" type="text"
                                                 class="form-control @error('name') is-invalid @enderror" name="name"
