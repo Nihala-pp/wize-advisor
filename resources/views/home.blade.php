f8444d320c6703e99f8190b2b85e3af33665b878
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-22 16:41:11 +0400

    wedwed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2676,7 +2676,7 @@
                                                                             <div class="elementor-widget-container">
                                                                                 <p
                                                                                     class="elementor-heading-title elementor-size-default">
-                                                                                    {{ $mentor->metaData ? Str::words($mentor->metaData->bio, '18') : '' }}
+                                                                                    {{ $mentor->metaData ? Str::words($mentor->metaData->bio, '17') : '' }}
                                                                                 </p>
                                                                             </div>
                                                                         </div>
