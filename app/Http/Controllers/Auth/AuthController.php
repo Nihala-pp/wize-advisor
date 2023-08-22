a519789793cf638c5ac03b9de178bf45ad90311b
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-22 18:47:56 +0400

    sxsdcsd

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -115,7 +115,6 @@ class AuthController extends Controller
                     ->mixedCase()
                     ->numbers()
                     ->symbols()
-                    ->uncompromised()
             ],
         ]);
 
