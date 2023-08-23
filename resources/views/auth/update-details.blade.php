9a7b4666ce6663e47b8380facea53cf29bb5aa1a
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-23 11:56:57 +0400

    wewe

--- resources/views/auth/update-details.blade.php
+++ resources/views/auth/update-details.blade.php
@@ -14,7 +14,7 @@
     <section>
         <div class="row">
             <div class="col-md-4 mb-4">
-                <div class="card mb-4">
+                <div class="card mb-4 mx-auto">
                     <div class="card-header py-3">
                         <h5 class="mb-0">Personal Details</h5>
                     </div>
