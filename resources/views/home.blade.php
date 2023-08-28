83a10d0679959ad6bf4deb5d167b8bf283b3c745
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-28 15:55:05 +0400

    sdsf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -300,6 +300,39 @@
         --wp-admin--admin-bar--position-offset: var(--wp-admin--admin-bar--height, 0px)
     }
 
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+
+    }
+
+    .pe-7s-user {
+        margin-right: 20px;
+        align: center;
+    }
+
+    .elementor-button-wrapper .browse {
+        margin-left: 20px;
+    }
+
+    .menuITem {
+        margin-left: 30px;
+    }
+
+    .name {
+        font-size: 13px;
+        line-height: 0em;
+        margin-right: 20px;
+    }
+
+
+
+    .menu {
+        font-size: 12px;
+        text-align: center;
+    }
+
     @media screen and (max-width:600px) {
         html :where(.is-position-sticky) {
             --wp-admin--admin-bar--position-offset: 0px
