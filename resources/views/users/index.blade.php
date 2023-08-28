c5d40541c5761c2af1ee04a352bb1a9b2b79be76
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-28 18:10:24 +0400

    sadasd

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -60,9 +60,10 @@
                 <!-- Collapsible wrapper -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Navbar brand -->
-                    <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                        <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" height="15"
-                            alt="MDB Logo" loading="lazy" />
+                    <a class="navbar-brand mt-2 mt-lg-0" href="{{ route('user.dashboard') }}">
+                        <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.png" alt="wiseAdvizor"
+                            width="150;" height="75px;" style="object-fit:contain;" class="d-inline-block align-top"
+                            height="15" alt="MDB Logo" loading="lazy" />
                     </a>
                     <!-- Left links -->
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
