6e4b5255112685309fa36048d00d42e395675d77
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-29 11:25:53 +0400

    erfet

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1535,9 +1535,53 @@
                             <div class="elementor-widget-container">
                                 <div class="elementor-button-wrapper">
                                     <div class="container-fluid">
-                                        <ul class="navbar-nav">
+                                        <div class="d-flex align-items-center">
+                                            <div class="dropdown">
+                                                <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#"
+                                                    id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown"
+                                                    aria-expanded="false">
+                                                    <i class="fas fa-bell"></i>
+                                                    <span
+                                                        class="badge rounded-pill badge-notification bg-danger">1</span>
+                                                </a>
+                                                <ul class="dropdown-menu dropdown-menu-end"
+                                                    aria-labelledby="navbarDropdownMenuLink">
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">Some news</a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">Another news</a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">Something else here</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div class="dropdown">
+                                                <a class="dropdown-toggle d-flex align-items-center hidden-arrow"
+                                                    href="#" id="navbarDropdownMenuAvatar" role="button"
+                                                    data-mdb-toggle="dropdown" aria-expanded="false">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                                        class="rounded-circle" height="25"
+                                                        alt="Black and White Portrait of a Man" loading="lazy" />
+                                                </a>
+                                                <ul class="dropdown-menu dropdown-menu-end"
+                                                    aria-labelledby="navbarDropdownMenuAvatar">
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">My profile</a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">Settings</a>
+                                                    </li>
+                                                    <li>
+                                                        <a class="dropdown-item" href="#">Logout</a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- <ul class="navbar-nav"> -->
                                             <!-- Avatar -->
-                                            <li class="nav-item me-3 me-lg-0 dropdown">
+                                            <!-- <li class="nav-item me-3 me-lg-0 dropdown">
                                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
                                                     role="button" data-mdb-toggle="dropdown" aria-expanded="false">
                                                     <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
@@ -1559,8 +1603,8 @@
                                                         <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
                                                     </li>
                                                 </ul>
-                                            </li>
-                                        </ul>
+                                            </li> -->
+                                        <!-- </ul> -->
                                     </div>
                                 </div>
                             </div>
