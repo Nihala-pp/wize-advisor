60d23d5ce9ad45734dd278474e7ecbbcd98ed0ff
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-29 17:23:22 +0400

    edfsf

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
     <link rel="dns-prefetch" href="//fonts.googleapis.com">
     <link rel="alternate" type="applicatio/rss+xml" title="wiseAdvizor &raquo; Feed" href="{{ asset('public/feed/') }}">
-    <link rel="alternate" type="application/rss+xml" title="wiseAdvizor &raquo; Comments Feed" href="{{ asset('public/comments/feed/) }}">
+    <link rel="alternate" type="application/rss+xml" title="wiseAdvizor &raquo; Comments Feed" href="{{ asset('public/comments/feed/') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('public/assets/css/style.css') }}">
