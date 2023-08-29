3d1bea929bbde2314b48a586bbb7413a485236b5
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-29 12:50:22 +0400

    esff

--- resources/views/be-a-mentor.blade.php
+++ resources/views/be-a-mentor.blade.php
@@ -2883,9 +2883,11 @@
     <script
         src="https://wiseadvizor.com/wp-content/plugins/wpforms-lite/assets/js/integrations/elementor/frontend.min.js?ver=1.8.2.2"
         id="wpforms-elementor-js"></script>
-    <script src="{{ asset('public/assets/js/jquery-1.10.2.js') }}"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
+    </script>
+    <!-- <script src="{{ asset('public/assets/js/jquery-1.10.2.js') }}"></script>
     <script src="{{ asset('public/assets/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('public/assets/js/ct-navbar.js') }}"></script>
+    <script src="{{ asset('public/assets/js/ct-navbar.js') }}"></script> -->
 
     <script type="text/javascript">
     (function($) {
