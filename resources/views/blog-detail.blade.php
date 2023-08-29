538e14811770a2782efcd8c0b54367e1f99e66a9
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-29 14:06:52 +0400

    dsfdf

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -17,9 +17,10 @@
         href="https://wiseadvizor.com/feed/">
     <link rel="alternate" type="application/rss+xml" title="WISE ADVIZOR &raquo; Comments Feed"
         href="https://wiseadvizor.com/comments/feed/">
-    <link href="asset('public/assets/css/pe-icon-7-stroke.css')" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
+    <!-- <link href="asset('public/assets/css/pe-icon-7-stroke.css')" rel="stylesheet" />
     <link href="{{ asset('public/assets/css/pe-icon-7-stroke.css') }}" rel="stylesheet" />
-    <link href="{{ asset('public/assets/css/ct-navbar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/css/ct-navbar.css') }}" rel="stylesheet" /> -->
     <script>
     window._wpemojiSettings = {
         "baseUrl": "https:\/\/s.w.org\/images\/core\/emoji\/14.0.0\/72x72\/",
@@ -1477,62 +1478,86 @@
                                 data-id="4bc8cfa" data-element_type="widget" data-widget_type="button.default">
                                 <div class="elementor-widget-container">
                                     <div class="elementor-button-wrapper">
-                                        @if(Auth::id())
-                                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                            <ul class="nav navbar-nav navbar-right">
-                                                <li class="dropdown">
-                                                    <a href="#" class="dropdown-toggle menuITem" data-toggle="dropdown">
-                                                        @if(Auth::user()->metaData &&
-                                                        Auth::user()->metaData->profile_pic)
-                                                        <img src="{{ asset('public/assets/img/') }}/{{ Auth::user()->metaData->profile_pic }}"
-                                                            alt="wiseAdvizor" width="150;" height="75px;"
-                                                            style="object-fit:contain;"
-                                                            class="d-inline-block align-top">
-                                                        <p class="name">{{ Auth::user()->name }}</p>
-                                                        @else
-                                                        <i class="pe-7s-user"></i>
-                                                        <p class="name">{{ Auth::user()->name }}</p>
-                                                        @endif
-                                                    </a>
-                                                    <ul class="dropdown-menu menu">
-                                                        <a href="{{ route('user.dashboard') }}">Dashboard</a></br>
-                                                        <a
-                                                            href="{{ route('user.profile', [Auth::id()]) }}">Profile</a></br>
-                                                        <a href="{{ route('user.review') }}">Reviews</a><br />
-                                                        <a href="{{ route('logout') }}">Signout</a>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        @else
-                                        <a class="elementor-button elementor-button-link elementor-size-sm"
-                                            href="{{ route('login') }}">
-                                            <span class="elementor-button-content-wrapper">
-                                                <span class="elementor-button-text">Sign In</span>
-                                            </span>
-                                        </a>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="elementor-element elementor-element-78270f3 elementor-widget__width-auto elementor-widget elementor-widget-button"
-                                data-id="78270f3" data-element_type="widget" data-widget_type="button.default">
-                                <div class="elementor-widget-container">
-                                    <div class="elementor-button-wrapper">
                                         <a class="elementor-button elementor-button-link elementor-size-sm"
                                             href="{{ route('browseMentor') }}">
                                             <span class="elementor-button-content-wrapper">
                                                 <span class="elementor-button-text">Browse Mentor</span>
                                             </span>
                                         </a>
+                                        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light"> -->
+                                        <!-- </nav> -->
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="elementor-element elementor-element-78270f3 elementor-widget__width-auto elementor-widget elementor-widget-button"
+                            data-id="78270f3" data-element_type="widget" data-widget_type="button.default">
+                            <div class="elementor-widget-container">
+                                <div class="elementor-button-wrapper">
+                                    @if(Auth::id())
+                                    <div class="d-flex align-items-center">
+                                        <div class="dropdown">
+                                            <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#"
+                                                id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown"
+                                                aria-expanded="false">
+                                                <i class="fas fa-bell"></i>
+                                                <span class="badge rounded-pill badge-notification bg-danger">1</span>
+                                            </a>
+                                            <ul class="dropdown-menu dropdown-menu-end"
+                                                aria-labelledby="navbarDropdownMenuLink">
+                                                <li>
+                                                    <a class="dropdown-item" href="#">Some news</a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item" href="#">Another news</a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item" href="#">Something else here</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="dropdown">
+                                            <a class="dropdown-toggle d-flex align-items-center" href="#"
+                                                id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown"
+                                                aria-expanded="false">
+                                                <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                                    class="rounded-circle" height="5" width="50"
+                                                    alt="Black and White Portrait of a Man" loading="lazy" />
+                                            </a>
+                                            <ul class="dropdown-menu dropdown-menu-end"
+                                                aria-labelledby="navbarDropdownMenuAvatar">
+                                                <li>
+                                                    <a class="dropdown-item"
+                                                        href="{{ route('user.profile', [Auth::id()]) }}">My
+                                                        Profile</a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item" href="{{ route('user.review') }}">My
+                                                        Reviews</a>
+                                                </li>
+                                                <li>
+                                                    <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    @else
+                                    <a class="elementor-button elementor-button-link elementor-size-sm"
+                                        href="{{ route('login') }}">
+                                        <span class="elementor-button-content-wrapper">
+                                            <span class="elementor-button-text">Sign In</span>
+                                        </span>
+                                    </a>
+                                    @endif
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </header>
+             </div>
         </div>
+    </header>
+    </div>
     </header>
 
     <div data-elementor-type="wp-page" data-elementor-id="4349" class="elementor elementor-4349">
@@ -2923,6 +2948,8 @@
     <script
         src="https://wiseadvizor.com/wp-content/plugins/wpforms-lite/assets/js/integrations/elementor/frontend.min.js?ver=1.8.2.2"
         id="wpforms-elementor-js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
+    </script>
     <script type="text/javascript">
     (function($) {
         "use strict";
