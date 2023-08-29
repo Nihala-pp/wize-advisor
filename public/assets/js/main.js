78345126e5e3d38f8c6e39c7953c5348b4bd9731
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-29 18:36:03 +0400

    gfdg

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,30 +22,6 @@
             }
         });
 
-    });
-
-    // Setup the calendar with the current date
-    $(document).ready(function () {
-        var date = new Date();
-        var today = date.getDate();
-        // Set click handlers for DOM elements
-        $(".right-button").click({ date: date }, next_year);
-        $(".left-button").click({ date: date }, prev_year);
-        $(".month").click({ date: date }, month_click);
-        $("#add-button").click({ date: date }, new_event);
-        // Set current month as active
-        $(".months-row").children().eq(date.getMonth()).addClass("active-month");
-        getAvailableDates(date.getMonth(), date.getFullYear())
-        init_calendar(date);
-        var events = check_events(today, date.getMonth(), date.getFullYear());
-        show_events(events, months[date.getMonth()], today);
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
         $("body").on('click', '#payNow', function () {
             // var time = $('.event-card').val();
             var formdata = $('.scheduleCallForm').serialize();
@@ -162,287 +138,287 @@
                 }
                 show_events(times, event.data.month, event.data.day);
             },
-    });
-};
-
-function getAvailableDates(month, year) {
-    var mentor = $('.mentor').val();
-    var timezone = $("#timezone").val();
-    var date = new Date();
-
-    return $.ajax("https://wiseadvizor.com/getDateAvailability", {
-        method: 'GET',
-        data: {
-            "_token": $('meta[name="csrf-token"]').attr('content'),
-            "mentor": mentor,
-            "month": month + 1,
-            "year": year,
-            "timezone": timezone,
-        },
-        success: function (response) {
-            var available_dates = [];
-            for (var key in response) {
-                // console.log(response[key]);
-                available_dates.push({
-                    day: response[key],
-                });
-            }
-            init_calendar(date, available_dates);
-            // return available_dates;
-        },
-    });
-}
-
-// Event handler for when a month is clicked
-function month_click(event) {
-    $(".events-container").show(250);
-    $("#dialog").hide(250);
-    var date = event.data.date;
-    $(".active-month").removeClass("active-month");
-    $(this).addClass("active-month");
-    var new_month = $(".month").index(this);
-    date.setMonth(new_month);
-    init_calendar(date);
-}
-
-// Event handler for when the year right-button is clicked
-function next_year(event) {
-    $("#dialog").hide(250);
-    var date = event.data.date;
-    var new_year = date.getFullYear()+1;
-    $("year").html(new_year);
-    date.setFullYear(new_year);
-    init_calendar(date);
-}
-
-// Event handler for when the year left-button is clicked
-function prev_year(event) {
-    $("#dialog").hide(250);
-    var date = event.data.date;
-    var new_year = date.getFullYear()-1;
-    $("year").html(new_year);
-    date.setFullYear(new_year);
-    init_calendar(date);
-}
-
-// Event handler for clicking the new event button
-function new_event(event) {
-    // if a date isn't selected then do nothing
-    if($(".active-date").length===0)
-        return;
-    // remove red error input on click
-    $("input").click(function(){
-        $(this).removeClass("error-input");
-    })
-    // empty inputs and hide events
-    $("#dialog input[type=text]").val('');
-    $("#dialog input[type=number]").val('');
-    $(".events-container").hide(250);
-    $("#dialog").show(250);
-    // Event handler for cancel button
-    $("#cancel-button").click(function() {
-        $("#name").removeClass("error-input");
-        $("#count").removeClass("error-input");
-        $("#dialog").hide(250);
-        $(".events-container").show(250);
-    });
-    // Event handler for ok button
-    $("#ok-button").unbind().click({date: event.data.date}, function() {
-        var date = event.data.date;
-        var name = $("#name").val().trim();
-        var count = parseInt($("#count").val().trim());
-        var day = parseInt($(".active-date").html());
-        // Basic form validation
-        if(name.length === 0) {
-            $("#name").addClass("error-input");
-        }
-        else if(isNaN(count)) {
-            $("#count").addClass("error-input");
-        }
-        else {
-            $("#dialog").hide(250);
-            console.log("new event");
-            new_event_json(name, count, date, day);
-            date.setDate(day);
-            init_calendar(date);
-        }
-    });
-}
-
-// Adds a json event to event_data
-function new_event_json(name, count, date, day) {
-    var event = {
-        "occasion": name,
-        "invited_count": count,
-        "year": date.getFullYear(),
-        "month": date.getMonth()+1,
-        "day": day
+        });
     };
-    event_data["events"].push(event);
-}
 
-// Display all events of the selected date in card views
-function show_events(events, month, day) {
-     // Clear the dates container
-     $(".events-container").empty();
-     $(".events-container").show(250);
-     console.log(event_data["events"]);
-     // If there are no events for this date, notify the user
-     if(events.length===0) {
-         var event_card = $("<div class='event-card'></div>");
-         var event_name = $("<div class='event-name'>There are no calls planned for "+month+" "+day+".</div>");
-         $(event_card).css({ "border-left": "10px solid #FF1744" });
-         $(event_card).append(event_name);
-         $(".events-container").append(event_card);
-     }
-    else {
-        // Go through and add each event as a card to the events container
-        for (var i = 0; i < events.length; i++) {
-            var event_card = $("<button type='button' class='btn btn-outline-primary btn-lg btn-block event-card' value=" + events[i]["start_time"] + " style='background-color:white;border:solid;border-color:#007bff;  width:75%' required></button>");
-            var event_name = $("<div class='event-name'>" + events[i]["start_time"] + "</div>");
-            // var event_count = $("<div class='event-count'>"+events[i]["invited_count"]+" Invited</div>");
-            if (events[i]["cancelled"] === true) {
-                $(event_card).css({
-                    "border-left": "10px solid #FF1744"
-                });
-                // event_count = $("<div class='event-cancelled'>Cancelled</div>");
+    function getAvailableDates(month, year) {
+        var mentor = $('.mentor').val();
+        var timezone = $("#timezone").val();
+        var date = new Date();
+
+        return $.ajax("https://wiseadvizor.com/getDateAvailability", {
+            method: 'GET',
+            data: {
+                "_token": $('meta[name="csrf-token"]').attr('content'),
+                "mentor": mentor,
+                "month": month + 1,
+                "year": year,
+                "timezone": timezone,
+            },
+            success: function (response) {
+                var available_dates = [];
+                for (var key in response) {
+                    // console.log(response[key]);
+                    available_dates.push({
+                        day: response[key],
+                    });
+                }
+                init_calendar(date, available_dates);
+                // return available_dates;
+            },
+        });
+    }
+
+    // Event handler for when a month is clicked
+    function month_click(event) {
+        $(".events-container").show(250);
+        $("#dialog").hide(250);
+        var date = event.data.date;
+        $(".active-month").removeClass("active-month");
+        $(this).addClass("active-month");
+        var new_month = $(".month").index(this);
+        date.setMonth(new_month);
+        init_calendar(date);
+    }
+
+    // Event handler for when the year right-button is clicked
+    function next_year(event) {
+        $("#dialog").hide(250);
+        var date = event.data.date;
+        var new_year = date.getFullYear() + 1;
+        $("year").html(new_year);
+        date.setFullYear(new_year);
+        init_calendar(date);
+    }
+
+    // Event handler for when the year left-button is clicked
+    function prev_year(event) {
+        $("#dialog").hide(250);
+        var date = event.data.date;
+        var new_year = date.getFullYear() - 1;
+        $("year").html(new_year);
+        date.setFullYear(new_year);
+        init_calendar(date);
+    }
+
+    // Event handler for clicking the new event button
+    function new_event(event) {
+        // if a date isn't selected then do nothing
+        if ($(".active-date").length === 0)
+            return;
+        // remove red error input on click
+        $("input").click(function () {
+            $(this).removeClass("error-input");
+        })
+        // empty inputs and hide events
+        $("#dialog input[type=text]").val('');
+        $("#dialog input[type=number]").val('');
+        $(".events-container").hide(250);
+        $("#dialog").show(250);
+        // Event handler for cancel button
+        $("#cancel-button").click(function () {
+            $("#name").removeClass("error-input");
+            $("#count").removeClass("error-input");
+            $("#dialog").hide(250);
+            $(".events-container").show(250);
+        });
+        // Event handler for ok button
+        $("#ok-button").unbind().click({ date: event.data.date }, function () {
+            var date = event.data.date;
+            var name = $("#name").val().trim();
+            var count = parseInt($("#count").val().trim());
+            var day = parseInt($(".active-date").html());
+            // Basic form validation
+            if (name.length === 0) {
+                $("#name").addClass("error-input");
             }
+            else if (isNaN(count)) {
+                $("#count").addClass("error-input");
+            }
+            else {
+                $("#dialog").hide(250);
+                console.log("new event");
+                new_event_json(name, count, date, day);
+                date.setDate(day);
+                init_calendar(date);
+            }
+        });
+    }
+
+    // Adds a json event to event_data
+    function new_event_json(name, count, date, day) {
+        var event = {
+            "occasion": name,
+            "invited_count": count,
+            "year": date.getFullYear(),
+            "month": date.getMonth() + 1,
+            "day": day
+        };
+        event_data["events"].push(event);
+    }
+
+    // Display all events of the selected date in card views
+    function show_events(events, month, day) {
+        // Clear the dates container
+        $(".events-container").empty();
+        $(".events-container").show(250);
+        console.log(event_data["events"]);
+        // If there are no events for this date, notify the user
+        if (events.length === 0) {
+            var event_card = $("<div class='event-card'></div>");
+            var event_name = $("<div class='event-name'>There are no calls planned for " + month + " " + day + ".</div>");
+            $(event_card).css({ "border-left": "10px solid #FF1744" });
             $(event_card).append(event_name);
             $(".events-container").append(event_card);
         }
-    }
-}
-
-// Checks if a specific date has any events
-function check_events(day, month, year) {
-    var events = [];
-    for (var i = 0; i < event_data["events"].length; i++) {
-        var event = event_data["events"][i];
-        if (event["day"] === day &&
-            event["month"] === month &&
-            event["year"] === year) {
-            events.push(event);
+        else {
+            // Go through and add each event as a card to the events container
+            for (var i = 0; i < events.length; i++) {
+                var event_card = $("<button type='button' class='btn btn-outline-primary btn-lg btn-block event-card' value=" + events[i]["start_time"] + " style='background-color:white;border:solid;border-color:#007bff;  width:75%' required></button>");
+                var event_name = $("<div class='event-name'>" + events[i]["start_time"] + "</div>");
+                // var event_count = $("<div class='event-count'>"+events[i]["invited_count"]+" Invited</div>");
+                if (events[i]["cancelled"] === true) {
+                    $(event_card).css({
+                        "border-left": "10px solid #FF1744"
+                    });
+                    // event_count = $("<div class='event-cancelled'>Cancelled</div>");
+                }
+                $(event_card).append(event_name);
+                $(".events-container").append(event_card);
+            }
         }
     }
-    return events;
-}
 
-// Checks if a specific date has any events
-function check_events(day, month, year) {
-    var events = [];
-    for(var i=0; i<event_data["events"].length; i++) {
-        var event = event_data["events"][i];
-        if(event["day"]===day &&
-            event["month"]===month &&
-            event["year"]===year) {
+    // Checks if a specific date has any events
+    function check_events(day, month, year) {
+        var events = [];
+        for (var i = 0; i < event_data["events"].length; i++) {
+            var event = event_data["events"][i];
+            if (event["day"] === day &&
+                event["month"] === month &&
+                event["year"] === year) {
                 events.push(event);
             }
+        }
+        return events;
     }
-    return events;
-}
 
-// Given data for events in JSON format
-var event_data = {
-    "events": [
-    {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10,
-        "cancelled": true
-    },
-    {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10,
-        "cancelled": true
-    },
-        {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10,
-        "cancelled": true
-    },
-    {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10
-    },
-        {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10,
-        "cancelled": true
-    },
-    {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10
-    },
-        {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10,
-        "cancelled": true
-    },
-    {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10
-    },
-        {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10,
-        "cancelled": true
-    },
-    {
-        "occasion": " Repeated Test Event ",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 10
-    },
-    {
-        "occasion": " Test Event",
-        "invited_count": 120,
-        "year": 2020,
-        "month": 5,
-        "day": 11
+    // Checks if a specific date has any events
+    function check_events(day, month, year) {
+        var events = [];
+        for (var i = 0; i < event_data["events"].length; i++) {
+            var event = event_data["events"][i];
+            if (event["day"] === day &&
+                event["month"] === month &&
+                event["year"] === year) {
+                events.push(event);
+            }
+        }
+        return events;
     }
-    ]
-};
 
-const months = [ 
-    "January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December" 
-];
+    // Given data for events in JSON format
+    var event_data = {
+        "events": [
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10,
+                "cancelled": true
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10,
+                "cancelled": true
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10,
+                "cancelled": true
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10,
+                "cancelled": true
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10,
+                "cancelled": true
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10,
+                "cancelled": true
+            },
+            {
+                "occasion": " Repeated Test Event ",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 10
+            },
+            {
+                "occasion": " Test Event",
+                "invited_count": 120,
+                "year": 2020,
+                "month": 5,
+                "day": 11
+            }
+        ]
+    };
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
 
 })(jQuery);
