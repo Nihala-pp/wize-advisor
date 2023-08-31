b366fabae0e546d313ed96a1df392ee0ef31fcea
nihala-pp <nihala-pp@pransainvestment.com>
2023-08-31 20:09:41 +0400

    sdsfds

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -1523,7 +1523,7 @@
                                             <a class="text-reset me-3 dropdown-toggle hidden-arrow" href="#"
                                                 id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown"
                                                 aria-expanded="false">
-                                                <i class="fas fa-bell"></i>
+                                                <i class="fas fa-bell fa-lg"></i>
                                                 <span class="badge rounded-pill badge-notification bg-danger">1</span>
                                             </a>
                                             <ul class="dropdown-menu dropdown-menu-end"
@@ -1539,12 +1539,12 @@
                                                 </li>
                                             </ul>
                                         </div>
-                                        <div class="dropdown">
+                                        <div class="dropdown" style="padding-right:30px">
                                             <a class="dropdown-toggle d-flex align-items-center" href="#"
                                                 id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown"
                                                 aria-expanded="false">
                                                 <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                                                    class="rounded-circle" height="5" width="50"
+                                                    class="rounded-circle" height="8" width="100"
                                                     alt="Black and White Portrait of a Man" loading="lazy" />
                                             </a>
                                             <ul class="dropdown-menu dropdown-menu-end"
