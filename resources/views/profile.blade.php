b1380e825543d12fddd4e0e2c0e6f5f1b0cad32a
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-01 14:40:38 +0400

    sadsad

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2144,7 +2144,7 @@
                                 }
 
                                 div.elementor-widget-container.description > ul {
-                                    word-spacing:-2px !important;
+                                    word-spacing:-1px !important;
                                     align-content: left !important;
                                 }
                                 </style>
