1a32553c1903ca7b7451539c594cf7e6127a5435
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-05 19:13:20 +0400

    sdadsad

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1619,9 +1619,8 @@
                 <div class="card">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                            <h6 class="text-white text-capitalize ps-3">@if(!empty($data)) Update Profile Details @else
-                                Add
-                                Profile Details @endif</h6>
+                            <h6 class="text-white text-capitalize ps-3">@if(!empty($data)) My Profile  @else
+                            My Profile @endif</h6>
                         </div>
                     </div>
                     <div class="card-body">
