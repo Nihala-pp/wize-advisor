9166c1b24a072bc205990b8490c2835a45dbfc8a
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-05 17:54:55 +0400

    dfdasfdsds

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 @include('partials.header')
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -1620,8 +1619,8 @@
                         <div class="card">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                                 <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                                    <h6 class="text-white text-capitalize ps-3">@if(!empty($data)) Edit Mentor @else Add
-                                        Mentor @endif</h6>
+                                    <h6 class="text-white text-capitalize ps-3">@if(!empty($data)) Update Profile Details @else Add
+                                    Profile Details @endif</h6>
                                 </div>
                             </div>
                             <div class="card-body">
