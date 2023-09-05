14df1524b677a1d3be09193c940766a1d59681d5
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-05 19:33:30 +0400

    wsadadsa

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1642,7 +1642,7 @@
                                     <label class="">Password</label>
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
+                                         autocomplete="current-password">
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
