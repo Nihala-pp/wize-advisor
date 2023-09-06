157a87a2a8cc53003a259aa79fb379bbbc65b57e
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-06 16:32:20 +0400

    fdfdgdgf

--- resources/views/users/change-password.blade.php
+++ resources/views/users/change-password.blade.php
@@ -1637,7 +1637,7 @@
     <!--   Core JS Files   -->
     <script type="text/javascript" src="{{ asset('public/assets/js/material-dashboard.min.js?v=3.0.0') }}">
     </script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"></script>
     </body> 
 </style>
 </html>
