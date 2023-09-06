af48e9d7b71ee880324f211314151e4cf7d89a35
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-06 17:16:47 +0400

    fdfdgdgf

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1544,6 +1544,11 @@
                                                             Profile</a>
                                                     </li>
                                                     <li>
+                                                        <a class="dropdown-item"
+                                                            href="{{ route('user.change-password') }}">Change
+                                                            Password</a>
+                                                    </li>
+                                                    <li>
                                                         <a class="dropdown-item" href="{{ route('user.review') }}">My
                                                             Reviews</a>
                                                     </li>
