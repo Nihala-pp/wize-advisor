6b90b7ff34d2c4a4fe930094d4a3ac5668787a87
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-07 13:51:57 +0400

    sdsfsf

--- resources/views/mail/call.blade.php
+++ resources/views/mail/call.blade.php
@@ -6,8 +6,8 @@
 </head>
 
 <body>
-    <h1>Dear [User's Name],</h1>
-    <p>We are excited to inform you that your requested call with the {Mentor Name} has been approved and scheduled! We
+    <h1>Dear {{ $details['user_name'] }},</h1>
+    <p>We are excited to inform you that your requested call with the {{ $details['mentor_name'] }} has been approved and scheduled! We
         appreciate your patience and are thrilled to facilitate this valuable connection between you and the mentor.
     </p><br />
     <p>Here are the details of your scheduled call: </p><br />
@@ -15,24 +15,23 @@
     <div class="card text-center mt-5">
         <div class="card-body">
             <p class="card-text">Meeting Details are mentioned below</p>
-            <h5 class="card-title">Mentor : <a href="{{ $details['join_url'] }}">{{ $details['join_url'] }} </a></h5>
-            <h5 class="card-title">Date : {{ $details['password'] }} </h5>
-            <h5 class="card-title">Time : {{ $details['password'] }} </h5>
+            <h5 class="card-title">Mentor : {{ $details['mentor_name'] }} </a></h5>
+            <h5 class="card-title">Date : {{ $details['date'] }} </h5>
+            <h5 class="card-title">Time : {{ $details['time'] }} </h5>
             <h5 class="card-title">Duration : 30 Min Meeting </h5>
             <h5 class="card-title">Joining Link : {{ $details['join_url'] }} </h5>
 
-            <p>We are excited to inform you that your requested call with the {Mentor Name} has been approved and
+            <p>We are excited to inform you that your requested call with the {{ $details['mentor_name'] }} has been approved and
                 scheduled! We appreciate your patience and are thrilled to facilitate this valuable connection between
                 you and the mentor. </p><br />
-            <p>We are excited to inform you that your requested call with the {Mentor Name} has been approved and
+            <p>We are excited to inform you that your requested call with the {{ $details['mentor_name'] }} has been approved and
                 scheduled! We appreciate your patience and are thrilled to facilitate this valuable connection between
                 you and the mentor. </p><br />
-            <p>We are excited to inform you that your requested call with the {Mentor Name} has been approved and
+            <p>We are excited to inform you that your requested call with the {{ $details['mentor_name'] }} has been approved and
                 scheduled! We appreciate your patience and are thrilled to facilitate this valuable connection between
                 you and the mentor. </p><br />
         </div>
     </div>
-    <p>Thank You for being an valued customer with wiseAdvizor</p>
+    <p>Best regards,  <br />wiseAdvizor Team</p>
 </body>
-
 </html>
