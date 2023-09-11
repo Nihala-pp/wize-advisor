40d4acf72e4b2b32b9f8b01ea7f82bf4402ab413
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-11 11:42:50 +0400

    \zxasxdsdsf

--- resources/views/admin/login_history.blade.php
+++ resources/views/admin/login_history.blade.php
@@ -186,7 +186,7 @@
                                             <td>
                                                 <div class="d-flex px-2 py-1">
                                                     <div>
-                                                        <img src="{{ asset('assets/img/').'/'. $activity->user->metaData ? $activity->user->metaData->profile_pic : '' }}"
+                                                        <img src="{{ asset('public/assets/img/').'/'. $activity->user->metaData ? $activity->user->metaData->profile_pic : '' }}"
                                                             class="avatar avatar-sm me-3 border-radius-lg" alt="user1">
                                                     </div>
                                                 </div>
