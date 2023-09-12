2584fb06245ff3a822f7cdda95a19a6d92554692
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-12 19:10:42 +0400

    aSSAs

--- resources/views/mentors/sessions.blade.php
+++ resources/views/mentors/sessions.blade.php
@@ -224,7 +224,7 @@
                                                     Illuminate\Support\Carbon::parse($user_timezone->format('H:i:s'))->addMinutes($upcoming_session->duration);
                                                     Illuminate\Support\Carbon::parse($user_timezone->format('H:i:s'))->addMinutes($upcoming_session->duration);
 
-                                                    echo $user_timezone->format('h:i A').'-'.$mentor_finish_time->format('h:i A');
+                                                    echo $user_timezone->format('h:i A').' - '.$mentor_finish_time->format('h:i A');
 
                                                     @endphp
                                                 </div>
