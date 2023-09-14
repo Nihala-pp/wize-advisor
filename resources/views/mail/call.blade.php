749fa2a500286745cfda5b9941c0f9140f4a11b2
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-14 17:52:55 +0400

    dsdfsfds

--- resources/views/mail/call.blade.php
+++ resources/views/mail/call.blade.php
@@ -6,8 +6,8 @@
 </head>
 
 <body>
-    <h1>Dear {{ $details['user_name'] }},</h1>
-    <p>We are excited to inform you that your requested call with the {{ $details['mentor_name'] }} has been approved and scheduled! We
+    <h1>Dear {{ $details->user->name }},</h1>
+    <p>We are excited to inform you that your requested call with the {{ $details->mentor->name }} has been approved and scheduled! We
         appreciate your patience and are thrilled to facilitate this valuable connection between you and the mentor.
     </p><br />
     <p>Here are the details of your scheduled call: </p><br />
@@ -15,13 +15,12 @@
     <div class="card text-center mt-5">
         <div class="card-body">
             <p class="card-text">Meeting Details are mentioned below</p>
-            <h5 class="card-title">Mentor : {{ $details['mentor_name'] }} </a></h5>
-            <h5 class="card-title">Date : {{ $details['date'] }} </h5>
-            <h5 class="card-title">Time : {{ $details['time'] }} </h5>
-            <h5 class="card-title">Timezone : {{ $details['utc'] }} </h5>
+            <h5 class="card-title">Mentor : {{ $details->mentor->name }} </a></h5>
+            <h5 class="card-title">Date : {{ $details->date }} </h5>
+            <h5 class="card-title">Time : {{ $details->time }} </h5>
+            <h5 class="card-title">Timezone : {{ $details->utc }} </h5>
             <h5 class="card-title">Duration : 30 Min Meeting </h5>
-            <h5 class="card-title">Joining Link : {{ $details['join_url'] }} </h5><br />
-            <h5 class="card-title">Password : {{ $details['password'] }} </h5><br />
+            <h5 class="card-title">Joining Link : {{ $details->join_url }} </h5><br />
 
             <p>Please mark your calendar accordingly and ensure that you are available at the scheduled time. The mentor is eagerly looking forward to providing you with guidance and support based on your specific needs and objectives. </p><br />
             <p>This call presents a wonderful opportunity for you to gain valuable insights, ask questions, and receive guidance from an experienced mentor in your field of interest. Be prepared to share information about your startup, challenges, and goals so that the mentor can provide tailored advice to help you on your entrepreneurial journey. </p><br />
