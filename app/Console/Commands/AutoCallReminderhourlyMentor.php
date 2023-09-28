d4bb3e2395af1171c30e63ca58165c4d2f11f9cc
nihala-pp <nihala-pp@pransainvestment.com>
2023-09-28 12:08:27 +0400

    asdsf

--- app/Console/Commands/AutoCallReminderhourlyMentor.php
+++ app/Console/Commands/AutoCallReminderhourlyMentor.php
@@ -31,24 +31,23 @@ class AutoCallReminderhourlyMentor extends Command
      */
     public function handle()
     {
-        Log::info('Every minute cron job running');
-        // $user = User::find(142);
-        // $call = AvailableSchedule::find(238);
-        // Mail::to($user->email)->send(new callReminder($call));
-        // $calls = AvailableSchedule::where('is_booked', 1)
-        //     ->whereMonth('date', date('m'))
-        //     ->whereDay('date', date('d'))
-        //     ->get();
+        $user = User::find(142);
+        $call = AvailableSchedule::find(238);
+        Mail::to($user->email)->send(new callReminder($call));
+        $calls = AvailableSchedule::where('is_booked', 1)
+            ->whereMonth('date', date('m'))
+            ->whereDay('date', date('d'))
+            ->get();
 
-        // if ($calls->count() > 0) {
-        //     foreach ($calls as $call) {
-        //         if((Carbon::parse($call->start_time)->subHour()->format('H:i:s')) == (Carbon::now()->timezone($call->time_zone)->format('H:i:s'))) {
+        if ($calls->count() > 0) {
+            foreach ($calls as $call) {
+                if((Carbon::parse($call->start_time)->subHour()->format('H:i:s')) == (Carbon::now()->timezone($call->time_zone)->format('H:i:s'))) {
 
-        //             Mail::to($call->user->email)->send(new callReminder($call));                  
-        //         }
-        //     }
-        // }
+                    Mail::to($call->user->email)->send(new callReminder($call));                  
+                }
+            }
+        }
 
-        // return 0;
+        return 0;
     }
 }
