44ef22123a16f25a0292af4dc0498a978673b11a
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-02 12:35:35 +0400

    we23e

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2797,11 +2797,11 @@
                         <section
                             class="elementor-section elementor-inner-section elementor-element elementor-element-6f5a20e elementor-section-boxed elementor-section-height-default elementor-section-height-default"
                             data-id="6f5a20e" data-element_type="section">
+                            @foreach($reviews as $review)
                             <div class="elementor-container elementor-column-gap-default">
                                 <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-035a934"
                                     data-id="035a934" data-element_type="column"
                                     data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-                                    @foreach($reviews as $review)
                                     <div class="elementor-widget-wrap elementor-element-populated">
                                         <div class="elementor-background-overlay"></div>
                                         <div class="elementor-element elementor-element-642ada1 elementor-view-default elementor-widget elementor-widget-icon"
@@ -2880,9 +2880,9 @@
                                             </div>
                                         </div>
                                     </div>
-                                    @endforeach
                                 </div>
                             </div>
+                            @endforeach
                         </section>
                     </div>
                 </div>
