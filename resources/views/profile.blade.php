91c4d753b95d746bccc9cff3028ce3226a1e543c
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-02 16:36:10 +0400

    asad

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2798,8 +2798,8 @@
                                 <div class="testimonial-block">
                                     <div class="row feedback">
                                         @foreach($reviews as $review)
+                                        <div class="card">
                                         <div class="col-lg-4 col-md-6 mb-4">
-                                            <div class="card">
                                                 <div class="testimonial-box m-0 h-100">
                                                     <img src="https://www.sociablekit.com/tutorials/tutorials-resources/images/quote-top.svg"
                                                         alt="" class="quote-icon" />
