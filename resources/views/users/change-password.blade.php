a5268a90fa68039bb7131b081ddc1367d7b906da
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-05 16:52:25 +0400

    dfdg

--- resources/views/users/change-password.blade.php
+++ resources/views/users/change-password.blade.php
@@ -17,7 +17,6 @@
 
 
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-        <div class="row" style="background-image: url('{{ asset('public/assets/img/cover.jpg') }}')">
             <div class="container-fluid py-4">
                 <div class="row mt-5">
                     <div class="col-4 col-lg-10 m-auto">
@@ -59,6 +58,7 @@
                         </div>
                     </div>
                 </div>
+            </div>
         <!-- @include('partials.web-footer') -->
     </main>
     </div>
