2e4e93a9a75b43748eb47d4c5c9756578645e7a8
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-05 18:41:39 +0400

    aSAD

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -376,7 +376,7 @@
                                                             data-mdb-toggle="dropdown" aria-expanded="false">
                                                             <i class="fas fa-bell fa-lg"></i>
                                                             <span
-                                                                class="badge rounded-pill badge-notification bg-danger">1</span>
+                                                                class="badge badge-notification bg-danger">1</span>
                                                         </a>
                                                         <ul class="dropdown-menu dropdown-menu-end"
                                                             aria-labelledby="navbarDropdownMenuLink">
