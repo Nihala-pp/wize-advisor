29a28bf45a731867cffbbdfd04d291eef9a133ce
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-06 15:57:15 +0400

    asd

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1517,8 +1517,8 @@
                                                     id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown"
                                                     aria-expanded="false">
                                                     <i class="fas fa-bell fa-2xl"></i>
-                                                    <span
-                                                        class="badge rounded-pill badge-notification bg-danger">1</span>
+                                                    <!-- <span
+                                                        class="badge rounded-pill badge-notification bg-danger">1</span> -->
                                                 </a>
                                                 <ul class="dropdown-menu dropdown-menu-end"
                                                     aria-labelledby="navbarDropdownMenuLink">
--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1522,8 +1522,8 @@
                                                     id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown"
                                                     aria-expanded="false">
                                                     <i class="fas fa-bell fa-2xl"></i>
-                                                    <span
-                                                        class="badge rounded-pill badge-notification bg-danger">1</span>
+                                                    <!-- <span
+                                                        class="badge rounded-pill badge-notification bg-danger">1</span> -->
                                                 </a>
                                                 <ul class="dropdown-menu dropdown-menu-end"
                                                     aria-labelledby="navbarDropdownMenuLink">
