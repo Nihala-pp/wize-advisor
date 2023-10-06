5654ba5f644c819466d132688c56db79c9573acc
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-06 16:26:06 +0400

    werwer

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -1054,8 +1054,7 @@
         class="page-template page-template-elementor_canvas page page-id-13 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-13">
 
         @include('partials.menu')
-
-        <!-- <div class="container"> -->
+        w
         <div class="row">
             <div class="col-sm-4 mb-3 mt-2">
                 <div class="card h-100">
