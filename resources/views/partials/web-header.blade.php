1ff560c1a777faa67da03287b05677e4bace0a37
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-10 11:48:38 +0400

    AQS

--- resources/views/partials/web-header.blade.php
+++ resources/views/partials/web-header.blade.php
@@ -1564,7 +1564,5 @@ var wpformsElementorVars = {
 <script
     src="https://wiseadvizor.com/wp-content/plugins/wpforms-lite/assets/js/integrations/elementor/frontend.min.js?ver=1.8.2.2"
     id="wpforms-elementor-js"></script>
-
-
 <!-- <body
     class="page-template page-template-elementor_canvas page page-id-13 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-13"> -->
