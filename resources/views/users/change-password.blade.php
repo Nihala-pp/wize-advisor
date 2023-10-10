f9bbfd865a4421e81211815e86b9922167668b33
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-10 14:15:38 +0400

    bhgug

--- resources/views/users/change-password.blade.php
+++ resources/views/users/change-password.blade.php
@@ -18,7 +18,6 @@
     <!-- Navbar -->
     <!-- End Navbar -->
 
-
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
             <div class="container-fluid py-4">
                 <div class="row mt-5">
