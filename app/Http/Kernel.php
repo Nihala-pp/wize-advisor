9e85af05ea156d9eaa02b78466cb66f0e58d586a
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-11 12:44:28 +0400

    efref

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\SessionExpired::class,
         ],
 
         'api' => [
@@ -50,6 +51,10 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
     ];
 
     /**
