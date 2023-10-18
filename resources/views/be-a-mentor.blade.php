534c0894819b9201ceae907c47222fb7f4516553
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-18 19:14:53 +0400

    wedwe

--- resources/views/be-a-mentor.blade.php
+++ resources/views/be-a-mentor.blade.php
@@ -1361,6 +1361,11 @@
         id="wpforms-elementor-js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
     <!-- <script src="{{ asset('public/assets/js/jquery-1.10.2.js') }}"></script>
     <script src="{{ asset('public/assets/js/bootstrap.js') }}"></script>
     <script src="{{ asset('public/assets/js/ct-navbar.js') }}"></script> -->
@@ -1372,7 +1377,7 @@
 
             $('#charge').on('change', function() {
                 alert("sdfsfds");
-                 $('.price').show();
+                $('.price').show();
             });
 
             $('#niso-carousel-952.owl-carousel').owlCarousel({
