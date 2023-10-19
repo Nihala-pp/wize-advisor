9c9008705ff787c812e4024fecc05e03e11a2927
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-19 19:28:29 +0400

    SASAD

--- resources/views/be-a-mentor.blade.php
+++ resources/views/be-a-mentor.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en-US">
-@include('partials.web-header')
+<html lang="en">
+@php header('Access-Control-Allow-Origin: *'); @endphp
 
+@include('partials.web-header')
+@include('partials.header')
 <body
-    class="page-template page-template-elementor_canvas page page-id-4301 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-4301">
+    class="page-template page-template-elementor_canvas page page-id-13 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-13">
     @include('partials.menu')
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <div class="container-fluid py-4">
