1177b54bc39bbc1a8e16beaf0b69be6cbb6020fd
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-19 15:19:05 +0400

    sdcsdc

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -295,10 +295,10 @@
         }
     }
     </style>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
     <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js">
-    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@6.1.5/index.global.min.js"></script>
     <script src="https://kit.fontawesome.com/42d5adcbca.js"></script>
