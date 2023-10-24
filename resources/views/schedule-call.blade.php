63887a8f9c6a8fd39860a0d81231798a97e5ca01
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-24 13:58:05 +0400

    asada

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -2354,7 +2354,8 @@
             $(".active-month").removeClass("active-month");
             $(this).addClass("active-month");
             var new_month = $(".month").index(this);
-            date.setMonth(date.getMonth() + 1);
+            date.setMonth(new_month);
+            // date.setMonth(date.getMonth() + 1);
             // date.setMonth(new_month);
             init_calendar(date);
         }
