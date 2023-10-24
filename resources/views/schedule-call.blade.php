454da2d543ebb6e9d13b06c5342b11a167abe2c8
nihala-pp <nihala-pp@pransainvestment.com>
2023-10-24 14:17:06 +0400

    wsdwaewq

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -2354,8 +2354,8 @@
             $(".active-month").removeClass("active-month");
             $(this).addClass("active-month");
             var new_month = $(".month").index(this);
-            date.setMonth(date.getMonth() + 1);
-            // date.setMonth(new_month);
+            // date.setMonth(date.getMonth() + 1);
+            date.setMonth(new_month);
             init_calendar(date);
         }
 
@@ -2437,16 +2437,16 @@
 
         // Display all events of the selected date in card views
         function show_events(events, month, day) {
-            console.log(month);
+            // console.log(month);
             // Clear the dates container
-            // var d = new Date();
-            // var new_month = getMonthFromString(month);
+            var d = new Date();
+            var new_month = getMonthFromString(month);
             // var month_name = getMonthName(new_month);
-            // if (months[d.getMonth()] === month) {
-            //     month_name = month;
-            // } else {
-            //     month_name = getMonthName(new_month);
-            // }
+            if (months[d.getMonth()] === month) {
+                month_name = month;
+            } else {
+                month_name = getMonthName(new_month);
+            }
             // console.log(month_name);
             $(".events-container").empty();
             $(".events-container").show(250);
@@ -2456,7 +2456,7 @@
                 // console.log(getMonth(month));
                 var event_card = $("<div class='event-card'></div>");
                 var event_name = $("<div class='event-name'>There are no slots available on " +
-                    month +
+                month_name +
                     " " +
                     day +
                     ".</div>");
