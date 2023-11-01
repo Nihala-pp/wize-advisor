3d9f4b5d3150a7234afc82da5320abd9785f8d27
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-01 17:37:11 +0400

    ewr

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -1710,7 +1710,7 @@
                         </div>
                         <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-3 filters datefilter">
                             <input type="date" placeholder="FILTER BY DATE" class="form-control selectpicker" id="date"
-                                name="date" data-style="btn-info" required>
+                                name="date" required>
                         </div>
                         <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-3 filters">
                             <select id="name" class="selectpicker" data-style="btn-info">
