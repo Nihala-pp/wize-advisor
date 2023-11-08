aa6cdaef99fe1bcdfe39ba6f0a1733dd652c51f7
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-08 15:58:34 +0400

    asads

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3705,6 +3705,11 @@ i.fas.fa-bell.fa-2xl {
         text-align: justify;
         text-justify: inter-word;
     }
+
+    .elementor-18 .elementor-element.elementor-element-2642eda .elementor-accordion-icon.elementor-accordion-icon-left {
+        margin-right: 8px;
+        padding-left: 10px;
+    }
 }
 </style>
 
