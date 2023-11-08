b9ad34abe2eaf8e192d91339d4c2d5606d258ee5
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-08 18:50:35 +0400

    eret

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4804,7 +4804,7 @@ i.fas.fa-bell.fa-2xl {
 h1.elementor-heading-title.elementor-size-default.heading {
     font-size: 19px !important;
     color: #330252 !important;
-    padding-left: 55px !important;
+    /* padding-left: 55px !important; */
     text-align: center !important;
 }
 
