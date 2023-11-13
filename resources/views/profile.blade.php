c98a2158771a839bdf87ec969377959786ad1c41
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-13 19:01:46 +0400

    aSAs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5067,7 +5067,7 @@ i.fas.fa-bell.fa-2xl {
     }
 
     .elementor-2372 .elementor-element.elementor-element-cab3f6f .elementor-divider-separator {
-        width: 29% !important;
+        width: 35% !important;
         margin: 0 auto;
         /* padding-left: 50px !important; */
         /* margin-center: 0; */
@@ -5077,7 +5077,7 @@ i.fas.fa-bell.fa-2xl {
         text-align: center;
         padding-top: 2px;
         padding-bottom: 2px;
-        padding-left:135px;
+        padding-left:150px;
     }
 
     .elementor-2372 .elementor-element.elementor-element-90a514a .elementor-heading-title {
