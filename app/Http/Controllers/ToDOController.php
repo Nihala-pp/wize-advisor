256607e6bf824d019caac0ab822f6f32e4190411
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-27 16:15:44 +0400

    sadsds

---
+++ app/Http/Controllers/ToDOController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ToDoMail;
+use App\Models\TodoList;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Exception;
+use Log;
+
+class ToDOController extends Controller
+{
+    public function index()
+    {
+        try {
+            $to_do_task = TodoList::where('is_done', '0')->whereMonth('created_at', date('m'))
+                ->whereDay('created_at', date('d'))->get();
+
+            foreach ($to_do_task as $to_do) {
+
+                $user_timezone = new \DateTime($to_do->created_at, new \DateTimeZone('Asia/Tbilisi'));
+
+                $user_finish_time = Carbon::parse($user_timezone->format('H:i'))->subHour()->format('Y-m-d H:i');
+
+                $current_time = Carbon::now()->timezone('Asia/Tbilisi')->format('Y-m-d H:i');
+
+                if ($user_finish_time == $current_time) {
+
+                    $details = [
+                        'task' => $to_do->task,
+                        'created_at' => $to_do->created_at,
+                    ];
+
+                    Mail::to('info@wiseadvizor.com')->send(new ToDoMail($details));
+                    Log::info('mail after call completion Successfully send');
+                }
+            }
+            return "Done!";
+        } catch (Exception $e) {
+            return "Something went wrong!";
+        }
+    }
+}
---
+++ resources/views/mail/to-do-mail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>wiseAdvizor</title>
+</head>
+
+<body>
+    <h1>Dear Admin,</h1>
+    <p>The Task - {{ $details->task }} you have created on {{ $details->created_at }} is in pending list. Please Take necessary action.</p>
+
+    <p>Best Regards, <br />wiseAdvizor Team</p>
+</body>
+
+</html>
