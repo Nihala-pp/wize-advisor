edac99791652d6ef80d6af08eea3bb64f5fbfef0
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-30 18:46:09 +0400

    asASA

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -570,6 +570,7 @@ class AdminController extends Controller
         foreach ($schedules as $schedule) {
 
             $user_timezone_start = new \DateTime($schedule->date . 'T' . $schedule->start_time, new \DateTimeZone($schedule->utc));
+            dd($user_timezone_start);
             $user_timezone_end = new \DateTime($schedule->date . 'T' . $schedule->end_time, new \DateTimeZone($schedule->utc));
 
             // $user_timezone->setTimezone(new \DateTimeZone('Asia/Tbilisi'));
