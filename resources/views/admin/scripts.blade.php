98c425515017a1f344e75dbce957a8da1be043bf
nihala-pp <nihala-pp@pransainvestment.com>
2023-11-30 18:01:02 +0400

    asdasd

--- resources/views/admin/scripts.blade.php
+++ resources/views/admin/scripts.blade.php
@@ -277,7 +277,7 @@ document.addEventListener('DOMContentLoaded', function() {
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
         themeSystem: 'bootstrap5',
         expandRows: true,
-        height: 1300,
+        height: 1400,
         allDaySlot: false,
         timeZone: 'local',
         initialView: 'resourceTimeGridDay',
