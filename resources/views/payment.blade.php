e40c6b784e5423db65db89095946d559252b87aa
nihala-pp <nihala-pp@pransainvestment.com>
2023-12-05 16:41:44 +0400

    swqsdqw

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -8,11 +8,21 @@
     class="page-template page-template-elementor_canvas page page-id-13 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-13">
     @include('partials.menu')
     <form id="payment-form" action="" method="post">
-        @csrf
-        <div id="dropin-container"></div>
-        <input type="submit" />
-        <input type="hidden" id="nonce" name="payment_method_nonce" />
+        <div class="col-md-8 card mt-5 mb-5">
+            <div class="py-12">
+                @csrf
+                <div id="dropin-container"></div>
+                <div style="display: flex;justify-content: center;align-items: center; color: white">
+                    <a id="submit-button" class="btn btn-sm btn-success mb-5">Pay Now</a>
+                </div> <input type="hidden" id="nonce" name="payment_method_nonce" />
+            </div>
+        </div>
     </form>
+
+    <!-- <div id="dropin-container" style="display: flex;justify-content: center;align-items: center;"></div>
+    <div style="display: flex;justify-content: center;align-items: center; color: white">
+        <a id="submit-button" class="btn btn-sm btn-success mb-5">Pay Now</a>
+    </div> -->
     <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
     <script>
     var submitButton = document.querySelector('#payNow');
@@ -43,8 +53,6 @@
         s0.parentNode.insertBefore(s1, s0);
     })();
     </script>
-    <!-- </div>
-    </div> -->
 </body>
 <style>
 .card {
