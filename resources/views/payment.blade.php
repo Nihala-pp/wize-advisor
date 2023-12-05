62bd4b07082f7ab8c3ecfa85cd85c534b7c5c270
nihala-pp <nihala-pp@pransainvestment.com>
2023-12-05 16:38:04 +0400

    qswqdqd

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -7,45 +7,44 @@
 <body
     class="page-template page-template-elementor_canvas page page-id-13 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-13">
     @include('partials.menu')
-    <div class="col-md-8 card mt-5 mb-5">
-        <div class="py-12">
-            @csrf
-            <div id="dropin-container" style="display: flex;justify-content: center;align-items: center;"></div>
-            <div style="display: flex;justify-content: center;align-items: center; color: white">
-                <a id="submit-button" class="btn btn-sm btn-success mb-5">Pay Now</a>
-            </div>
-            <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
-            <script>
-            var submitButton = document.querySelector('#payNow');
+    <form id="payment-form" action="" method="post">
+        @csrf
+        <div id="dropin-container"></div>
+        <input type="submit" />
+        <input type="hidden" id="nonce" name="payment_method_nonce" />
+    </form>
+    <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
+    <script>
+    var submitButton = document.querySelector('#payNow');
 
-            braintree.dropin.create({
-                authorization: '{{$clientToken}}',
-                container: '#dropin-container'
-            }, function(err, dropinInstance) {
-                submitButton.addEventListener('click', function() {
-                    dropinInstance.requestPaymentMethod(function(err, payload) {
-                        // Send payload.nonce to your server.
-                    });
-                });
+    braintree.dropin.create({
+        authorization: '{{$clientToken}}',
+        container: '#dropin-container'
+    }, function(err, dropinInstance) {
+        submitButton.addEventListener('click', function() {
+            dropinInstance.requestPaymentMethod(function(err, payload) {
+                // Send payload.nonce to your server.
             });
-            </script>
+        });
+    });
+    </script>
 
-            <!--Start of Tawk.to Script-->
-            <script type="text/javascript">
-            var Tawk_API = Tawk_API || {},
-                Tawk_LoadStart = new Date();
-            (function() {
-                var s1 = document.createElement("script"),
-                    s0 = document.getElementsByTagName("script")[0];
-                s1.async = true;
-                s1.src = 'https://embed.tawk.to/65046147b1aaa13b7a770b04/1hacho0lm';
-                s1.charset = 'UTF-8';
-                s1.setAttribute('crossorigin', '*');
-                s0.parentNode.insertBefore(s1, s0);
-            })();
-            </script>
-        </div>
-    </div>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/65046147b1aaa13b7a770b04/1hacho0lm';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+    </script>
+    <!-- </div>
+    </div> -->
 </body>
 <style>
 .card {
