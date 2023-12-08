2600f60e9579a3f8acd12b60ae1e28728bf04b40
nihala-pp <nihala-pp@pransainvestment.com>
2023-12-08 18:59:16 +0400

    sqwdwed

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -11,12 +11,14 @@
         <div class="col-md-8 card mt-5 mb-5">
             <div class="py-12">
                 @csrf
-                <div id="dropin-container"></div>
-                <!-- <input type="hidden" id="nonce" name="payment_method_nonce"> -->
+                <!-- <div id="dropin-container"></div>
+                <! -- <input type="hidden" id="nonce" name="payment_method_nonce"> -->
                 <input type="hidden" name="call_id" value="{{ $call_data->id }}" id="call_id">
-                <div style="display: flex;justify-content: center;align-items: center; color: white">
-                    <button type="submit" class="btn btn-sm btn-success mb-5" id="submit-button">Pay Now</button>
-                </div>
+                <input type="hidden" name="price" value="{{ $call_data->mentor->metaData->price_per_call }}" id="price">
+                <button id="google-pay-button">Pay with Google</button>
+                <!-- <div style="display: flex;justify-content: center;align-items: center; color: white">
+                    <button type="submit" class="btn btn-sm btn-success mb-5" id="submit-button">Pay Now</button> -->
+                <!-- </div>  -->
             </div>
         </div>
     </form>
@@ -25,53 +27,127 @@
     <div style="display: flex;justify-content: center;align-items: center; color: white">
         <a id="submit-button" class="btn btn-sm btn-success mb-5">Pay Now</a>
     </div> -->
-    <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
-    <script>
+    <script src="https://pay.google.com/gp/p/js/pay.js"></script>
+
+    <!-- <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script> -->
+    <!-- <script>
         var button = document.querySelector('#submit-button');
         var client_token = "{{$clientToken}}";
 
         braintree.dropin.create({
-          authorization: client_token,
-          selector: '#dropin-container',
-          paypal: {
-            flow: 'vault'
-          }
+            authorization: client_token,
+            selector: '#dropin-container',
+            paypal: {
+                flow: 'vault'
+            }
         }, function (createErr, instance) {
-          if (createErr) {
-            console.log('Create Error', createErr);
-            return;
-          }
-          button.addEventListener('click', function (event) {
-            event.preventDefault();
+            if (createErr) {
+                console.log('Create Error', createErr);
+                return;
+            }
+            button.addEventListener('click', function (event) {
+                event.preventDefault();
 
-            instance.requestPaymentMethod(function (err, payload) {
-            $(function() {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
+                instance.requestPaymentMethod(function (err, payload) {
+                    $(function () {
+                        $.ajaxSetup({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
+                                    'content')
+                            }
+                        });
+                        return $.ajax("https://wiseadvizor.com/token", {
+                            method: 'POST',
+                            data: {
+                                nonce: payload.nonce,
+                                call_id: $('#call_id').val()
+                            },
+                            success: function (response) {
+                                $('.payment-page').html(response);
+                            },
+                            error: function (response) {
+                                $('.payment-page').html(response);
+                            }
+                        });
+                    });
                 });
-                return $.ajax("https://wiseadvizor.com/token", {
-                    method: 'POST',
-                    data: {
-                        nonce : payload.nonce,
-                        call_id : $('#call_id').val()
-                    },
-                    success: function (response) {
-                        $('.payment-page').html(response);
-                    },
-                    error: function (response) {
-                        $('.payment-page').html(response);
-                    }
-                });
-              });
             });
-          });
         });
+    </script> -->
+
+    <script>
+    var price = $('#price').val();
+    const base64url = (str) => btoa(str).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+
+    const paymentDataRequest = {
+        apiVersion: 2,
+        apiVersionMinor: 0,
+        allowedPaymentMethods: [{
+            type: 'CARD',
+            parameters: {
+                allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                allowedCardNetworks: ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"]
+            },
+            tokenizationSpecification: {
+                type: 'PAYMENT_GATEWAY',
+                parameters: {
+                    gateway: 'your_payment_gateway',
+                    gatewayMerchantId: 'BCR2DN4T3HN7LXCE'
+                }
+            }
+        }],
+        merchantInfo: {
+            merchantId: 'BCR2DN4T3HN7LXCE',
+            merchantName: 'wiseAdvizor'
+        },
+        transactionInfo: {
+            totalPriceStatus: 'FINAL',
+            totalPriceLabel: 'Total',
+            totalPrice: price,
+            currencyCode: 'USD',
+            countryCode: 'US'
+        }
+    };
+
+    const paymentsClient = new google.payments.api.PaymentsClient({
+        environment: 'TEST' // Change to 'PRODUCTION' for live transactions
+    });
+
+    document.getElementById('google-pay-button').addEventListener('click', () => {
+        const paymentDataRequestWithParameters = Object.assign({}, paymentDataRequest);
+        paymentDataRequestWithParameters.transactionInfo.totalPrice = price; // Update with your actual price
+
+        paymentsClient.loadPaymentData(paymentDataRequestWithParameters)
+            .then((paymentData) => {
+                // Send the payment data to the server for processing
+                fetch('/handle-payment-confirmation', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            paymentToken: paymentData.paymentMethodData.tokenizationData.token
+                        })
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        // Handle the server response
+                        console.log(data);
+                    })
+                    .catch(error => {
+                        // Handle errors
+                        console.error(error);
+                    });
+            })
+            .catch((error) => {
+                // Handle errors
+                console.error(error);
+            });
+    });
     </script>
 
     <!--Start of Tawk.to Script-->
- <script type="text/javascript">
+    <script type="text/javascript">
     var Tawk_API = Tawk_API || {},
         Tawk_LoadStart = new Date();
     (function() {
@@ -83,7 +159,7 @@
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
     })();
- </script>
+    </script>
 </body>
 <style>
 .card {
