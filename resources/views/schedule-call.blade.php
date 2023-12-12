050def5fa553534c744da3bad55572c920470931
nihala-pp <nihala-pp@pransainvestment.com>
2023-12-12 15:53:29 +0400

    asadas

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -2287,7 +2287,6 @@
     <script src="{{ asset('public/assets/js/popper.js') }}"></script>
     <!-- <script src="{{ asset('public/assets/js/bootstrap.min.js') }}"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-    <script async src="https://pay.google.com/gp/p/js/pay.js" onload="onGooglePayLoaded()"></script>
     <script type="text/javascript">
     (function($) {
         // Setup the calendar with the current date
