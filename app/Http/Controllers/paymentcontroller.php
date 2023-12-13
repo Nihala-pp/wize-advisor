58edd9b03b600734abc66221bb3f82502569bf52
nihala-pp <nihala-pp@pransainvestment.com>
2023-12-13 18:37:49 +0400

    qasq

--- app/Http/Controllers/paymentcontroller.php
+++ app/Http/Controllers/paymentcontroller.php
@@ -3,34 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use PayPalCheckoutSdk\Core\PayPalHttpClient;
+use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use App\Order;
+use App\OrderItem;
+use App\Http\Controllers\CommonController;
+use App\Jobs\StoreOrderPlacedEmailSender;
+use App\Jobs\StoreOrderCancelledEmailSender;
+use App\Http\Controllers\ShopController;
+use Redirect;
+use Session;
+use URL;
+use Log;
+use DB;
 
-class paymentcontroller extends Controller
+class PaymentController extends Controller
 {
+    // private $_token_response;
+    // private $_paypalApiUrl;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
     public function __construct()
     {
-  /** PayPal api context **/
-        $paypal_conf = \Config::get('paypal');   
+        /** PayPal api context **/
+        $paypal_conf = \Config::get('paypal');
         // Creating an environment
         $clientId = $paypal_conf['client_id'];
         $clientSecret = $paypal_conf['secret'];
         $this->_paypalApiUrl = $paypal_conf['api_url'];
-        $params=['name'=>$clientId, 'surname'=>$clientSecret];
-        $ch = curl_init($this->_paypalApiUrl."v1/oauth2/token");
+        $params = ['name' => $clientId, 'surname' => $clientSecret];
+        $ch = curl_init($this->_paypalApiUrl . "v1/oauth2/token");
         curl_setopt($ch, CURLOPT_POST, 1);
         $headers = array(
             'Content-Type: application/json',
             'Accept: application/json',
-            'Authorization: Basic '. base64_encode($clientId.":".$clientSecret) 
+            'Authorization: Basic ' . base64_encode($clientId . ":" . $clientSecret)
         );
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, rawurldecode(http_build_query(array(
             'grant_type' => 'client_credentials'
-          ))));
-          $this->_token_response = json_decode(curl_exec($ch));
-          $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-          Log::info('token generation status: '.$status.'response'.gettype($this->_token_response));      
+        ))));
+        $this->_token_response = json_decode(curl_exec($ch));
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        Log::info('token generation status: ' . $status . 'response' . gettype($this->_token_response));
     }
+
 
     public function payWithpaypal(Request $request)
     {
@@ -39,12 +62,12 @@ class paymentcontroller extends Controller
         $input = $request->all();
         $order_no = $input['order_no'];
         $amount = $input['amount'];
-        $ch = curl_init($this->_paypalApiUrl."v2/checkout/orders");
+        $ch = curl_init($this->_paypalApiUrl . "v2/checkout/orders");
         curl_setopt($ch, CURLOPT_POST, 1);
         $headers = array(
             'Content-Type: application/json',
             'Accept: application/json',
-            'Authorization: Bearer '. $this->_token_response->access_token
+            'Authorization: Bearer ' . $this->_token_response->access_token
         );
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -53,50 +76,53 @@ class paymentcontroller extends Controller
             "purchase_units": [
               {
                 "amount": {
-                  "currency_code": "'.$paypal_conf['currency_type'].'",
-                  "value": "'.$amount.'"
+                  "currency_code": "' . $paypal_conf['currency_type'] . '",
+                  "value": "' . $amount . '"
                 }
               }
             ]
           }');
-          $_order_response = json_decode(curl_exec($ch), true);
-          $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-          Log::info('order generation response: '.$status.'response'.gettype($_order_response));
+        $_order_response = json_decode(curl_exec($ch), true);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        Log::info('order generation response: ' . $status . 'response' . gettype($_order_response));
         return response()->json($_order_response);
     }
 
     public function getPaymentStatus(Request $request)
     {
-          $input = $request->all();
-          $payerId = $request->payerID; 
-          $paypalOrderId = $request->orderID; 
-          $facilitorAccessToken =  $request->facilitatorAccessToken;
-          $ch = curl_init($this->_paypalApiUrl."v2/checkout/orders/".$paypalOrderId."/capture");
-          
-          curl_setopt($ch, CURLOPT_POST, 1);
-        
+
+        $input = $request->all();
+        $payerId = $request->payerID;
+        $paypalOrderId = $request->orderID;
+        $facilitorAccessToken = $request->facilitatorAccessToken;
+        log::info('facilitorAccessToken is' . $facilitorAccessToken);
+        $ch = curl_init($this->_paypalApiUrl . "v2/checkout/orders/" . $paypalOrderId . "/capture");
+        curl_setopt($ch, CURLOPT_POST, 1);
+
         $headers = array(
             'Content-Type: application/json',
             'Accept: application/json',
-            'Authorization: Bearer '. $this->_token_response->access_token,
-            'PayPal-Request-Id:'.$facilitorAccessToken
+            'Authorization: Bearer ' . $this->_token_response->access_token,
+            'PayPal-Request-Id:' . $facilitorAccessToken
         );
-
-        log::info('order_no'.$paypalOrderId.'facilitortoken'.$facilitorAccessToken);
+        log::info('order_no' . $paypalOrderId . 'facilitortoken' . $facilitorAccessToken);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
         $_capture_response = json_decode(curl_exec($ch), true, 512);
         $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-          
-        Log::info('order capture data : '.$status.'for'.$facilitorAccessToken.'response'
-                         .gettype($_capture_response['purchase_units'][0]['payments']['captures']));
-  
+        Log::info($_capture_response);
+
+        Log::info($paypalOrderId);
+        Log::info('order capture data : ' . $status . 'for' . $facilitorAccessToken . 'response' . gettype($_capture_response['purchase_units'][0]['payments']['captures']));
+
+
         return response()->json($_capture_response);
     }
 
-    public function cancel_order()
+    public function orderCancel(Request $request)
     {
-        
+        Log::info($request->all());
+        return json_encode(array('statusMsg' => "SUCCESS"), JSON_FORCE_OBJECT);
+
     }
 }
