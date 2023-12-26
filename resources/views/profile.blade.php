3464f18d238fde05da88873e73064579c78c8902
nihala-pp <nihala-pp@pransainvestment.com>
2023-12-26 13:19:53 +0400

    asasd

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1733,7 +1733,7 @@
                                             data-widget_type="heading.default">
                                             <div class="elementor-widget-container">
                                                 <p class="elementor-heading-title elementor-size-default">
-                                                    <b>Next Availability <br></b><br>   @if(!empty($nextAvailability)) {{ $nextAvailability->date }} @endif
+                                                    <b>Next Availability <br></b><br>   @if(!empty($nextAvailability)) {{ $nextAvailability->date->format('jS F Y') }} @endif
                                                 </p>
                                             </div>
                                         </div>
