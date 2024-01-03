f7f4b0347bfd205d6b75f758cce82bef72643cf6
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-03 18:24:09 +0400

    qwqe

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2699,7 +2699,7 @@
                                                 data-widget_type="heading.default">
                                                 <div class="elementor-widget-container">
                                                     <span class="elementor-heading-title elementor-size-default">
-                                                        <p> {{ $exp->start_date }} {{ \Str::lower('TO')}}
+                                                        <p> {{ $exp->start_date }} {{ strtolower('TO')}}
                                                             {{ $exp->end_date ? $exp->end_date : 'Present' }} </p>
                                                     </span>
                                                 </div>
