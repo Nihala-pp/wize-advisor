e59c87ae3e2a104cf308b2b95ee662f978386c6f
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-05 12:35:55 +0400

    werwre

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -3657,9 +3657,9 @@ screen and (min-width: 1024px) {
     }
 
     .elementor-star-rating__wrapper {
-        padding-left: 120px;
+        padding-left: 125px;
         display: flex;
-        align-items: right;
+        /* align-items: right; */
     }
 
     .price {
