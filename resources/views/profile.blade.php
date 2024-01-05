609de22919b6fd38481b768e954990d1a0965abb
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-05 11:31:16 +0400

    qweqewe

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4452,6 +4452,22 @@
         });
     }(jQuery));
     </script>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/65046147b1aaa13b7a770b04/1hacho0lm';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 <style>
 .elementor-4878 .elementor-element.elementor-element-f428972 .elementor-heading-title {
@@ -5082,9 +5098,9 @@ i.fas.fa-bell.fa-2xl {
     color: #ffff !important;
 }
 
-body .elementor-32 .elementor-element.elementor-element-dd4d324 .elementor-labels-above .elementor-field-group > label {
+body .elementor-32 .elementor-element.elementor-element-dd4d324 .elementor-labels-above .elementor-field-group>label {
     padding-bottom: 10px !important;
 }
-
 </style>
+
 </html>
