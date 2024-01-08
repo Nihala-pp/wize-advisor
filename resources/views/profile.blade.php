8547341f06265a6ea53965b7bc6ca17864936116
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-08 16:40:23 +0400

    AWEQE

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5944,6 +5944,7 @@ body .elementor-32 .elementor-element.elementor-element-dd4d324 .elementor-label
     .name {
         margin-top:50px !important;
         text-transform:bold !important;
+        color: #002D5B;    
     }
 }
 </style>
