d8e8ec45f7ba55c10dfdd60a3ee0063d613680d5
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-08 14:47:03 +0400

    qwsqe

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1968,8 +1968,7 @@
                                                 data-widget_type="heading.default">
                                                 <div class="elementor-widget-container">
                                                     <p class="elementor-heading-title elementor-size-default">
-                                                        <b>Languages </b><br><br> English, Arabic
-                                                    </p>
+                                                        <b>Languages </b><br><br> English, Arabic</p>
                                                 </div>
                                             </div>
                                         </div>
