d0625700802f4502649a99b61891597b5af07f84
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-09 12:29:10 +0400

    wsdqwewq

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6060,14 +6060,14 @@ body .elementor-32 .elementor-element.elementor-element-dd4d324 .elementor-label
     }
 
     .elementor-4878 .elementor-element.elementor-element-422d6d7 .elementor-icon-box-title {
-        margin-bottom: 0px;
+        margin-bottom: 12px;
     }
 
     .elementor-4878 .elementor-element.elementor-element-422d6d7 .elementor-icon-box-description {
         font-size: 10px;
     }
 
-    
+
 
 }
 </style>
