a9ae984dc20f594491338d5517f676bb5bce59ba
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-10 13:34:52 +0400

    wewerwr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4432,8 +4432,8 @@
                     </div>
                 </section>
                 <section
-                    class="elementor-section elementor-top-section elementor-element elementor-element-77e621d elementor-section-boxed elementor-section-height-default elementor-section-height-default wpr-particle-no wpr-jarallax-no wpr-parallax-no wpr-sticky-section-no"
-                    data-id="77e621d" data-element_type="section"
+                    class="elementor-section elementor-top-section elementor-element elementor-element-b2c5733 elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+                    data-id="b2c5733" data-element_type="section"
                     data-settings="{&quot;background_background&quot;:&quot;video&quot;,&quot;background_video_link&quot;:&quot;https:\/\/www.youtube.com\/watch?v=MDU2NsdQ9R0&amp;list=PL7RcAd6D0_T31iFRpnhZsQo1mtg1Uox_p&quot;,&quot;background_play_on_mobile&quot;:&quot;yes&quot;}">
                     <div class="elementor-background-video-container">
                         <iframe class="elementor-background-video-embed" frameborder="0" allowfullscreen=""
@@ -4442,6 +4442,7 @@
                             width="640" height="360"
                             src="https://www.youtube.com/embed/MDU2NsdQ9R0?controls=0&amp;rel=0&amp;playsinline=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwiseadvizor.com&amp;widgetid=1"
                             id="widget2" style="width: 547.556px; height: 308px;"></iframe>
+                        <!-- <div class="elementor-background-video-embed"></div> -->
                     </div>
                     <div class="elementor-container elementor-column-gap-default">
                         <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-c429127"
