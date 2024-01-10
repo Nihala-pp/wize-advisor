fd4d273f298c7faaf1ad9b059fb3a46c74966675
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-10 16:43:05 +0400

    werewrw

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -3720,7 +3720,7 @@ i.fas.fa-bell.fa-2xl {
         padding-right: 16px !important;
         padding-left: 5px !important;
         padding-top: 16px !important;
-        padding-bottom: 16px !important;
+        /* padding-bottom: 16px !important; */
         color: #54b4d3 !important;
         font-size: 12px !important;
     }
