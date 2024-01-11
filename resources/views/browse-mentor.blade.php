8140fb144f089ffced3cc43f1e16f7af3864354e
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-11 19:36:41 +0400

    awedwerw

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -3713,7 +3713,7 @@ i.fas.fa-bell.fa-2xl {
 
 @media screen and (max-width: 767px) {
     .elementor-star-rating__wrapper {
-        padding-left: 118px;
+        padding-left: 120px;
         display: flex;
     }
 
