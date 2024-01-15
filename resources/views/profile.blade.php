45d4ac799fe5b9fb26ae86c25d9d54fecdcb42e7
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-15 11:09:17 +0400

    deweq

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5435,6 +5435,8 @@
     <script
         src="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/inc/plugins/elementor/assets/js/elementor.js?ver=6.4.2"
         id="qi-addons-for-elementor-elementor-js"></script>
+    <script src="{{ asset('public/wp-content/plugins/elementor-pro/assets/js/webpack-pro.runtime.min.js?ver=3.5.0') }}"
+        id="elementor-pro-webpack-runtime-js"></script>
     <script id="wpr-addons-js-js-extra">
     var WprConfig = {
         "ajaxurl": "\/https:\/\/wiseadvizor.com\/wp-admin\/admin-ajax.php",
