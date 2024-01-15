9e968ff18a801d005bbde0662e072c0e7d9db8ab
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-15 17:30:24 +0400

    wswaqe

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4753,7 +4753,7 @@
                                                     data-widget_type="heading.default">
                                                     <div class="elementor-widget-container">
                                                         <p class="elementor-heading-title elementor-size-default">
-                                                            <a href="#how_it_works"> How it
+                                                            <a name="how_it_works"> How it
                                                                 Works</a>
                                                         </p>
                                                     </div>
