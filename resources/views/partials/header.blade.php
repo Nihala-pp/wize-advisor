757077b0c193f94b7ace6c93a334f1d4047b0acf
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-17 10:51:20 +0400

    e2q3e23

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -318,6 +318,17 @@
     <script src="{{ asset('public/assets/js/plugins/timepicker-bs4.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1HZW2R3J0M"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-1HZW2R3J0M');
+    </script>
     <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
