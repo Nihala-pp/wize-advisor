0517410c9ffa4698c458676a0869bf0243362357
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-18 19:01:45 +0400

    21w23e43

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -161,11 +161,7 @@
                             'X-CSRF-TOKEN': '{{ csrf_token() }}'
                         },
                         data: {
-                            "order_no": {
-                                {
-                                    $order_no
-                                }
-                            },
+                            "order_no": {{ $order_no }},
                             "call_id": call_id,
                             'coupon': coupon,
                             'mentor_id': mentor_id
