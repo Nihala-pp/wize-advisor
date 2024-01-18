4329b4c9e47e731b2b4bb1978612a2088ce43905
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-18 20:54:29 +0400

    QWQE

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -2917,13 +2917,15 @@
         z-index: 100000;
     }
     </style>
-    <script src="{{ asset('public/assets/js/popper.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-    <script src="{{ asset('public/assets/js/bootstrap.min.js') }}"></script>
+    <!-- <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script> -->
+    <!-- <script src="{{ asset('public/assets/js/bootstrap.min.js') }}"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
     <script type="text/javascript">
     (function($) {
