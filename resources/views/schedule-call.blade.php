321bdd8a165b5dd13b1aa05f7cfceebf66d8c8da
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-18 21:34:10 +0400

    asdsad

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -2889,7 +2889,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!-- <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.js"></script> -->
     <!-- <script src="{{ asset('public/assets/js/bootstrap.min.js') }}"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
