244129bd00c14ba3150796feedda2a660503c3bd
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-19 13:14:39 +0400

    qweqwerw

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -449,7 +449,7 @@
 
             <div class="container-fluid py-4">
                 <div class="row">
-                    <div class="col-lg-12 col-md-12 mb-md-0 mb-4">
+                    <div class="col-lg-12 col-md-12 mb-md-0 mb-2">
                         <p class="heading center"><b>Welcome, {{ auth()->user()->name }} ! </b></p>
                         <p class="center para">Book your session today and grow faster with the wisdom of mentorship.</p>
                         <p class="center para">Get 25% off on each call with code MENTOR25</p>
