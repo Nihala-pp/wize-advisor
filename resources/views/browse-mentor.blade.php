81fd5efdae37e76289a892c1acbf1c47c872bbae
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-19 18:41:18 +0400

    qweqre

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -3610,10 +3610,11 @@
         </script> -->
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js">
         </script> -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js">
-        </script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
         </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js">
+        </script>
+        
         <!-- <script src="{{ asset('public/assets/js/ct-navbar.js') }}">
         </script> -->
         <script type="text/javascript">
