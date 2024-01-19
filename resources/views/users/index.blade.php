5703e8a1cf43801e9716680c197da83e0fc0b76a
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-19 13:27:18 +0400

    aswe

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -447,7 +447,7 @@
     ======================================================-->
         <div class="row" style="background-image: url('{{ asset('public/assets/img/cover.jpg') }}')">
 
-            <div class="container-fluid">
+            <div class="container-fluid py-4 req">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 mb-md-0 mb-4">
                         <p class="heading center"><b>Welcome, {{ auth()->user()->name }} ! </b></p>
