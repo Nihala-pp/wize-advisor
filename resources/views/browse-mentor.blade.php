f749fbf906312ab1cc844c8b90a16df9caa076e4
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-19 19:42:32 +0400

    sdasfd

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -47,6 +47,9 @@
     <link rel="alternate" type="application/rss+xml" title="wiseAdvizor &raquo; Comments Feed"
         href="https://wiseadvizor.com/comments/feed/">
     <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
+        <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
 
     <!-- Google Tag Manager -->
