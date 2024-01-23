3727eb66fd6a4c5f01eec6aab001ce39db9d4301
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-23 11:37:58 +0400

    qwqewq

--- resources/views/schedule-call.blade.php
+++ resources/views/schedule-call.blade.php
@@ -1235,7 +1235,6 @@
 
 <body
     class="page-template page-template-elementor_canvas page page-id-11 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-11 payment-page">
-
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PK457VCB" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
