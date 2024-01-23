b97c90d90e9c3309d2b2d7b6f448e1928e356e4a
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-23 16:43:10 +0400

    yghyugyu

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -2171,8 +2171,9 @@
                                 <option value="DESC">DESC </option>
                             </select>
                         </div>
-
+                        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-3 filters">
                         <button class="btn btn-info">Apply</button>
+                        </div>
                     </div>
                 </div>
             </div>
