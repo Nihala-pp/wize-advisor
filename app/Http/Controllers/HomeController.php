2cdd570d656476eb63d3c21bf794ecddb0c10804
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-24 13:28:03 +0400

    wewer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -181,7 +181,7 @@ class HomeController extends Controller
         ->whereHas('metaData', function ($query) use ($filters) {
           /** @var Builder $query */
           if ($filters['sort_by'] == 'DESC') {
-            $query->orderByDesc('price_per_call');
+            $query->orderBy('price_per_call', 'desc');
           }
         })
         ->whereHas('expertise', function ($query) use ($filters) {
