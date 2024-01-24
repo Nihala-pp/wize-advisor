5c151f04548ea6b6762dd64d07b8d156749ea165
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-24 15:40:03 +0400

    qwewqew

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -2171,8 +2171,8 @@
                             <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-3 filters">
                                 <select id="sort" class="selectpicker" data-style="btn-info" name="filters[sort_by]">
                                     <option value="">Sort by Price</option>
-                                    <option value="asc">ASC</option>
-                                    <option value="desc">DESC
+                                    <option value="asc" {{ $filters[sort_by] == "asc" ? 'selected' : '' }}>ASC</option>
+                                    <option value="desc" {{ $filters[sort_by] == "desc" ? 'selected' : '' }}>DESC
                                     </option>
                                 </select>
                             </div>
