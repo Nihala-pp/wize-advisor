2bfd0df472dbde34f5224509cf12ae1327dc5a3e
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-24 17:41:43 +0400

    edwrre

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,7 +8,7 @@
     <script>
     var theplus_ajax_url = "https://wiseadvizor.com/wp-admin/admin-ajax.php";
     var theplus_ajax_post_url = "https://wiseadvizor.com/wp-admin/admin-post.php";
-    var theplus_nonce = "d4a31829e8";
+    var theplus_nonce = "e7ae24e787";
     </script>
     <meta name="robots" content="max-image-preview:large">
     <link rel="dns-prefetch" href="//fonts.googleapis.com">
