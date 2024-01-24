bbe5d197395e593a7dbae48eb07d4a3093b605c2
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-24 11:04:22 +0400

    ewre

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -2180,7 +2180,7 @@
                                 <button class="btn btn-info apply_filters">Apply</button>
                             </div>
                             <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mt-3">
-                                <a class="reset_filters">Clear Filters</a>
+                                <a class="reset_filters" href="">Clear Filters</a>
                             </div>
                         </div>
                     </form>
