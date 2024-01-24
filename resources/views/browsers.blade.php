75f2b467b628a21ef95d5692a24312be389ce62a
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-24 12:02:24 +0400

    ewrew

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -2179,7 +2179,7 @@
                             <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mt-3 filters">
                                 <button class="btn btn-info apply_filters">Apply</button>
                             </div>
-                            <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mt-3 clear_filters">
+                            <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mt-3 filters clear_filters">
                                 <a class="reset_filters" href="{{ route('browseMentor') }}">Clear Filters</a>
                             </div>
                         </div>
