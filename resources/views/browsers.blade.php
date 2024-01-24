9cb3fadcfd5511ec8996d635b9865545059471de
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-24 11:46:36 +0400

    weqdwqrew

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -2180,7 +2180,7 @@
                                 <button class="btn btn-info apply_filters">Apply</button>
                             </div>
                             <div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mt-3 clear_filters">
-                                <a class="reset_filters" href="">Clear Filters</a>
+                                <a class="reset_filters" href="{{ route('browseMentor') }}">Clear Filters</a>
                             </div>
                         </div>
                     </form>
