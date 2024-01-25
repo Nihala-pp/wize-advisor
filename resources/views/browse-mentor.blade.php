eecb6ebc749c8a3c45a79248098fa0c07359b339
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-25 16:22:36 +0400

    sdefr

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -2155,7 +2155,7 @@
                                 </select>
                             </div>
                             <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mt-3 filters datefilter">
-                                <input type="date" placeholder="FILTER BY DATE" class="form-control selectpicker"
+                                <input type="date" placeholder="FILTER BY DATE" onchange="this.className=(this.value!=''?'has-value':'')" class="form-control selectpicker"
                                     id="date" name="filters[date]">
                             </div>
                             <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12  filters name">
