a83e77086ecd800941f06b065a519179d6dc6189
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-25 18:55:47 +0400

    qewqwew

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Browse Mentor &#8211; wiseAdvizor</title>
+    <title>Browse Mentors | wiseAdvizor: Startup mentorship platform</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>
     var theplus_ajax_url = "https://wiseadvizor.com/wp-admin/admin-ajax.php";
