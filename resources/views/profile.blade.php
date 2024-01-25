45191c84d314b3aa631e7f202f64ba6ffbddb7fa
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-25 19:12:31 +0400

    wqeew

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Profile &#8211; wiseAdvizor</title>
+    <title> {{ $data->name }} - wiseAdvizor</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>
     var theplus_ajax_url = "https://wiseadvizor.com/wp-admin/admin-ajax.php";
