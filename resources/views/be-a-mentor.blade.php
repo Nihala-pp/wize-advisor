b3166c9ad459123922764631edc042efa005d8ed
nihala-pp <nihala-pp@pransainvestment.com>
2024-01-31 13:44:39 +0400

    wewrer

--- resources/views/be-a-mentor.blade.php
+++ resources/views/be-a-mentor.blade.php
@@ -593,7 +593,7 @@
                                                         data-widget_type="heading.default">
                                                         <div class="elementor-widget-container">
                                                             <p class="elementor-heading-title elementor-size-default"><a
-                                                                    href="https://wiseadvizor.com/privacy-policy/">Privacy
+                                                                    href="https://wiseadvizor.com/privacy-policy">Privacy
                                                                     Policy</a></p>
                                                         </div>
                                                     </div>
