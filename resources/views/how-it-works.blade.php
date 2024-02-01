af6fddb9ae797e3fea19ec8838ec67bb8d8fc109
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-01 16:38:22 +0400

    qwewre

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -1817,7 +1817,7 @@
                                                     class="wpr-feature-list-line"></span>
                                                 <div class="wpr-feature-list-icon-inner-wrap">
                                                     <a href="https://wiseadvizor.com/wp-content/uploads/2024/02/user.png"></a>
-                                                    <i aria-hidden="true" class="fas fa-rocket"></i>
+                                                    <!-- <i aria-hidden="true" class="fas fa-rocket"></i> -->
                                                 </div>
                                             </div>
                                             <div class="wpr-feature-list-content-wrap">
