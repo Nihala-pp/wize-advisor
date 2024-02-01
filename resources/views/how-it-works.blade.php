a9fab3959aee149955a5b7a52b1a648c4af61841
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-01 18:47:49 +0400

    wqeedwre

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -1161,6 +1161,12 @@
                                                                 class="hfe-menu-toggle sub-arrow hfe-menu-child-0"><i
                                                                     class="fa"></i></span></a></div>
                                                     <ul class="sub-menu">
+                                                        <li id="menu-item-2412" itemprop="name"
+                                                            class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
+                                                            <a href="https://wiseadvizor.com/community-post/"
+                                                                itemprop="url" class="hfe-sub-menu-item">How it Works
+                                                            </a>
+                                                        </li>
                                                         <li id="menu-item-24" itemprop="name"
                                                             class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
                                                             <a href="https://wiseadvizor.com/community-post/"
