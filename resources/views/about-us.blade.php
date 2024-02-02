331fb6ecb7b763a6feefdabf4ec325562e37a6f2
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-02 18:58:43 +0400

    asdwd

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1255,8 +1255,8 @@
                                                     <div class="hfe-has-submenu-container"><a
                                                             href="https://wiseadvizor.com/resources/" itemprop="url"
                                                             class="hfe-menu-item">Resources<span
-                                                                class="hfe-menu-toggle sub-arrow hfe-menu-child-0"><i
-                                                                    class="fa"></i></span></a></div>
+                                                                class="hfe-menu-toggle hfe-menu-child-0"><i
+                                                                    class="fas fa-chevron-down"></i></span></a></div>
                                                     <ul class="sub-menu">
                                                         <li id="menu-item-24" itemprop="name"
                                                             class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
@@ -3945,6 +3945,14 @@
 /* .browseMentor {
     padding-right: 20px;
 } */
+
+i.fas.fa-chevron-down {
+    margin-top: -15px !important;
+    padding-left: 10px !important;
+    font-size: 9px !important;
+    font-family: "Font Awesome 5 Free" !important;
+    font-weight: 900 !important;
+}
 
 i.fas.fa-bell.fa-2xl {
     font-size: 32px !important;
