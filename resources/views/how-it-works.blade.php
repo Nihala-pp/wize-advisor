a63b5c3cde7d5ae725c2d1bcea14301966578d9b
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-02 18:25:12 +0400

    wsewr

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -4084,6 +4084,7 @@
 </body>
 <style>
 i.fas.fa-chevron-down {
+    padding-left:10px !important;
     font-size: 9px !important;
     font-family: "Font Awesome 5 Free" !important;
     font-weight: 900 !important;
