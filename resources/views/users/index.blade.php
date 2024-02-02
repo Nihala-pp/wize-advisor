1f60af28035891b7e7116ffff458106e47399ca2
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-02 15:52:49 +0400

    qwewe

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -90,13 +90,12 @@
                                                 <li id="menu-item-29" itemprop="name"
                                                     class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-11 current_page_item parent hfe-creative-menu">
                                                     <a href="https://wiseadvizor.com/how-it-works" itemprop="url"
-                                                        class="hfe-sub-menu-item">How it works
-                                                    </a>
+                                                        class="hfe-menu-item">How it works</a>
                                                 </li>
                                                 <li id="menu-item-28" itemprop="name"
                                                     class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu">
-                                                    <a href="https://wiseadvizor.com/faq/" itemprop="url"
-                                                        class="hfe-sub-menu-item">FAQ'S</a>
+                                                    <a href="https://wiseadvizor.com/faq" itemprop="url"
+                                                        class="hfe-menu-item">FAQ's</a>
                                                 </li>
                                                 <li id="menu-item-27" itemprop="name"
                                                     class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children parent hfe-has-submenu hfe-creative-menu">
@@ -106,7 +105,7 @@
                                                                 class="hfe-menu-toggle sub-arrow hfe-menu-child-0"><i
                                                                     class="fa"></i></span></a></div>
                                                     <ul class="sub-menu">
-                                                        
+
                                                         <li id="menu-item-24" itemprop="name"
                                                             class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
                                                             <a href="https://wiseadvizor.com/community-posts/"
@@ -118,7 +117,7 @@
                                                             <a href="https://wiseadvizor.com/libraries/" itemprop="url"
                                                                 class="hfe-sub-menu-item">Libraries</a>
                                                         </li>
-                                                       
+
                                                         <li id="menu-item-2842" itemprop="name"
                                                             class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2650 current_page_item hfe-creative-menu">
                                                             <a href="https://wiseadvizor.com/blogs/" itemprop="url"
