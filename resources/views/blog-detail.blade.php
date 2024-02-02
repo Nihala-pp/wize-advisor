a5ab0d23df5a165c62d700e1eabf01580b5c9037
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-02 11:54:21 +0400

    wewrre

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -1747,11 +1747,6 @@
                                                             <a href="https://wiseadvizor.com/libraries/" itemprop="url"
                                                                 class="hfe-sub-menu-item">Libraries</a>
                                                         </li>
-                                                        <li id="menu-item-26" itemprop="name"
-                                                            class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
-                                                            <a href="https://wiseadvizor.com/faq/" itemprop="url"
-                                                                class="hfe-sub-menu-item">FAQ</a>
-                                                        </li>
                                                         <li id="menu-item-2842" itemprop="name"
                                                             class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
                                                             <a href="https://wiseadvizor.com/blogs/" itemprop="url"
