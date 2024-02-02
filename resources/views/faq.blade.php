59f77e73d08b0cd5759f9488e0fbc2c4e460259f
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-02 18:29:06 +0400

    sdsad

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1271,10 +1271,9 @@
                                                     <div class="hfe-has-submenu-container"><a
                                                             href="https://wiseadvizor.com/resources/" itemprop="url"
                                                             class="hfe-menu-item">Resources<span
-                                                                class="hfe-menu-toggle sub-arrow hfe-menu-child-0"><i
+                                                                class="hfe-menu-toggle hfe-menu-child-0"><i
                                                                     class="fa"></i></span></a></div>
                                                     <ul class="sub-menu">
-
                                                         <li id="menu-item-24" itemprop="name"
                                                             class="menu-item menu-item-type-post_type menu-item-object-page hfe-creative-menu">
                                                             <a href="https://wiseadvizor.com/community-posts/"
