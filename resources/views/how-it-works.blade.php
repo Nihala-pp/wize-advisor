0b2150887e7f3f7d73b3a1ea038eac4aa67824f3
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-05 16:56:44 +0400

    qweqwre

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -725,7 +725,7 @@
     <link rel="alternate" type="application/json" href="https://wiseadvizor.com/wp-json/wp/v2/pages/5225" />
     <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://wiseadvizor.com/xmlrpc.php?rsd" />
     <meta name="generator" content="WordPress 6.4.3" />
-    <link rel="canonical" href="https://wiseadvizor.com/elementor-5225/" />
+    <link rel="canonical" href="https://wiseadvizor.com/how-it-works" />
     <link rel='shortlink' href='https://wiseadvizor.com/?p=5225' />
     <link rel="alternate" type="application/json+oembed"
         href="https://wiseadvizor.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fweb.wiseadvizor.com%2Felementor-5225%2F" />
