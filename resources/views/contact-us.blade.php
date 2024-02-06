1e10a13c79fac04e16c63f4b1075a9a5837e0b9d
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-06 15:40:41 +0400

    56ty56

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Contact Us | wiseAdvizor: Startup mentorship platform</title>
+    <meta name="description" content="Community Post| Discover the easiest businesses to start and find the perfect mentor to guide your career on our website - wiseAdvizor.">
+    <meta name="keywords"
+        content="mentoring organizations, one on one mentoring">    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script>
     var theplus_ajax_url = "https://wiseadvizor.com/wp-admin/admin-ajax.php";
