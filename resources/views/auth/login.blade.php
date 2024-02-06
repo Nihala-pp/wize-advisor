b9a58acf0c0f90c6c8dc7ce5a222eb1a6751894f
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-06 13:21:02 +0400

    qwewqerw

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
 -->
 <!DOCTYPE html>
 <html lang="en">
-<title> Get Expert Business Guidance | Your Go-To Business Coach for Building a Successful Startup   </title>
+<title> Your Go-To Business Coach for Building a Successful Startup   </title>
 <meta name="description" content="Get expert mentorship & business guidance for building a startup with wiseAdvizor. Connect with a business coach today.">
 <meta name="keywords" content="business guidance, building a startup, business coach">
 @include('partials.header')
