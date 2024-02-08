0b8fd0e0fda98061a0da259b65cc1620c6c7662c
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-08 15:21:37 +0400

    qwerwer

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1954,7 +1954,17 @@
                                                 <p><span style="color: #000000">Head of marketing,
                                                         Ampverse</span><br /><span style="color: #000000">10 sessions, 5
                                                         Reviews</span></p>
-                                                <p><span style="color: #000000">ewfrefregfrtghty</span></p>
+                                                <div class="elementor-star-rating" title="5/5"
+                                                    itemtype="http://schema.org/Rating" itemscope=""
+                                                    itemprop="reviewRating">
+                                                    <i class="elementor-star-full">&#xE934;</i><i
+                                                        class="elementor-star-full">&#xE934;</i><i
+                                                        class="elementor-star-full">&#xE934;</i><i
+                                                        class="elementor-star-full">&#xE934;</i><i
+                                                        class="elementor-star-full">&#xE934;</i> <span
+                                                        itemprop="ratingValue" class="elementor-screen-only">5/5</span>
+                                                </div>
+                                                <!-- <p><span style="color: #000000">ewfrefregfrtghty</span></p> -->
                                                 <p><span style="color: #000000"><strong>Expertise:</strong></span></p>
                                                 <ul>
                                                     <li><span style="color: #000000">Marketing Campaign</span></li>
