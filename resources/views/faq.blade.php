e790809cbbd9c5fa2d03c74b6e74ae2efe5d50ed
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-13 16:55:29 +0400

    esdfefre

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -2538,7 +2538,7 @@
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Ask Question</h5>
+                                                    <h5 class="modal-title" id="exampleModalLabel">Tell Us Your Query</h5>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                         aria-label="Close"></button>
                                                 </div>
