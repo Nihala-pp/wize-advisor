b758d902bf7aed16cb73839bfc565ade79721509
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-13 14:00:02 +0400

    qweqwe

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -2528,7 +2528,7 @@
                                     <!-- Button trigger modal -->
                                     <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                         data-bs-target="#exampleModal">
-                                        Launch demo modal
+                                        Ask a Question
                                     </button>
 
                                     <!-- Modal -->
@@ -2537,7 +2537,7 @@
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                    <h5 class="modal-title" id="exampleModalLabel">Ask Question</h5>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                         aria-label="Close"></button>
                                                 </div>
