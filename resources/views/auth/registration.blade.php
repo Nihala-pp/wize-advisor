39bcad2205dc86bb23eff8365d085dbbf60a470f
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-13 19:20:41 +0400

    aSASD

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1120,7 +1120,7 @@ html body .animated {
                                     @csrf
                                     <input type="hidden" name="token" value="{{ $token ?: 'Null' }}">
                                     <input type="hidden" name="mentor_id" value="{{ $id ?: 'Null' }}">
-                                    <div class="input-group input-group-static mb-0">
+                                    <div class="input-group input-group-static mb-3">
                                         <label class="">Name</label>
                                         <input id="name" type="text"
                                             class="form-control @error('name') is-invalid @enderror" name="name"
@@ -1132,7 +1132,7 @@ html body .animated {
                                         </span>
                                         @enderror
                                     </div>
-                                    <div class="input-group input-group-static mb-0">
+                                    <div class="input-group input-group-static mb-3">
                                         <label class="">Email</label>
                                         <input id="email" type="email"
                                             class="form-control @error('email') is-invalid @enderror" name="email"
@@ -1144,7 +1144,7 @@ html body .animated {
                                         </span>
                                         @enderror
                                     </div>
-                                    <div class="input-group input-group-static mb-0">
+                                    <div class="input-group input-group-static mb-3">
                                         <label class="">Password</label>
                                         <input id="password" type="password"
                                             class="form-control @error('password') is-invalid @enderror" name="password"
@@ -1155,25 +1155,25 @@ html body .animated {
                                         </span>
                                         @enderror
                                     </div>
-                                    <div class="input-group input-group-static mb-0">
+                                    <div class="input-group input-group-static mb-3">
                                         <label class="">Confirm Password</label>
                                         <input id="password" type="password"
                                             class="form-control @error('password') is-invalid @enderror"
                                             name="password_confirmation" required autocomplete="current-password">
                                     </div>
-                                    <div class="input-group input-group-static mb-0">
+                                    <div class="input-group input-group-static mb-3">
                                         <label class="">Company Name</label>
                                         <input type="text" class="form-control" name="company_name">
                                     </div>
-                                    <!-- <div class="input-group input-group-static mb-0">
+                                    <!-- <div class="input-group input-group-static mb-3">
                                         <label class="">Designation</label>
                                         <input type="text" class="form-control" name="designation">
                                     </div>  -->
-                                    <div class="input-group input-group-static mb-0">
+                                    <div class="input-group input-group-static mb-3">
                                         <label class="">LinkedIn Url</label>
                                         <input type="url" class="form-control" name="linked_in">
                                     </div>
-                                    <div class="mb-0">
+                                    <div class="mb-3">
                                         <label class="form-label">Expertise you are looking for?</label></br>
                                         <select class="select form-control @error('expert') is-invalid @enderror"
                                             multiple data-mdb-clear-button="true" name="expert[]" required>
@@ -1182,7 +1182,7 @@ html body .animated {
                                             @endforeach
                                         </select>
                                     </div>
-                                    <div class="timezone mb-0">
+                                    <div class="timezone mb-3">
                                         <label class=""></label></br>
                                         <select name="timezone"
                                             class="select2 form-control @error('timezone') is-invalid @enderror"
@@ -1198,7 +1198,7 @@ html body .animated {
                                         </span>
                                         @enderror
                                     </div>
-                                    <div class="form-check form-check-info text-start ps-0 mt-0">
+                                    <div class="form-check form-check-info text-start ps-0 mt-3">
                                         <input class="form-check-input @error('terms_condition') is-invalid @enderror"
                                             type="checkbox" name="terms_condition" value="1" id="flexCheckDefault"
                                             required>
