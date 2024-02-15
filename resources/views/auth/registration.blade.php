9a1af9b6f3391699012386a4279c1b1f493ff759
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-15 19:13:53 +0400

    qweqewr

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -2288,11 +2288,7 @@ html body .animated {
     margin-top: -35px;
 }
 
-.image {
-    margin-left: 235px;
-    margin-bottom: 40px;
-    margin-top: 35px;
-}
+
 
 .qodef-qi-separator .qodef-m-line {
     position: relative;
