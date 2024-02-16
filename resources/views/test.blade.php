da824f64d280fbdd36f45f89ec398640c3aa3aa9
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-16 19:57:54 +0400

    qwewr

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1998,7 +1998,7 @@
             }
             </style>
             <div class="elementor-container elementor-column-gap-no">
-                <div class="row" style="display:none;">
+                <div class="row">
                     @foreach($mentors as $mentor)
                     <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-6a69efe"
                         data-id="6a69efe" data-element_type="column">
@@ -2242,7 +2242,7 @@
                     </div>
                     @endforeach
                 </div>
-                <div class="row">
+                <div class="row" style="display:none;">
                     @foreach($mentors as $mentor)
                     <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-6a69efe"
                         data-id="6a69efe" data-element_type="column">
