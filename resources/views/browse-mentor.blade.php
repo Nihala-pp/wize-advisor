56f859500f97a0e213b69fdff1da76284571b9f8
nihala-pp <nihala-pp@pransainvestment.com>
2024-02-19 17:26:35 +0400

    3er3ret

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -1100,6 +1100,10 @@
     <link rel="stylesheet" id="qi-addons-for-elementor-style-css"
         href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/main.min.css?ver=6.4.1"
         media="all">
+    <link rel='stylesheet' id='twenty-twenty-one-style-css'
+        href='https://web.wiseadvizor.com/wp-content/themes/twentytwentyone/style.css?ver=2.0' media='all' />
+    <link rel='stylesheet' id='twenty-twenty-one-print-style-css'
+        href='https://wiseadvizor.com/wp-content/themes/twentytwentytwo/assets/css/print.css?ver=2.0' media='print' />
     <link rel="stylesheet" id="theplus-front-css-css"
         href="https://wiseadvizor.com/wp-content/uploads/theplus-addons/theplus.min.css?ver=1699364619" media="all">
     <link rel="stylesheet" id="twentytwentytwo-style-css"
