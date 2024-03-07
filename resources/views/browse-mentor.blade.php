819fd98152356dfb68c116f7ae114c7920a919c6
nihala-pp <nihala-pp@pransainvestment.com>
2024-03-07 12:55:33 +0400

    ewrwre

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -4537,15 +4537,6 @@ i.fas.fa-bell.fa-2xl {
         32px !important;
 }
 
-@media screen and (min-width: 1024px) {
-    .elementor-2772 .elementor-element.elementor-element-9bcde29 {
-        transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
-        margin-top: -47px;
-        margin-bottom: 0px;
-        padding: 20px 20px 20px 275px;
-    }
-}
-
 @media screen and (max-width: 767px) {
 
     .elementor-32 .elementor-element.elementor-element-540d2d1>.elementor-widget-container {
