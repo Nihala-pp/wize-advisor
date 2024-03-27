7400f070b1bc259774388d2c9ea3f8f0db419158
nihala-pp <nihala-pp@pransainvestment.com>
2024-03-27 14:48:09 +0400

    ewqewwr

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 -->
 <!DOCTYPE html>
 <html lang="en">
-@if($token && $id && $name)
+@if(!empty($token && $id && $name))
 <title> {{$name}} - Your Mentor for Building a Successful Startup </title>
 <meta name="description"
     content="Get expert mentorship & business guidance for building a startup with wiseAdvizor. Connect with {{$name}} today.">
