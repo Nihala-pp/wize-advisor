fe6a353de31dd89361459ec7c50ce1e1d68002af
nihala-pp <nihala-pp@pransainvestment.com>
2024-03-28 13:13:50 +0400

    qwewerw

--- app/Http/Controllers/CallReminderUserController.php
+++ app/Http/Controllers/CallReminderUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Mail\callReminderUser;
 use App\Models\ScheduledCall;
+use App\Notifications\CallReminderUserDayBefore;
+use App\Notifications\CallReminderUserHourBefore;
 use Exception;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
@@ -25,6 +27,32 @@ class CallReminderUserController extends Controller
                     if ((Carbon::parse($call->start_time)->subHour()->format('H:i')) == (Carbon::now()->timezone($call->utc)->format('H:i'))) {
 
                         Mail::to($call->user->email)->send(new callReminderUser($call));
+                        $call->user->notify(new CallReminderUserHourBefore($call->mentor));
+                        Log::info('AutoCallReminderhourlyUser Successfully completed');
+                    }
+                }
+            }
+
+            return "Done!";
+        } catch (Exception $e) {
+            return "Something went wrong!";
+        }
+    }
+
+    public function userReminderDayBeforeMail()
+    {
+        try {
+            $calls = ScheduledCall::where('status', 'Approved')
+                ->whereMonth('date', date('m'))
+                ->whereDay('date', date('d'))
+                ->get();
+
+            if ($calls->count() > 0) {
+                foreach ($calls as $call) {
+                    if ((Carbon::parse($call->start_time)->subDays(1)->format('Y-m-d H:i')) == (Carbon::now()->timezone($call->utc)->format('Y-m-d H:i'))) {
+
+                        Mail::to($call->user->email)->send(new callReminderUser($call));
+                        $call->user->notify(new CallReminderUserDayBefore($call->mentor));
                         Log::info('AutoCallReminderhourlyUser Successfully completed');
                     }
                 }
---
+++ app/Notifications/CallReminderUserDayBefore.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\ScheduledCall;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class CallReminderUserDayBefore extends Notification
+{
+    use Queueable;
+
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+
+        $schedule_call = ScheduledCall::latest()->where('user_id', Auth::id())->where('mentor_id', $this->user->id)->first();
+        return [
+            'message' => "Reminder for Call with {$this->user->name} on {$schedule_call->date} at {$schedule_call->start_time}",
+            'mentor_id' => $this->user->id,
+            'user_id' =>  Auth::id(),
+        ];
+    }
+}
