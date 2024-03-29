a7947a3a5578590de0e6f0c075eaeb59d13293a1
nihala-pp <nihala-pp@pransainvestment.com>
2024-03-29 12:02:14 +0400

    awewqwr

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4218,9 +4218,9 @@
                                                     }
                                                     </style>
                                                     <div class="progress">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
-                                                            aria-valuemin="0" aria-valuemax="100" style="width:70%">
-                                                            70%
+                                                        <div class="progress-bar" role="progressbar" aria-valuenow="100"
+                                                            aria-valuemin="0" aria-valuemax="100" style="width:100%">
+                                                            {{ $exp->company_name }}
                                                         </div>
                                                     </div>
                                                 </div>
