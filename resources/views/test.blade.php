a9c548b208c916cf69449295c8f4422e467e206a
nihala-pp <nihala-pp@pransainvestment.com>
2024-04-02 13:42:30 +0400

    ddsfdsf

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1361,7 +1361,7 @@
                                             data-id="080f3cc" data-element_type="widget"
                                             data-widget_type="heading.default">
                                             <div class="elementor-widget-container">
-                                                <h5 class="elementor-heading-title elementor-size-default">Get In Touch
+                                                <h5 class="elementor-heading-title elementor-size-default">Register
                                                 </h5>
                                             </div>
                                         </div>
