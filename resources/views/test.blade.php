3d9bfede99dfeb2a7438c2083ccdfe47dc85aa9f
nihala-pp <nihala-pp@pransainvestment.com>
2024-04-02 15:12:58 +0400

    werwrtwe

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2888,9 +2888,9 @@
             <script
                 src="https://web.wiseadvizor.com/wp-content/plugins/wpforms-lite/assets/js/wpforms-modern.min.js?ver=1.8.6.4"
                 id="wpforms-modern-js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                crossorigin="anonymous"></script>
+                crossorigin="anonymous"></script> -->
             <!-- <script>
             (function() {
                 function wpforms_js_error_loading() {
