a80533a9f4094987ad08484be887fa4c2cd61e19
nihala-pp <nihala-pp@pransainvestment.com>
2024-05-01 14:54:15 +0400

    wewr

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -4354,6 +4354,20 @@ padding-left:
             50px;
     }
 
+    #date {
+        width: 220px;
+        height: 36px;
+        background-color: #54b4d3;
+        color: #fff !important;        
+        font-weight: 200px;
+        border-color: #54b4d3 !important;
+        border-radius: 5px;
+        font-size: 0.75rem !important;
+        padding-left: 20px !important;
+        padding-bottom: 20px;
+        padding-top: 0px;
+    }
+
     .butto {
         /*
 /*
