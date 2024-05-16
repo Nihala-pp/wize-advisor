cdb3506ac0fd513f62ea7ea6a1632600ae47ca48
nihala-pp <nihala-pp@pransainvestment.com>
2024-05-16 18:01:03 +0400

    erwet

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-@php header('Access-Control-Allow-Origin: *'); @endphp
-
 @include('partials.web-header-new')
-
+<link rel="stylesheet" href="{{ asset('public/assets/css/material-kit-pro.min.css') }}">
+<link rel="stylesheet" href="{{ asset('public/assets/css/material-dashboard.min.css') }}">
 <body
     class="page-template page-template-elementor_canvas page page-id-13 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-13">
     @include('partials.header-common')
