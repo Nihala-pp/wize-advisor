1a902465465be91dac34856a6f08df67b0b16f3b
nihala-pp <nihala-pp@pransainvestment.com>
2024-05-16 18:28:36 +0400

    asads

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -127,10 +127,6 @@
         </div>
     </main>
     </div>
-    <script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
-    </script>
     </script>
     <!-- </div> -->
     <!--   Core JS Files   -->
