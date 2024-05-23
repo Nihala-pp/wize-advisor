31bc249cdec350cc6eee3b9c2b3433c409f13219
nihala-pp <nihala-pp@pransainvestment.com>
2024-05-23 12:53:07 +0400

    dsfcsd

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -18,7 +18,7 @@
     <!-- CSS Files -->
     <!-- <link id="pagestyle" href="{{ asset('public/assets/css/material-dashboard.css?v=3.0.0') }}" rel="stylesheet" />  -->
     <!-- <link rel="stylesheet" href="{{ asset('public/assets/css/material-kit-pro.min.css') }}">  -->
-    <link href="{{ asset('public/assets/vendor/@fontawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('public/assets/vendor/@fontawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet"> -->
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
