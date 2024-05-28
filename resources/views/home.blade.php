3e5ea630e726a4a57970f1857954f931f961ef0e
nihala-pp <nihala-pp@pransainvestment.com>
2024-05-28 10:49:42 +0400

    qwqe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2030,13 +2030,13 @@
             href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1"
             media="all">
     </noscript>
-    <link rel="preload"
+    <!-- <link rel="preload"
         href="https://fonts.googleapis.com/css?family=Roboto%3A700%2C500%2C400&#038;subset=latin&#038;display=swap&#038;ver=3.0.2"
         as="style" id="tpgb-gfonts-global-css" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript>
+    <noscript> -->
         <link rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto%3A700%2C500%2C400&#038;subset=latin&#038;display=swap&#038;ver=3.0.2">
-    </noscript>
+    <!-- </noscript> -->
     <link rel="preload"
         href="https://wiseadvizor.com/wp-content/uploads/theplus_gutenberg/plus-global.css?ver=1686921954" as="style"
         id="plus-global-css" media="all" onload="this.onload=null;this.rel='stylesheet'">
