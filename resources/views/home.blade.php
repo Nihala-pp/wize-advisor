1e086705afb2d9edefdf7264d68fc1fc23060171
nihala-pp <nihala-pp@pransainvestment.com>
2024-05-28 11:13:25 +0400

    wewew

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2813,9 +2813,9 @@
     };
     var smoothAllowedBrowsers = [];
     </script>
-    <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" async defer id="jquery-core-js">
+    <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1"  id="jquery-core-js">
     </script>
-    <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" async defer
+    <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1"
         id="jquery-migrate-js">
     </script>
     <script
