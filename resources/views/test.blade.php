1d43b620cfacf765c2b5a27f9c136aa23a8d05e7
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-06 19:02:00 +0400

    wqewrwe

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1299,7 +1299,7 @@
                                                         display: inline-block
                                                     }
                                                     </style>
-                                                    <p>{{ Str::of(strip_tags($blog->description))->words(20, ' ....') }}
+                                                    <p>{{ Str::of(strip_tags($blog->description))->words(18, ' ....') }}
                                                     </p>
                                                 </div>
                                             </div>
