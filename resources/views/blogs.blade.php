a8522794ded595c084aac9bce3db9c0bff439a8b
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-10 19:48:38 +0400

    qwqew

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4557,7 +4557,8 @@ i.fas.fa-chevron-down {
         padding: 10px 20px;
         border-radius: 7px;
         margin: 10px;
-        color: #001E64 !important;
+        color: #fff !important;
+        background-color: #001E64;
     }
 
     .elementor-3007 .elementor-element.elementor-element-a2b9669>.elementor-element-populated {
