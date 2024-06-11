fac28b9ebcbd5f966055a007a2382d5ec55b67c6
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-11 11:24:25 +0400

    qwewqerw

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1677,7 +1677,7 @@
                         </div>
                         <section
                             class="elementor-section elementor-inner-section elementor-element elementor-element-9cbb325 elementor-section-boxed elementor-section-height-default elementor-section-height-default wpr-particle-no wpr-jarallax-no wpr-parallax-no wpr-sticky-section-no"
-                            data-id="9cbb325" data-element_type="section">
+                            data-id="9cbb325" data-element_type="section" style="margin-left: -45px;">
                             @foreach ($categories as $category)
                               <a href="{{ route('blogCategories', [ $category->id , $category->name]) }}" class="badge badge-dark">{{ $category->name }}</a>
                             @endforeach
