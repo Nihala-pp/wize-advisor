e26e061eabb7edba579c545e1626f1cfc21986fd
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-11 11:15:20 +0400

    aswqe

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4515,7 +4515,7 @@ body {
     overflow-x: hidden;
 }
 
-section.elementor-section .elementor-inner-section .elementor-element .elementor-element-9cbb325 .elementor-section-boxed .elementor-section-height-default .elementor-section-height-default .wpr-particle-no .wpr-jarallax-no .wpr-parallax-no .wpr-sticky-section-no
+.elementor-2650 .elementor-section .elementor-inner-section .elementor-element .elementor-element-9cbb325 .elementor-section-boxed .elementor-section-height-default .elementor-section-height-default .wpr-particle-no .wpr-jarallax-no .wpr-parallax-no .wpr-sticky-section-no
 {
     margin-left: -45px !important;
 }
