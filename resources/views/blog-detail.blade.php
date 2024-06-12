0ba066f896955a7a55b1dfc237ba53f61752c279
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-12 10:41:50 +0400

    wewewr

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -3389,14 +3389,14 @@ i.fas.fa-chevron-down {
         margin-bottom: -30px;
     } */
 
-    .elementor-3007 .elementor-element.elementor-element-556f326 img {
+    /* .elementor-3007 .elementor-element.elementor-element-556f326 img {
         width: 200%;
         max-width: 120%;
         height: 80px;
         object-fit: contain;
         margin-top: 7px;
         margin-bottom: -20px;
-    }
+    } */
 
     .elementor-4349 .elementor-element.elementor-element-1abaa4f .elementor-button {
         font-size: 10px;
