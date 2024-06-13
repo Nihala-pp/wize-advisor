bbef0489211f438fd2a57b0ee83c187b925aaaad
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-13 16:57:52 +0400

    qwqewq

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1659,7 +1659,7 @@
                                                     data-widget_type="heading.default">
                                                     <div class="elementor-widget-container">
                                                         <p class="elementor-heading-title elementor-size-default"><a
-                                                                href="https://wiseadvizor.com/terms-and-conditions/">Terms
+                                                                href="https://wiseadvizor.com/terms-conditions">Terms
                                                                 & Conditions</a></p>
                                                     </div>
                                                 </div>
@@ -1715,7 +1715,7 @@
                                                     data-widget_type="heading.default">
                                                     <div class="elementor-widget-container">
                                                         <p class="elementor-heading-title elementor-size-default"><a
-                                                                href="https://wiseadvizor.com/blogs/">FAQs</a>
+                                                                href="https://wiseadvizor.com/faq">FAQs</a>
                                                         </p>
                                                     </div>
                                                 </div>
@@ -1729,7 +1729,7 @@
                                                     data-widget_type="heading.default">
                                                     <div class="elementor-widget-container">
                                                         <p class="elementor-heading-title elementor-size-default"><a
-                                                                href="https://wiseadvizor.com/browse-mentor/">Browse
+                                                                href="https://wiseadvizor.com/BrowseMentors">Browse
                                                                 Mentors</a></p>
                                                     </div>
                                                 </div>
