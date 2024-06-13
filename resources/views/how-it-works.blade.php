0cff5810f1c8f88e35f8c9c565dec8cfd5ae767b
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-13 10:13:46 +0400

    weqew3

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -3395,9 +3395,9 @@
         "recaptcha_type": "v2"
     };
     </script>
-    <script
+    <!-- <script
         src="https://wiseadvizor.com/wp-content/plugins/wpforms-lite/assets/js/integrations/elementor/frontend.min.js?ver=1.8.6.4"
-        id="wpforms-elementor-js"></script>
+        id="wpforms-elementor-js"></script> -->
     <script id="elementor-admin-bar-js-before">
     var elementorAdminBarConfig = {
         "elementor_edit_page": {
