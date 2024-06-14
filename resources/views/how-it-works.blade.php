0614165b48807bf6f1e496bf31259ecac9d63982
nihala-pp <nihala-pp@pransainvestment.com>
2024-06-14 18:32:08 +0400

    wqeqeq

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -44,7 +44,7 @@
     </script>
     <meta name='robots' content='max-image-preview:large' />
     <link rel="alternate" type="application/rss+xml" title="WISE ADVIZOR &raquo; Feed"
-        href="https://wiseadvizor.com/feed/" />
+        href="https://wiseadvizor.com" />
     <link rel="alternate" type="application/rss+xml" title="WISE ADVIZOR &raquo; Comments Feed"
         href="https://wiseadvizor.com/comments/feed/" />
     <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
