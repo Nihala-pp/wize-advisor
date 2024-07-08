733ddf6607eda9534b5d0f6da76a275a8750be4e
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-08 12:25:42 +0400

    tgthgty

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -616,7 +616,7 @@
     <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://wiseadvizor.com/xmlrpc.php?rsd" />
     <meta name="generator" content="WordPress 6.4.3" />
     <link rel="canonical" href="https://wiseadvizor.com/blogs/{{ $selectedBlog->id }}/{{ $selectedBlog->name }}">
-    <link rel='shortlink' href='https://wiseadvizor.com/?p=4349' />
+    <!-- <link rel='shortlink' href='https://wiseadvizor.com/?p=4349' /> -->
     <link rel="alternate" type="application/json+oembed"
         href="https://wiseadvizor.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fhttps://wiseadvizor.com%2Ffaq%2F" />
     <link rel="alternate" type="text/xml+oembed"
