0e9978734843a5f71085cbcb9b0cdb1793b45a94
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-09 18:25:29 +0400

    qwqeqw

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1181,7 +1181,7 @@ html body .animated {
                                             </span>
                                             <input id="password" type="password"
                                                 class="form-control @error('password') is-invalid @enderror"
-                                                name="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=\S+$).{8,}$" required autocomplete="current-password">
+                                                name="password"  required autocomplete="current-password">
                                             @error('password')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -2189,6 +2189,21 @@ html body .animated {
 
     password.onchange = validatePassword;
     confirm_password.onkeyup = validatePassword;
+
+
+    var y = document.getElementById("password").value;
+if (y.length < 8) {
+    alert("Your password needs a minimum of 8 characters")
+    return false;
+}
+if (y.search(/[a-z]/) < 1) {
+    alert("Your password needs a lower case letter")
+    return false;
+}
+if (y.search(/[A-Z]/) < 1) {
+    alert("Your password needs an uppser case letter")
+    return false;
+}
     </script>
 </body>
 
