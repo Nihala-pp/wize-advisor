959f0f22c60545575d99fc37e2e91e5a6f7627c7
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-10 13:06:43 +0400

    wqqweq

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -2479,7 +2479,7 @@ html body .animated {
 
 .valid:before {
     position: relative;
-    left: -25px;
+    left: -15px;
     content: "✔";
 }
 
@@ -2490,7 +2490,7 @@ html body .animated {
 
 .invalid:before {
     position: relative;
-    left: -25px;
+    left: -15px;
     content: "✖";
 }
 </style>
