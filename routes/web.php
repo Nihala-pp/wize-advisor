efc9c1c7df198f2f4bb9534d7a4fc480144d913c
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-11 15:05:23 +0400

    qwqeq

--- routes/web.php
+++ routes/web.php
@@ -126,8 +126,8 @@ Route::post('/order/cancel',[PaymentController::class, 'cancel_order'])->name('o
 //stripe payment gateway
 
 Route::post('/checkout',[PaymentController::class, 'checkout'])->name('checkout');
-Route::post('/success',[PaymentController::class, 'success'])->name('success-test');
-Route::post('/cancel',[PaymentController::class, 'cancel'])->name('cancel');
+Route::get('/success-test',[PaymentController::class, 'success'])->name('success-test');
+Route::get('/cancel',[PaymentController::class, 'cancel'])->name('cancel');
 
 
 
