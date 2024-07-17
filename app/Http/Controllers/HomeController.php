3d98c9f0ba3273267fa56c867d7364644d19ba55
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-17 14:10:30 +0400

    wqeswqe

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -493,7 +493,7 @@ window.location.href = "https://wiseadvizor.com/be-a-mentor";
                         'product_data' => [
                             'name' => $data['duration'] .' Minute meeting with '. $mentor->name,
                         ],
-                        'unit_amount_decimal' => $price
+                        'unit_amount_decimal' => round($price)
                     ],
                     'quantity'   => 1,
                 ],
