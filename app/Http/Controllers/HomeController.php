961551da545ec109e4e07a23dd5f6df6eb89d4be
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-18 12:15:58 +0400

    wq

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -405,11 +405,10 @@ window.location.href = "https://wiseadvizor.com/be-a-mentor";
 
     if(!$voucher) 
     {
-      dd("dsdsfdg");
-      $validator->errors()->add('discount_code', 'Voucher  doesnt exists');
+      $validator->errors()->add('discount_code', 'Voucher  doesnt exists')->validate();
     }
 
-      $validator->validate();
+      // $validator->validate();
 
     if ($request->hasFile('doc')) {
       $completeFileName = $request->file('doc')->getClientOriginalName();
