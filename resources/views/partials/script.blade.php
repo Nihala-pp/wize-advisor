c5aab22f40c52d4ab602cd5fd28b7343d59c8c51
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-19 13:09:14 +0400

    qqweqewq

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1093,11 +1093,11 @@
         href="https://wiseadvizor.com/wp-content/plugins/niso-carousel-slider/assets/css/owl.carousel.min.css?ver=1.0"
         media="all">
     <link rel="stylesheet" id="niso-theme-default-css"
-        href="https://wiseadvizor.com/wp-content/plugins/niso-carousel-slider/assets/css/themes/niso.theme.default.css?ver=1.0"
+        href="https://wiseadvizor.com/wp-content/plugins/niso-carousel-slider/assets/css/themes/niso.theme.default.min.css?ver=1.0"
         media="all">
-    <!-- <link rel="stylesheet" id="niso-animate-css"
+    <link rel="stylesheet" id="niso-animate-css"
         href="https://wiseadvizor.com/wp-content/plugins/niso-carousel-slider/assets/css/animate.min.css?ver=1.3.18"
-        media="all"> -->
+        media="all">
     <link rel="stylesheet" id="niso-carousel-css"
         href="https://wiseadvizor.com/wp-content/plugins/niso-carousel-slider/assets/css/niso-carousel.css?ver=1.3.18"
         media="all">
