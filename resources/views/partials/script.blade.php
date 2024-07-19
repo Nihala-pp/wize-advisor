3d3db79736466129036d57184040ed5b014d8fb5
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-19 12:45:27 +0400

    23435435

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1126,9 +1126,9 @@
         font-stretch: normal;
     }
     </style>
-    <link rel="stylesheet" id="hfe-style-css"
+    <!-- <link rel="stylesheet" id="hfe-style-css"
         href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor.css?ver=1.6.14"
-        media="all">
+        media="all"> -->
     <link rel="stylesheet" id="elementor-icons-css"
         href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0"
         media="all">
