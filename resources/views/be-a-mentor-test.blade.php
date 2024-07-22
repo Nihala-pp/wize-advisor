0b57a5878f99c26e6b626cd954d65ee34118fc08
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-22 16:13:40 +0400

    qwewer

--- resources/views/be-a-mentor-test.blade.php
+++ resources/views/be-a-mentor-test.blade.php
@@ -607,6 +607,8 @@
         href='https://web.wiseadvizor.com/wp-content/uploads/elementor/css/global.css?ver=1719392983' media='all' />
     <link rel='stylesheet' id='elementor-post-7046-css'
         href='https://web.wiseadvizor.com/wp-content/uploads/elementor/css/post-7046.css?ver=1720441388' media='all' />
+        <link rel="stylesheet" id="elementor-post-3007-css"
+        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.css?ver=1688644136" media="all">
     <link rel='stylesheet' id='elementor-post-32-css'
         href='https://web.wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1719392983' media='all' />
     <link rel='stylesheet' id='qi-addons-for-elementor-grid-style-css'
