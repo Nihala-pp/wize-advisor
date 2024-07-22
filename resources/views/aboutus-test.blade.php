0e626596bd55d0c8c2bbb853a85afd096c040fde
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-22 13:58:13 +0400

    qewew

--- resources/views/aboutus-test.blade.php
+++ resources/views/aboutus-test.blade.php
@@ -3,12 +3,33 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>About us New design &#8211; WISE ADVIZOR</title>
+    <title>Mentorship for New Ventures: Explore Easy Start-Up Ideas</title>
+    <meta name="description"
+        content="Find the ideal mentor to kickstart your career and explore a range of easy-to-start businesses on our website. Start your entrepreneurial journey with ease.">
+    <meta name="keywords" content="easy businesses to start, i need a mentor for my career, Startup Growth">
+    <meta name="description"
+        content="wiseAdvizor: Your startup mentorship platform. Get personalized 1:1 guidance from experienced mentors. Elevate your entrepreneurial journey.">
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Book 1:1 call with top startup mentors on wiseAdvizor">
+    <meta property="og:description"
+        content="wiseAdvizor connects you with experienced startup mentors who understand your startup. Get personalized mentorship to avoid costly mistakes, unlock sustainable growth, and achieve your business goals.">
+    <meta property="og:image"
+        content="https://wiseadvizor.com/wp-content/uploads/2023/07/Book-11-call-with-world-top-mentors-on-wiseadvizor.com.webp">
+    <meta property="og:url" content="https://wiseadvizor.com">
+    <meta property="og:site_name" content="wiseAdvizor" />
+    <meta name="twitter:title" content="Schedule a 1:1 session with top-tier mentors for your startup success">
+    <meta name="twitter:description"
+        content="Connect directly with entrepreneurs and industry leaders on wiseAdvizor. Seek guidance, discuss challenges, and get expert advice to grow your startup.">
+    <meta name="twitter:url" content="https://wiseadvizor.com">
+    <meta name="twitter:image"
+        url="https://wiseadvizor.com/wp-content/uploads/2023/07/Book-11-call-with-world-top-mentors-on-wiseadvizor.com.webp">
+    <meta name="twitter:card" content="summary_large_image">
     <meta name='robots' content='max-image-preview:large' />
-    <link rel="alternate" type="application/rss+xml" title="WISE ADVIZOR &raquo; Feed"
+    <!-- <link rel="alternate" type="application/rss+xml" title="WISE ADVIZOR &raquo; Feed"
         href="https://wiseadvizor.com/feed/" />
     <link rel="alternate" type="application/rss+xml" title="WISE ADVIZOR &raquo; Comments Feed"
-        href="https://wiseadvizor.com/comments/feed/" />
+        href="https://wiseadvizor.com/comments/feed/" /> -->
     <script>
     window._wpemojiSettings = {
         "baseUrl": "https:\/\/s.w.org\/images\/core\/emoji\/14.0.0\/72x72\/",
@@ -27,6 +48,9 @@
     </noscript>
     <link rel='stylesheet' id='admin-bar-css' href='https://wiseadvizor.com/wp-includes/css/admin-bar.min.css?ver=6.4.3'
         media='all' />
+    <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
+
     <style id='admin-bar-inline-css'>
     @media screen {
         html {
@@ -3137,6 +3161,8 @@
     <script src="https://wiseadvizor.com/wp-content/plugins/elementor/assets/js/elementor-admin-bar.min.js?ver=3.21.6"
         id="elementor-admin-bar-js"></script>
     <script src="https://wiseadvizor.com/wp-includes/js/hoverintent-js.min.js?ver=2.2.1" id="hoverintent-js-js">
+    </script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
     </script>
     <script id="admin-bar-js-before">
     (function() {
