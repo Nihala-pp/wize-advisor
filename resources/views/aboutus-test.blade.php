35471e58a826a4b11e88810eaf2e3f8444b676fa
nihala-pp <nihala-pp@pransainvestment.com>
2024-07-22 14:15:18 +0400

    w433

--- resources/views/aboutus-test.blade.php
+++ resources/views/aboutus-test.blade.php
@@ -3202,6 +3202,12 @@
     position: relative;
 }
 
+.elementor-3007 .elementor-element.elementor-element-cf19fcc:not(.elementor-motion-effects-element-type-background),
+.elementor-3007 .elementor-element.elementor-element-cf19fcc>.elementor-motion-effects-container>.elementor-motion-effects-layer {
+    margin-top: -35px;
+    background-color: #FFFFFF;
+}
+
 i.fas.fa-chevron-down {
     margin-top: -15px !important;
     padding-left: 10px !important;
@@ -3244,86 +3250,86 @@ body.elementor-page-7317>.elementor-motion-effects-container>.elementor-motion-e
 
 @media (max-width: 767px) {
 
-.elementor-32 .elementor-element.elementor-element-540d2d1>.elementor-widget-container {
-    padding: 0px 20px 10px 100px;
-}
+    .elementor-32 .elementor-element.elementor-element-540d2d1>.elementor-widget-container {
+        padding: 0px 20px 10px 100px;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 .sub-menu,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown-expandible,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-menu-item,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-sub-menu-item {
-    background-color: #fff;
-    color: #001E64;
-}
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 .sub-menu,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown-expandible,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-menu-item,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-sub-menu-item {
+        background-color: #fff;
+        color: #001E64;
+    }
 
-.elementor-button.elementor-size-xs {
-    font-size: 12px;
-    padding: 10px 20px;
-    border-radius: 7px;
-    margin: 10px;
-}
+    .elementor-button.elementor-size-xs {
+        font-size: 12px;
+        padding: 10px 20px;
+        border-radius: 7px;
+        margin: 10px;
+    }
 
-.elementor-button {
-    display: inline-block;
-    line-height: 1;
-    background-color: #001E64;
-    /* font-size: 15px; */
-    /* padding: 12px 24px; */
-    /* border-radius: 3px; */
-    /* color: #fff; */
-    fill: #fff;
-    text-align: center;
-    transition: all .3s;
-}
+    .elementor-button {
+        display: inline-block;
+        line-height: 1;
+        background-color: #001E64;
+        /* font-size: 15px; */
+        /* padding: 12px 24px; */
+        /* border-radius: 3px; */
+        /* color: #fff; */
+        fill: #fff;
+        text-align: center;
+        transition: all .3s;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-f871cda {
-    width: 30%;
-}
+    .elementor-3007 .elementor-element.elementor-element-f871cda {
+        width: 30%;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-094a417 {
-    width: 50%;
-}
+    .elementor-3007 .elementor-element.elementor-element-094a417 {
+        width: 50%;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-a2b9669 {
-    width: 20%;
-}
+    .elementor-3007 .elementor-element.elementor-element-a2b9669 {
+        width: 20%;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-a2b9669>.elementor-element-populated {
-    padding: 0px 0px 0px 30px;
-    margin-top: 60px;
-}
+    .elementor-3007 .elementor-element.elementor-element-a2b9669>.elementor-element-populated {
+        padding: 0px 0px 0px 30px;
+        margin-top: 60px;
+    }
 
-#menu-item-5136 {
-    font-size: 8px;
-}
+    #menu-item-5136 {
+        font-size: 8px;
+    }
 
-i.fas.fa-bell.fa-2xl {
-    font-size: 27px !important;
-    color: #000;
-    padding-left: 10px;
-}
+    i.fas.fa-bell.fa-2xl {
+        font-size: 27px !important;
+        color: #000;
+        padding-left: 10px;
+    }
 
-.rounded-circle {
-    margin-top: 10px;
-    margin-bottom: 10px;
-    height: auto;
-    max-width: 100%;
-    border: none;
-    border-radius: 0;
-    box-shadow: none;
-}
+    .rounded-circle {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        height: auto;
+        max-width: 100%;
+        border: none;
+        border-radius: 0;
+        box-shadow: none;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 .sub-menu,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown-expandible,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-menu-item,
-.elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-sub-menu-item {
-    background-color: #fff;
-    color: #001E64 !important;
-}
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 .sub-menu,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown-expandible,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-menu-item,
+    .elementor-3007 .elementor-element.elementor-element-ee84ee1 nav.hfe-dropdown .menu-item a.hfe-sub-menu-item {
+        background-color: #fff;
+        color: #001E64 !important;
+    }
 
-/* i.fas.fa-bell.fa-2xl {
+    /* i.fas.fa-bell.fa-2xl {
 font-size: 32px !important;
 padding-left: 10px;
 color: #000;
@@ -3337,10 +3343,10 @@ margin-bottom: 10px;
 @media (max-width: 320px) {}
 
 @media (max-width: 280px) {
-.elementor-3007 .elementor-element.elementor-element-a2b9669>.elementor-element-populated {
-    padding: 0px 0px 0px 20px;
-    margin-top: 60px;
-}
+    .elementor-3007 .elementor-element.elementor-element-a2b9669>.elementor-element-populated {
+        padding: 0px 0px 0px 20px;
+        margin-top: 60px;
+    }
 }
 
 @media (max-width: 360px) {}
@@ -3350,59 +3356,60 @@ margin-bottom: 10px;
 @media (min-width: 540px) {}
 
 @media (min-width: 768px) and (max-width: 1024px) {
-.elementor-3007 .elementor-element.elementor-element-4bc8cfa>.elementor-widget-container {
-    padding: 25px 10px 0px 0px;
-    margin-left: -60px;
-}
+    .elementor-3007 .elementor-element.elementor-element-4bc8cfa>.elementor-widget-container {
+        padding: 25px 10px 0px 0px;
+        margin-left: -60px;
+    }
 
-.wp-image-624 {
-    width: 100%;
-    height: 15.02%;
-    max-width: 150px !important;
-}
+    .wp-image-624 {
+        width: 100%;
+        height: 15.02%;
+        max-width: 150px !important;
+    }
 
-/* .elementor-5225 .elementor-element.elementor-element-13dfc598 .elementor-heading-title {
+    /* .elementor-5225 .elementor-element.elementor-element-13dfc598 .elementor-heading-title {
     font-size: 40px;
     padding-left: 20px;
 } */
 
-dl,
-ol,
-ul {
-    margin-top: 0;
-    margin-bottom: 1rem;
-    margin-left: 20px;
-}
+    dl,
+    ol,
+    ul {
+        margin-top: 0;
+        margin-bottom: 1rem;
+        margin-left: 20px;
+    }
 
-.menu-item .menu-item-type-post_type .menu-item-object-page .menu-item-has-children .parent .hfe-has-submenu .hfe-creative-menu .parent-has-child {
-    background-color: #fff !important;
-}
+    .menu-item .menu-item-type-post_type .menu-item-object-page .menu-item-has-children .parent .hfe-has-submenu .hfe-creative-menu .parent-has-child {
+        background-color: #fff !important;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-78270f3 .elementor-button {
-    font-family: "Helvetica", Sans-serif;
-    font-size: 10px;
-    margin-right: 15px;
-    font-weight: 400;
-    background-color: transparent;
-    background-image: linear-gradient(90deg, var(--e-global-color-1e1892c) 0%, var(--e-global-color-1e1892c) 100%);
-    border-style: none;
-    border-radius: 7px 7px 7px 7px;
-}
+    .elementor-3007 .elementor-element.elementor-element-78270f3 .elementor-button {
+        font-family: "Helvetica", Sans-serif;
+        font-size: 10px;
+        margin-right: 15px;
+        font-weight: 400;
+        background-color: transparent;
+        background-image: linear-gradient(90deg, var(--e-global-color-1e1892c) 0%, var(--e-global-color-1e1892c) 100%);
+        border-style: none;
+        border-radius: 7px 7px 7px 7px;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-4bc8cfa .elementor-button {
-    font-family: "Helvetica", Sans-serif;
-    font-size: 10px;
-    font-weight: 400;
-    background-color: transparent;
-    background-image: linear-gradient(90deg, var(--e-global-color-1e1892c) 0%, var(--e-global-color-1e1892c) 100%);
-    border-style: none;
-    border-radius: 7px 7px 7px 7px;
-}
+    .elementor-3007 .elementor-element.elementor-element-4bc8cfa .elementor-button {
+        font-family: "Helvetica", Sans-serif;
+        font-size: 10px;
+        font-weight: 400;
+        background-color: transparent;
+        background-image: linear-gradient(90deg, var(--e-global-color-1e1892c) 0%, var(--e-global-color-1e1892c) 100%);
+        border-style: none;
+        border-radius: 7px 7px 7px 7px;
+    }
 
-.elementor-3007 .elementor-element.elementor-element-cf19fcc:not(.elementor-motion-effects-element-type-background),
-.elementor-3007 .elementor-element.elementor-element-cf19fcc>.elementor-motion-effects-container>.elementor-motion-effects-layer {
-    background-color: #FFFFFF;
-}
+    /* .elementor-3007 .elementor-element.elementor-element-cf19fcc:not(.elementor-motion-effects-element-type-background),
+    .elementor-3007 .elementor-element.elementor-element-cf19fcc>.elementor-motion-effects-container>.elementor-motion-effects-layer {
+        background-color: #FFFFFF;
+    } */
 }
 </style>
+
 </html>
