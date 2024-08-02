93197bb58bd3aac17c7b8a4f35b30e1c46ab728f
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-02 18:31:32 +0400

    qweqeq

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3394,7 +3394,7 @@
                                     <iframe loading="lazy"
                                         style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
                                         src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFpQ4GcAh0&#x2F;4eHgU5JmdwNppzpWZ4J1Ug&#x2F;watch?embed"
-                                        allowfullscreen="allowfullscreen" allow="fullscreen" autoplay="" loop="" controlslist="nodownload">
+                                        allowfullscreen="allowfullscreen" allow="fullscreen">
                                     </iframe>
                                 </div>
                             </div>
