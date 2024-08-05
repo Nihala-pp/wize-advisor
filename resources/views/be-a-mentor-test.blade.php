f456f8fac66ae66563372d1ca8f137ac8f8b3be3
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-05 12:33:25 +0400

    qwqew

--- resources/views/be-a-mentor-test.blade.php
+++ resources/views/be-a-mentor-test.blade.php
@@ -3780,6 +3780,10 @@ i.fas.fa-bell.fa-2xl {
         padding: 10px 25px 10px 25px;
     }
 
+    .elementor-7046 .elementor-element.elementor-element-735632ea2 {
+        padding: 50px 20px 30px 20px;
+    }
+
 }
 
 @media (max-width: 320px) {}
