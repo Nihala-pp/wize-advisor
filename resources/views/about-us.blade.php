db1c8cb87818b1f9d0d0989d8af570230251ea6c
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-06 12:52:50 +0400

    er3r34

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -620,7 +620,7 @@
     <style>
         #wpadminbar .theplus-purge-clear>.ab-item:before {
             content: '';
-            background-image: url(https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-elementor-page-builder//assets/images/theplus-logo-small.png) !important;
+            background-image: url("https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-elementor-page-builder/assets/images/theplus-logo-small.png") !important;
             background-size: 20px !important;
             background-position: center;
             width: 20px;
