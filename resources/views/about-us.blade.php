f41cdeec4edb5cbfde0069d4ff6e198b99842f38
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-06 12:59:24 +0400

    QWQEWRE

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -612,7 +612,7 @@
     <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://wiseadvizor.com/xmlrpc.php?rsd" />
     <meta name="generator" content="WordPress 6.4.3" />
     <link rel="canonical" href="https://wiseadvizor.com/about-us/" />
-    <link rel='shortlink' href='https://wiseadvizor.com/?p=7317' />
+    <!-- <link rel='shortlink' href='https://wiseadvizor.com/?p=7317' /> -->
     <link rel="alternate" type="application/json+oembed"
         href="https://wiseadvizor.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fweb.wiseadvizor.com%2Felementor-7317%2F" />
     <link rel="alternate" type="text/xml+oembed"
