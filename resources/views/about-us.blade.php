d6da1d810366b45508524177c749356f11c6bb72
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-07 16:09:39 +0400

    2qe23e23

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -48,7 +48,7 @@
     <!-- <link rel='stylesheet' id='admin-bar-css' href='https://wiseadvizor.com/wp-includes/css/admin-bar.min.css?ver=6.4.3'
         media='all' /> -->
     <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" async>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet">
 
     <!-- <style id='admin-bar-inline-css'>
     @media screen {
@@ -1315,7 +1315,7 @@
         id="wpr-particles-js"></script> -->
     <script
         src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/jarallax/jarallax.min.js?ver=1.12.7"
-        id="wpr-jarallax-js" async></script>
+        id="wpr-jarallax-js"></script>
     <script
         src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/parallax/parallax.min.js?ver=1.0"
         id="wpr-parallax-hover-js"></script>
