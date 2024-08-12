731c94ae775cd0dfc2b24575ca6ecd825b518d89
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-12 17:40:34 +0400

    3243

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3056,7 +3056,7 @@ li {
 .elementor-element .elementor-element-a7cb679 h3 {
     color: #150E1F;
     text-transform: capitalize !important;
-    margin-top: -75px;
+    margin-top: -55px;
 }
 
 .elementor-4878 .elementor-element.elementor-element-3f8ebce>.elementor-widget-container {
