8d598a6a6d1d9115d29651bdaaa1d1d85c4db326
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-19 17:56:12 +0400

    sdsads

--- resources/views/users/profile-test.blade.php
+++ resources/views/users/profile-test.blade.php
@@ -365,6 +365,10 @@
     </script> -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.umd.min.js">
     </script>
+    <script type="text/javascript">
+        $('select').selectpicker();
+
+    </script>
 </body>
 
 </html>
