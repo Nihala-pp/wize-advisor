8e8c7b2e5b8c529972b271503c6c87cc2f640969
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-19 18:57:34 +0400

    ASDSADFS

--- resources/views/users/profile-test.blade.php
+++ resources/views/users/profile-test.blade.php
@@ -411,9 +411,9 @@ img {
         font-size: 1rem;
         line-height: 1.5;
         color: #495057;
-        background-color: #000;
+        background-color: #000 !important;
         background-clip: padding-box;
-        border: 1px solid #ced4da;
+        /* border: 1px solid #ced4da; */
         border-radius: .25rem;
         transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
 }
