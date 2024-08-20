d66a1f07326605faeab92a738b059489a04ca689
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-20 18:00:51 +0400

    qwq

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -370,6 +370,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+    <script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
+      id="hfe-frontend-js-js"></script>
     <script type="text/javascript">
     $('select').selectpicker();
     </script>
