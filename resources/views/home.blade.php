d0b38ebde045cb4b8d855b73aa555503fcf43c9b
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-20 13:31:24 +0400

    qwewq

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <title>wiseAdvizor: Startup mentorship platform</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description"
-        content="wiseAdvizor: Your startup mentorship platform. Get personalized 1:1 guidance from experienced mentors. Elevate your entrepreneurial journey.">
+        content="wiseAdvizor: Your Startup mentorship platform. Get personalized 1:1 guidance from experienced mentors. Elevate your entrepreneurial journey.">
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Book 1:1 call with top startup mentors on wiseAdvizor">
--- resources/views/users/profile-test.blade.php
+++ resources/views/users/profile-test.blade.php
@@ -20,9 +20,9 @@
             data-id="658d30a" data-element_type="section"
             data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
             <div class="elementor-container elementor-column-gap-default">
-                <form method="POST" action="{{ route('user.save') }}" enctype="multipart/form-data">
-                    @csrf
-                    <div class="row">
+                <div class="row">
+                    <form method="POST" action="{{ route('user.save') }}" enctype="multipart/form-data">
+                        @csrf
                         <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-06004d0"
                             data-id="06004d0" data-element_type="column">
                             <div class="elementor-widget-wrap elementor-element-populated">
@@ -340,8 +340,8 @@
                                         class="fa-solid mx-1 fa-floppy-disk"></i>{{ __('Save') }}</button>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </section>
     </div>
