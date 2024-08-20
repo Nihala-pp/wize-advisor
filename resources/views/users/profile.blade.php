60b889a28eced86242383e08e08c5684b93a3caf
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-20 16:53:27 +0400

    WQEQEW

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -14,6 +14,7 @@
 
 <body
     class="page-template page-template-elementor_header_footer page page-id-8108  wp-embed-responsive ehf-footer ehf-template-twentytwentyone ehf-stylesheet-twentytwentyone qodef-qi--no-touch qi-addons-for-elementor-1.7.1 is-light-theme no-js singular elementor-default elementor-template-full-width elementor-kit-5 elementor-page elementor-page-8108">
+    @include('partials.header-common')
     <div data-elementor-type="wp-page" data-elementor-id="8108" class="elementor elementor-8108">
         <section
             class="elementor-section elementor-top-section elementor-element elementor-element-658d30a elementor-section-boxed elementor-section-height-default elementor-section-height-default wpr-particle-no wpr-jarallax-no wpr-parallax-no wpr-sticky-section-no"
