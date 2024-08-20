bc560918ad0c74110b82c1b5744dd3dae1cd784b
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-20 14:55:40 +0400

    asaDSA

--- resources/views/users/profile-test.blade.php
+++ resources/views/users/profile-test.blade.php
@@ -70,7 +70,7 @@
                                     </div>
                                 </div>
                                 <div class="elementor-elementq elementor-element-80e2a2d elementor-position-left elementor-view-default elementor-mobile-position-top elementor-vertical-align-top elementor-widget elementor-widget-icon-box"
-                                    data-id="80e2a2d" data-element_type="widget" data-widget_type="icon-box.default" style="margin-bottom:5px;">
+                                    data-id="80e2a2d" data-element_type="widget" data-widget_type="icon-box.default" style="margin-bottom:10px;">
                                     <div class="elementor-widget-container">
                                         <div class="elementor-icon-box-wrapper">
 
