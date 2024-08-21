a3c34df37c55efad7d8da6a2e2841a1e9ebb04c5
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-21 08:31:25 +0400

    wqqwqewq

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -274,7 +274,7 @@
                                     Please Enter your Name.
                                 </div>
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="validationCustom02" class="form-label">Email </label>
                                 <input type="email" name="email" class="form-control" id="validationCustom02"
                                     value="{{ $data->email ? : '' }}" required>
@@ -282,22 +282,22 @@
                                     Please Enter your Email.
                                 </div>
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="validationCustom03" class="form-label">Company Name</label>
                                 <input type="text" name="company_name" class="form-control" id="validationCustom03"
                                     value="{{ $data->metaData->company ? $data->metaData->company : '' }}">
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="validationCustom04" class="form-label">Designation</label>
                                 <input type="text" name="designation" class="form-control" id="validationCustom04"
                                     value="{{ $data->metaData->designation ? $data->metaData->designation : '' }}">
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="validationCustom05" class="form-label">Linked-In Url</label>
                                 <input type="url" name="linked_in" class="form-control" id="validationCustom05"
                                     value="{{ $data->metaData ? $data->metaData->social_linked_in : '' }}">
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="validationCustom07" class="form-label">Expertise</label>
                                 <select class="selectpicker form-control" multiple data-live-search="true"
                                     data-width="100%" id="validationCustom07" name="expert[]" required>
@@ -325,7 +325,7 @@
                                     Please choose Expertise.
                                 </div>
                             </div>
-                            <div class="mb-3">
+                            <div class="form-group mb-3">
                                 <label for="validationCustom06" class="form-label">Timezone</label>
                                 <select class="selectpicker form-control" data-live-search="true" data-width="100%"
                                     id="validationCustom06" name="timezone" required>
