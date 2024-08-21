215b6712d13d3c1f59b3d6412a24888e0566a817
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-21 09:48:03 +0400

    ewwrwe

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -74,7 +74,7 @@
                                 </div>
                                 <div class="elementor-elementq elementor-element-80e2a2d elementor-position-left elementor-view-default elementor-mobile-position-top elementor-vertical-align-top elementor-widget elementor-widget-icon-box"
                                     data-id="80e2a2d" data-element_type="widget" data-widget_type="icon-box.default"
-                                    style="margin-bottom:10px;">
+                                    style="margin-bottom:10px;margin-top:-20px !important">
                                     <div class="elementor-widget-container">
                                         <div class="elementor-icon-box-wrapper">
 
