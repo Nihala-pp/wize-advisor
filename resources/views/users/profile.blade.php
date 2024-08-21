c2a13d3449b7fa5d3a2c262716705688b2921a1b
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-21 11:17:26 +0400

    wqqwewq

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -392,6 +392,14 @@ img {
     padding-bottom: 50px;
 }
 
+.elementor-8108 .elementor-element.elementor-element-7259f9a  > .elementor-widget-container {
+    padding: 5px 0px 5px 10px;
+    border-style: solid;
+    border-width: 1px 1px 1px 1px;
+    border-color: #3B3B3B;
+    border-radius: 5px 5px 5px 5px;
+}
+
 .form-control {
     color: #fff !important;
     background-color: #000 !important;
