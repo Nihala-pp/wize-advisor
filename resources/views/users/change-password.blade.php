095c3e5d513cbb35a209413aac3a7a0ae9d8cd1a
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-22 18:19:02 +0400

    ewqewq

--- resources/views/users/change-password.blade.php
+++ resources/views/users/change-password.blade.php
@@ -78,7 +78,7 @@
     </script>
     <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"> -->
     </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>qw
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript">
     $(function() {
