2d8a03cac2ed90c7a18c41673968c5d010e78812
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-22 17:32:27 +0400

    wqewe

--- resources/views/users/change-password.blade.php
+++ resources/views/users/change-password.blade.php
@@ -79,7 +79,7 @@
     <!--   Core JS Files   -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-   <script type="text/javascript">
+    <script type="text/javascript">
     $(function() {
         validate_pass();
 
