806913c3d4054c0d7046ba67c9ab802485d1825a
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-22 13:45:51 +0400

    4343

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -351,7 +351,7 @@
                                 </div>
                                 <!-- <input class="btn btn-primary" type="submit" value="Save"> -->
                                 <div> 
-                                    <button type="submit" class="btn btn-primary">Save</button>
+                                    <a class="btn btn-primary">Save</a>
                                 </div>
                             </form>
                         </div>
