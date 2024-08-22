2361bd13fde4bc109890aeb7986fe8a3a4d28ee7
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-22 11:18:58 +0400

    sadsd

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -6,8 +6,8 @@
     <meta name='robots' content='max-image-preview:large' />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css">
+    <!-- <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css"> -->
     <link rel='stylesheet' id='elementor-preview-8222-css'
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-8108.css?ver=1723472779' media='all' />
     <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" async> -->
@@ -335,8 +335,8 @@
                                 </div>
                                 <div class="form-group col-md-12">
                                     <label for="validationCustom06" class="form-label">Timezone</label>
-                                    <select name="timezone" class="form-control timezone"
-                                        data-live-search="true" data-width="100%" id="validationCustom06" required>
+                                    <select name="timezone" class="form-control timezone" data-live-search="true"
+                                        data-width="100%" id="validationCustom06" required>
                                         <option value="">Choose Your Timezone</option>
                                         @foreach($timezone as $zone => $time)
                                         <option value="{{ $zone }}"
@@ -349,7 +349,8 @@
                                         Please choose a Timezone.
                                     </div>
                                 </div>
-                                <button type="submit" name="submitform" id="submitform" class="btn btn-primary">{{ __('Save') }}</button>
+                                <button type="submit" name="submitform" id="submitform"
+                                    class="btn btn-primary">{{ __('Save') }}</button>
                             </form>
                         </div>
                     </div>
@@ -359,16 +360,18 @@
     </section>
     </div>
     @include('partials.web-script')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src ="{{ asset('public/assets/js/mobiscroll.jquery.min.js') }}">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/js/bootstrap-select.min.js"></script>
+    </script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/js/bootstrap-select.min.js"></script> -->
     <script type="text/javascript">
     (function($) {
         $(document).ready(function() {
-            $('select').selectpicker();
+            // $('select').selectpicker();
         });
     }(jQuery));
     </script>
