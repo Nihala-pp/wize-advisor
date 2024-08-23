16073ad07f434c231af6191c5fc67eef0784f207
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-23 10:51:04 +0400

    ewqewer

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -38,7 +38,7 @@
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-7009.css?ver=1717773569' media='all' />
     <link rel='stylesheet' id='elementor-post-7012-css'
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-7012.css?ver=1717773569' media='all' />
-    <link rel="canonical" href="https://wiseadvizor.com/blogs/{{ $selectedBlog->id }}/{{ $selectedBlog->title }}">
+    <link rel="canonical" href="https://wiseadvizor.com/blogs/{{ $selectedBlog->id }}/{{ Str::slug($selectedBlog->title) }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
