dfe8d74e38fbd528b6a2ce558e53ad8d922b53f2
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-29 13:46:34 +0400

    qweqewq

--- resources/views/home-test.blade.php
+++ resources/views/home-test.blade.php
@@ -530,7 +530,9 @@
 
                                                     <div class="elementor-icon-box-icon">
                                                         <span class="elementor-icon elementor-animation-">
-                                                            <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/Browse.png" height="30px;" width="30px;" style="border-radius:70px;"></img>
+                                                            <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/Browse.png"
+                                                                height="30px;" width="30px;"
+                                                                style="border-radius:70px;"></img>
                                                             <!-- <i aria-hidden="true" class="fas fa-star"></i>  -->
                                                         </span>
                                                     </div>
@@ -564,7 +566,9 @@
 
                                                     <div class="elementor-icon-box-icon">
                                                         <span class="elementor-icon elementor-animation-">
-                                                        <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/Schedule.png" height="30px;" width="30px;" style="border-radius:70px;"></img>                                                        </span>
+                                                            <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/Schedule.png"
+                                                                height="30px;" width="30px;"
+                                                                style="border-radius:70px;"></img> </span>
                                                     </div>
 
                                                     <div class="elementor-icon-box-content">
@@ -596,7 +600,10 @@
 
                                                     <div class="elementor-icon-box-icon">
                                                         <span class="elementor-icon elementor-animation-">
-                                                            <i aria-hidden="true" class="fas fa-star"></i> </span>
+                                                            <img src="https://wiseadvizor.com/wp-content/uploads/2023/06/Meet-the-mentor.png"
+                                                                height="30px;" width="30px;"
+                                                                style="border-radius:70px;"></img> </span>
+                                                        </span>
                                                     </div>
 
                                                     <div class="elementor-icon-box-content">
