c05573cc1415d464bca6fb1e397a87966fa49262
nihala-pp <nihala-pp@pransainvestment.com>
2024-08-29 11:02:20 +0400

    qwqewq

--- resources/views/home-test.blade.php
+++ resources/views/home-test.blade.php
@@ -4026,7 +4026,7 @@ i.fas.fa-chevron-down {
     font-size: 16px;
     font-weight: 400;
     line-height: 1.5em;
-    margin-right: 50px;
+    margin-right: 50px !important;
 }
 
 .elementor-counter .elementor-counter-number-suffix {
