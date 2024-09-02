348d81d2c3426b3aa7dd4423376762dd395a4e20
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-02 14:59:40 +0400

    qweqwew

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -45,7 +45,7 @@
     <link rel="stylesheet" id="elementor-post-2772-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-2772.css?ver=1699364619" media="all">
     <link rel="stylesheet" id="twenty-twenty-one-style-css"
-        href="https://wiseadvizor.com/wp-content/themes/twentytwentyone/style.css?ver=2.0" media="all">
+        href="https://wiseadvizor.com/wp-content/themes/twentytwentyone/style.min.css?ver=2.0" media="all">
     @include('partials.web-style')
     <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js">
     </script>
