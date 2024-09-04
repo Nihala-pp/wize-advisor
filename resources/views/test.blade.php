9c8445e82efeca7fba6b9362e4222c1a1933e2ac
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-04 11:39:09 +0400

    AsaS

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2151,8 +2151,8 @@
 .elementor-8 .elementor-element.elementor-element-9a6781f:not(.elementor-motion-effects-element-type-background),
 .elementor-8 .elementor-element.elementor-element-9a6781f>.elementor-motion-effects-container>.elementor-motion-effects-layer {
     margin-top: -90px;
-    background-color: transparent;
-    background-image: linear-gradient(90deg, #000 0%, #111111 100%);
+    background-color: transparent !important;
+    background-image: linear-gradient(90deg, #000 0%, #111111 100%) !important;
 }
 
 .elementor-column .elementor-col-60 .elementor-top-column .elementor-element .elementor-element-a037b03 {
