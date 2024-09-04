18b4fc471319e52f8c9ecced269431a459ca1ed6
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-04 09:34:43 +0400

    4r45r54

--- resources/views/partials/web-header-new.blade.php
+++ resources/views/partials/web-header-new.blade.php
@@ -1330,7 +1330,7 @@
     id="hfe-frontend-js-js"></script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/webpack.runtime.min.js?ver=3.14.1') }}"
     id="elementor-webpack-runtime-js"></script>
-<script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=3.14.1') }}"
+<script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=3.14.1') }}" defer
     id="elementor-frontend-modules-js"></script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2') }}"
     id="elementor-waypoints-js"></script>
