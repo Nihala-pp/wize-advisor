d83d82a637f958a9c8ee7e9ae8df7aa2fe69cce1
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-05 12:06:33 +0400

    asadwd

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1836,7 +1836,7 @@
     .elementor-32 .elementor-element.elementor-element-60f0ea5:not(.elementor-motion-effects-element-type-background),
     .elementor-32 .elementor-element.elementor-element-60f0ea5>.elementor-motion-effects-container>.elementor-motion-effects-layer {
         background-color: #000 !important;
-        /* background-image: linear-gradient(90deg, #FFDE59 0%, #FF66C4 100%); */
+        background-image: linear-gradient(90deg, #000 0%, #000 100%);
     }
 
     .elementor-8 .elementor-element.elementor-element-ba90b25:not(.elementor-motion-effects-element-type-background),
