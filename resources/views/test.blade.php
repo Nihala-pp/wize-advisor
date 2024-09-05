433b5ab7876717d2809f87893e8fbaab7ade5ff8
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-05 10:57:03 +0400

    weqwewq

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1758,7 +1758,7 @@
         <section
             class="elementor-section elementor-top-section elementor-element elementor-element-9a6781g elementor-section-content-middle elementor-section-height-min-height elementor-section-boxed elementor-section-height-default elementor-section-items-middle"
             data-id="9a6781g" data-element_type="section"
-            data-settings="{&quot;background_background&quot;:&quot;gradient&quot;}">
+            data-settings="{&quot;background_background&quot;:&quot;gradient&quot;}" style="background-color:#000;">
             <div class="elementor-container elementor-column-gap-no" style="max-width:1500px;">
                 <div class="elementor-column elementor-col-60 elementor-top-column elementor-element elementor-element-a037b04"
                     data-id="a037b04" data-element_type="column">
