6692f652922a6fef1dc00787ad613eba37ce88d6
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-09 15:41:24 +0400

    qwqweqw

--- resources/views/users/dashboard-test.blade.php
+++ resources/views/users/dashboard-test.blade.php
@@ -703,10 +703,10 @@
                             data-id="7f40e2c" data-element_type="section"
                             data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
                             <div class="elementor-container elementor-column-gap-default">
+                            @foreach($suggested_mentors as $suggested_mentor)
                                 <div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-3297377"
                                     data-id="3297377" data-element_type="column">
                                     <div class="elementor-widget-wrap elementor-element-populated">
-                                    @foreach($suggested_mentors as $suggested_mentor)
                                         <div class="elementor-element elementor-element-89a31e1 elementor-widget__width-initial elementor-position-top elementor-widget elementor-widget-image-box"
                                             data-id="89a31e1" data-element_type="widget"
                                             data-widget_type="image-box.default">
