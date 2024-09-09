c42859e6ca43ee05c45f8cff5a7a97ddd11eb207
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-09 18:35:06 +0400

    ewqew

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -406,6 +406,7 @@ alert("Password Updated Successfully!");
     }
 
     $referral_link = "https://wiseadvizor.com/registration/".[auth()->user()->name]."/".["Discount35"]."/".["Startup-mentorship-platform"];
+    dd($referral_link);
     
     if (auth()->user()->role_id == 3 && auth()->user()->metaData) {
        return view('users.dashboard-test', compact('upcoming_sessions', 'completed_sessions', 'requested_sessions', 'notifications', 'suggested_mentors', 'referral_link'));
