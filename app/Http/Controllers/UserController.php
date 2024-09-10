2804fb59d1c87ef72cf5301bb6aa190431f5a337
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-10 10:48:51 +0400

    q2weqe

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -413,7 +413,7 @@ alert("Password Updated Successfully!");
 
     $user->update([      
       'referral_code' => $discount_code,
-      'referral_discount_value' => 35
+      'referral_discount_value' => 45
     ]);
 
     // $discount_code = $this->discount_code_generator($length = 6);
