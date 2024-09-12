03bd284527de65c1f2214cfc82a98cee4c578ab0
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-12 17:08:02 +0400

    qwqewq

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -33,7 +33,6 @@
     <link rel="canonical" href="https://wiseadvizor.com/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 </head>
-
 <body
     class="page-template page-template-elementor_canvas page page-id-8 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-8">
 
