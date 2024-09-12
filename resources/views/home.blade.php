f7678599a15fb3c949603cc96239bedb5cc02fd6
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-12 14:23:19 +0400

    asdadsa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2956,7 +2956,7 @@ i.fas.fa-chevron-down {
         font-family: "Helvetica", Sans-serif;
         font-weight: 400;
         font-size: 12px !important;
-        margin-top: -35px;
+        margin-top: -40px;
         margin-left: 145px;
         /* margin-right: 20px; */
     }
