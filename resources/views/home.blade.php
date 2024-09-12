1162714812377f5fd44dd11c34869d4029f006b1
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-12 13:43:00 +0400

    23eeee2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2449,7 +2449,7 @@ i.fas.fa-chevron-down {
 }
 
 .elementor-8 .elementor-element.elementor-element-babd5b5>.elementor-background-overlay {
-    background-image: url("https://web.wiseadvizor.com/wp-content/uploads/2024/06/cubes.png");
+    background-image: url("https://wiseadvizor.com/wp-content/uploads/2024/06/cubes.png");
     background-position: center center;
     background-repeat: no-repeat;
     background-size: cover;
