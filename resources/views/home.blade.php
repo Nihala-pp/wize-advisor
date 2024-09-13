7df5a82e0c5bd692072d1abcb48d907a3ccc3779
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-13 18:08:12 +0400

    ttyhyt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1913,10 +1913,10 @@
     </div>
     <!-- <link rel="preload" fetchpriority="high" as="image"
         href="https://wiseadvizor.com/wp-content/uploads/2024/07/ain-14-unscreen-2.webp" type="image/webp" crossorigin> -->
-    <link rel="stylesheet" id="elementor-post-30-css"
-        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-30.css?ver=1699609171" disabled>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet"
+    <link rel="preconnect"  id="elementor-post-30-css"
+        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-30.css?ver=1699609171">
+    <link rel="preconnect"  href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="preconnect"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
     @include('partials.web-style')
     @include('partials.footer-web')
