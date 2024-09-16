1140b63840390d6c58213d9a916df83c4f1e8011
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-16 17:28:02 +0400

    qwqwq

--- resources/views/partials/web-style.blade.php
+++ resources/views/partials/web-style.blade.php
@@ -363,7 +363,7 @@ html :where(.is-position-sticky) {
 </noscript>
 <link rel="preconnect"
     href="https://fonts.googleapis.com/css?family=Roboto%3A700%2C500%2C400&#038;subset=latin&#038;&display=swap&#038;ver=3.0.2"
-    as="font" type="font/woff2" async crossorigin>
+    as="font" type="font/woff2" disabled crossorigin>
 <link rel="preload" href="https://wiseadvizor.com/wp-content/uploads/theplus_gutenberg/plus-global.css?ver=1686921954"
     as="style" id="plus-global-css" media="all" onload="this.onload=null;this.rel='stylesheet'" crossorigin>
 <noscript>
@@ -1095,7 +1095,7 @@ h6 {
     href="https://wiseadvizor.com/wp-content/themes/twentytwentytwo/style.min.css?ver=1.4" defer>
 <link rel="preconnect"
     href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CSail%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&#038;display=swap&#038;ver=6.4.1"
-    as="font" type="font/woff2" id="google-fonts-1-css" crossorigin async>
+    as="font" type="font/woff2" id="google-fonts-1-css" crossorigin disabled>
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
     as="style" id="elementor-icons-shared-0-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin>
