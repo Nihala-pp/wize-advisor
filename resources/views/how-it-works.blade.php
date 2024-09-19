3c4038b611c2a1f605c52232c5977f0c768ddecf
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-19 12:15:08 +0400

    qwqwewq

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -26,8 +26,8 @@
     <meta name='robots' content='max-image-preview:large' />
     <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
     <script data-cfasync="false" src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1"
-        id="jquery-core-js" async></script>
-    <script data-cfasync="false" src="https://wiseadvizor.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1"
+        id="jquery-core-js"></script>
+    <script async data-cfasync="false" src="https://wiseadvizor.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1"
         id="jquery-migrate-js"></script>
     <link rel="canonical" href="https://wiseadvizor.com/how-it-works" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
