2a3d57733f5dd5fcb738801d04c5d236c7109180
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-19 13:50:40 +0400

    wewqewq

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -499,7 +499,7 @@
                                                             <p class="exp-heading"><span
                                                                     style="color: #000000"><strong>Expertise:</strong></span>
                                                             </p>
-                                                            <ul class="list-expert" style="margin-top: -15px;">
+                                                            <ul class="list-expert">
                                                                 @php
                                                                 $expertise =
                                                                 \App\Models\Expertise::where('mentor_id',$mentor->id)->take(4)->get();
