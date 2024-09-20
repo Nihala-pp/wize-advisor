62a4fa1c44393a6c8d49770cccba0cf1be7a390f
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-20 16:51:50 +0400

    wewqe

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1901,10 +1901,9 @@ i.fas.fa-bell.fa-2xl {
         // });
 
         $('#shareWithMail').click(function() {
-            var formattedBody = "This is cause link: " + (copiedLink);
-            var mailToLink = "mailto:?subject= " + user +
-                " wants you to donate to this noble cause&body=" +
-                encodeURIComponent(formattedBody);
+            var formattedBody =
+                "Sign up on wiseAdvizor with this link and get 35% off your first call with a mentor! 🎉  Connect 1: 1 with startup mentors, founders, and industry leaders for personalized guidance. Don 't miss out—Sign up now!: " + (copiedLink);
+            var mailToLink = "mailto:?subject= " + user + encodeURIComponent(formattedBody);
             window.location.href = mailToLink;
         });
 
