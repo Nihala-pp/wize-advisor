6315e56aaeef1e4b96d37d73c54ed5ac1c40cc75
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-20 12:48:14 +0400

    wewer

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -418,7 +418,7 @@
             </div>
         </section>
     </div>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel='stylesheet' id='elementor-post-7317-css'
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-7317.css?ver=1720441384' media='all' />
     <link rel="icon" type="image/png" href="{{ asset(path: 'public/assets/img/PNG-Blue.png') }}">
@@ -426,7 +426,7 @@
     @include('partials.web-style')
 
     @include('partials.footer-web')
-    <script src="cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     @include('partials.web-script')
 </body>
 <style>
