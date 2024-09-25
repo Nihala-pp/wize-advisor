26b5cd88a23641deb76abe33c461ae3b0b13967b
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-25 17:10:06 +0400

    343543

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,12 +37,12 @@
     <link rel='preload' id='elementor-post-3007-css'
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.css?ver=1706786161' as="style"
         onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload" id="elementor-post-32-css"
+    <!-- <link rel="preload" id="elementor-post-32-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous"> -->
+    <!-- <link rel="preload"
         href="https://wiseadvizor.com/public/wp-content/plugins/elementor/assets/js/frontend-modules.min.js" as="script"
-        crossorigin>
+        crossorigin> -->
     <!-- <link rel="preload" href="https://wiseadvizor.com/js/main.min.js?ver=6.2.2" as="script" crossorigin> -->
     <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js" as="script"
         crossorigin>
