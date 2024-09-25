43c1f0ba594e4b6946dfd73140a3b45cd29f8f09
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-25 10:31:37 +0400

    3435

--- resources/views/partials/web-style.blade.php
+++ resources/views/partials/web-style.blade.php
@@ -1124,6 +1124,7 @@ h6 {
 <link rel="preload" href="https://wiseadvizor.com/public/wp-content/plugins/qi-addons-for-elementor/inc/plugins/elementor/assets/js/elementor.min.js?ver=6.2.2" as="script" crossorigin="anonymous">
 <link rel="preload" href="https://wiseadvizor.com/public/wp-content/plugins/slide-anything/lightgallery/plugins/zoom/lg-zoom.min.js?ver=2.5.0" as="script" crossorigin="anonymous">
 <link rel="preload" href="https://wiseadvizor.com/public/wp-content/plugins/unlimited-elements-for-elementor/assets_libraries/owl-carousel-new/owl.carousel.min.js?ver=1.5.107" as="script" crossorigin="anonymous">
+<link rel="preload" href="https://wiseadvizor.com/public/wp-content/uploads/theplus-addons/theplus.min.js?ver=1688661372" as="script" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <style id="skip-link-styles">
 .skip-link.screen-reader-text {
