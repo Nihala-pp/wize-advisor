26c3f15b3302c6e266960d56adafd510d797bf07
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-25 12:54:42 +0400

    werewr

--- resources/views/partials/web-style.blade.php
+++ resources/views/partials/web-style.blade.php
@@ -1099,11 +1099,11 @@ h6 {
     <link rel="stylesheet"
         href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3">
 </noscript>
-<link rel="preload" id="elementor-icons-fa-solid-css"
+<link rel="stylesheet" id="elementor-icons-fa-solid-css"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/solid.min.css?ver=5.15.3" as="style"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload"  id="elementor-icons-fa-regular-css"
+<link rel="stylesheet"  id="elementor-icons-fa-regular-css"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/regular.min.css?ver=5.15.3" as="style"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload" id="elementor-icons-fa-brands-css"
+<link rel="stylesheet" id="elementor-icons-fa-brands-css"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/brands.min.css?ver=5.15.3" as="style"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" id="wpr-text-animations-css-css"
     href="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/css/lib/animations/text-animations.min.css?ver=1.3.85" as="style"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
