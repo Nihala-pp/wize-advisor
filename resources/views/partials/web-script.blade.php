a0bac18c580bed23856fc89546c2b6519639baa0
nihala-pp <nihala-pp@pransainvestment.com>
2024-09-27 12:44:06 +0400

    ererte

--- resources/views/partials/web-script.blade.php
+++ resources/views/partials/web-script.blade.php
@@ -48,7 +48,7 @@ gtag('config', 'G-QC47PPTD6T');
     sibling.parentElement.insertBefore(skipLink, sibling);
 }());
 </script>
-<script src="{{ asset('public/wp-content/plugins/niso-carousel-slider/assets/js/owl.carousel.min.js?ver=1.0') }}"
+<script async src="{{ asset('public/wp-content/plugins/niso-carousel-slider/assets/js/owl.carousel.min.js?ver=1.0') }}"
     id="niso-carousel-owl.min-js"></script>
 <script src="{{ asset('public/wp-content/plugins/niso-carousel-slider/assets/js/jquery.mousewheel.min.js?ver=1.0') }}"
     id="jquery.mousewheel.min-js"></script>
