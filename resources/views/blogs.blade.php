9e91ef30163defed5f90f6994f9ed5eb47c2bd20
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-01 18:39:44 +0400

    wqewqew

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -44,8 +44,6 @@
     <link rel="preload" id="elementor-post-32-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
         onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel='stylesheet' id='elementor-post-7005-css'
-        href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-7005.css?ver=1717773569' media='all' />
     <link rel="preload" fetchpriority="high" as="image"
         href="https://wiseadvizor.com/public/wp-content/uploads/2024/06/1721978543.webp" type="image/webp">
 
@@ -622,6 +620,8 @@
     <link rel="stylesheet" as="style" id="elementor-icons-shared-0-css"
         href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
         onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel='stylesheet' id='elementor-post-7005-css'
+        href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-7005.css?ver=1717773569' media='all' />
     <link rel='stylesheet' id='elementor-post-2651-css'
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-2651.css?ver=1717513898' media='all' />
     <link rel='stylesheet' id='elementor-post-7009-css'
