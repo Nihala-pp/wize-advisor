391f0e15406ba9f9d76251f3c40cb64b1cb2021b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-01 16:24:50 +0400

    wewe

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -500,7 +500,7 @@
                                     <img loading="lazy" decoding="async" width="750" height="375"
                                         src="{{ asset('public/wp-content/uploads/2024/06/') }}/{{ $blog->image }}"
                                         class="attachment-large size-large wp-image-6398"
-                                        alt="digital marketing strategies"
+                                        alt="business development strategies"
                                         srcset="{{ asset('public/wp-content/uploads/2024/06/') }}/{{ $blog->image }} 1024w, {{ asset('public/wp-content/uploads/2024/06/') }}/{{ $blog->image }} 300w, {{ asset('public/wp-content/uploads/2024/06/') }}/{{ $blog->image }} 768w, {{ asset('public/wp-content/uploads/2024/06/') }}/{{ $blog->image }} 1300w"
                                         sizes="(max-width: 750px) 100vw, 750px"
                                         style="width:100%;height:50%;max-width:2400px" />
