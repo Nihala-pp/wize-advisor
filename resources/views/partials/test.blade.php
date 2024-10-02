18505c6f1c7159f404ebf8719ca06b20bd089da7
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-02 11:45:58 +0400

    reetr

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -40,9 +40,6 @@
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
     as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload" as="style" id="twentytwentytwo-style-css"
-    href="https://wiseadvizor.com/wp-content/themes/twentytwentytwo/style.min.css?ver=1.4"
-    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" as="style"
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" as="style" id="owl_carousel_css-css"
