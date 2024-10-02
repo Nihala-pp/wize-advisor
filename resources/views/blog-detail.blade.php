c3da154cef48d6e87bd0ee7c5d148b397d65af29
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-02 11:24:31 +0400

    wew4343

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -468,9 +468,6 @@
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-7012.css?ver=1717773569' media='all' />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- @include('partials.web-style') -->
-
     @include('partials.footer-web')
 
     <script type="text/javascript">
