d3ba0c3f17e4504c54520a099431004b9f4df774
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 12:24:55 +0400

    wrere

--- resources/views/partials/test-script.blade.php
+++ resources/views/partials/test-script.blade.php
@@ -85,10 +85,7 @@ var qodefQiAddonsGlobal = {
 </script> -->
 <!-- <script  async src="{{ asset('public/wp-includes/js/jquery/ui/slider.min.js?ver=1.13.2') }}" id="jquery-ui-slider-js">
 </script> -->
-<script defer src="{{ asset('public/wp-content/uploads/theplus-addons/theplus.min.js?ver=1688661372') }}"
-        id="theplus-front-js-js" ></script>
-<script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
-    id="hfe-frontend-js-js"></script>
+<!-- w -->
 <script defer
     src="{{ asset('public/wp-content/plugins/qi-addons-for-elementor/assets/plugins/swiper/swiper.min.js?ver=6.2.2') }}"
     id="swiper-js"></script>
