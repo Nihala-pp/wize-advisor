c0afa0495a26e88fbb47d4d3a8636b1d6b173819
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 12:57:22 +0400

    qwqewq

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -593,50 +593,12 @@
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-2772.css?ver=1699364619" media="all">
     <link rel="stylesheet" id="twenty-twenty-one-style-css"
         href="https://wiseadvizor.com/wp-content/themes/twentytwentyone/style.min.css?ver=2.0" media="all">
-
-    <link rel="preload" as="style" id="hfe-style-css"
-        href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor.min.css?ver=1.6.14"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin>
-    <link rel="preload" as="style"
-        href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
-        id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload"
-        href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
-        as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link async rel="preload" as="style"
-        href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.14.1"
-        id="elementor-frontend-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel='preload' as="style" id='elementor-pro-css'
-        href="https://wiseadvizor.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.8.2"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel='preload' id='elementor-post-3007-css'
-        href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.css?ver=1706786161' as="style"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload" id="elementor-post-32-css"
-        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="stylesheet" as="style" id="elementor-icons-css"
-        href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0">
-    <link rel="stylesheet" as="style" id="elementor-icons-shared-0-css"
-        href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload" as="style" id="elementor-post-5-css"
-        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-5.css?ver=1699355418"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload" as="style"
-        href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
-        id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload"
-        href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
-        as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link rel="preload" as="style"
-        href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1"
-        id="tpgb-fontawesome" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" async>
     <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js">
     </script>
 
     @include('partials.footer-web')
+
+    @include('partials.test')
 
     @include('partials.test-script')
 
@@ -1750,5 +1712,4 @@ i.fas.fa-bell.fa-2xl {
 
 @media (min-width: 768px) and (max-width: 1024px) {}
 </style>
-
 </html>
