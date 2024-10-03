fc0f2856f65f77f53123a4c5df5910b89630a625
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 12:39:41 +0400

    3243243

--- resources/views/partials/test-script.blade.php
+++ resources/views/partials/test-script.blade.php
@@ -16,9 +16,9 @@ gtag('config', 'G-QC47PPTD6T');
 <script defer
     src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/jarallax/jarallax.min.js?ver=1.12.7"
     id="wpr-jarallax-js"></script> -->
-<script defer
+<!-- <script defer
     src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/parallax/parallax.min.js?ver=1.0"
-    id="wpr-parallax-hover-js"></script>
+    id="wpr-parallax-hover-js"></script> -->
 <script async src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/frontend.min.js?ver=1.3.85"
      id="wpr-addons-js-js"></script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/webpack.runtime.min.js') }}">
