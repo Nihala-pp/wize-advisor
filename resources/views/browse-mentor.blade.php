ae9c3229c371f5e7b3aa4d48f6cabdaac82cf3df
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 11:22:59 +0400

    wqewqew

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -601,7 +601,7 @@
 
     @include('partials.footer-web')
 
-    @include('partials.web-script')
+    <!-- @include('partials.web-script') -->
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
