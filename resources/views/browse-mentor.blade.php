2da40124c1564f98b3053f15566e9589a9ebe6f6
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 17:19:11 +0400

    tretr

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -587,7 +587,6 @@
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet">
     <link rel="stylesheet" id="elementor-post-2772-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-2772.css?ver=1699364619" media="all">
     <link rel="stylesheet" id="twenty-twenty-one-style-css"
