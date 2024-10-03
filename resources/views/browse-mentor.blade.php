236a1c26c2355bf808b63f5b26ed2eb016b832b4
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 10:53:39 +0400

    343534

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -14,7 +14,7 @@
     @endif
     <meta name="keywords"
         content="Find a mentor, entrepreneur advice, mentorship program, growth mentor, business tips">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Book 1:1 call with top startup mentors on wiseAdvizor">
