4f3779a3f0cd561182a44ed55db02d6e9d4f206f
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 12:39:17 +0400

    tetre

--- resources/views/partials/test-script.blade.php
+++ resources/views/partials/test-script.blade.php
@@ -10,12 +10,12 @@ gtag('js', new Date());
 
 gtag('config', 'G-QC47PPTD6T');
 </script>
-<script
+<!-- <script
     src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/particles/particles.min.js?ver=3.0.6"
     id="wpr-particles-js"></script>
 <script defer
     src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/jarallax/jarallax.min.js?ver=1.12.7"
-    id="wpr-jarallax-js"></script>
+    id="wpr-jarallax-js"></script> -->
 <script defer
     src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/lib/parallax/parallax.min.js?ver=1.0"
     id="wpr-parallax-hover-js"></script>
