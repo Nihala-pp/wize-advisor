5b6803fcc60763d27faee2e517cfd348de977043
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-03 12:47:36 +0400

    234343

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -14,7 +14,7 @@
     @endif
     <meta name="keywords"
         content="Find a mentor, entrepreneur advice, mentorship program, growth mentor, business tips">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Book 1:1 call with top startup mentors on wiseAdvizor">
@@ -594,15 +594,52 @@
     <link rel="stylesheet" id="twenty-twenty-one-style-css"
         href="https://wiseadvizor.com/wp-content/themes/twentytwentyone/style.min.css?ver=2.0" media="all">
 
-    @include('partials.test')
-
+    <link rel="preload" as="style" id="hfe-style-css"
+        href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor.min.css?ver=1.6.14"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin>
+    <link rel="preload" as="style"
+        href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
+        id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload"
+        href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
+        as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link async rel="preload" as="style"
+        href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.14.1"
+        id="elementor-frontend-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel='preload' as="style" id='elementor-pro-css'
+        href="https://wiseadvizor.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.8.2"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel='preload' id='elementor-post-3007-css'
+        href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.css?ver=1706786161' as="style"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" id="elementor-post-32-css"
+        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="stylesheet" as="style" id="elementor-icons-css"
+        href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0">
+    <link rel="stylesheet" as="style" id="elementor-icons-shared-0-css"
+        href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="elementor-post-5-css"
+        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-5.css?ver=1699355418"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style"
+        href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
+        id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload"
+        href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
+        as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style"
+        href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1"
+        id="tpgb-fontawesome" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" async>
     <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js">
     </script>
 
     @include('partials.footer-web')
 
     @include('partials.test-script')
-    
+
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
@@ -1263,7 +1300,7 @@ i.fas.fa-bell.fa-2xl {
     body {
         max-width: 100% !important;
         overflow-x: hidden !important;
-    } 
+    }
 
     .grid-child p {
         margin-bottom: -5px;
@@ -1713,4 +1750,5 @@ i.fas.fa-bell.fa-2xl {
 
 @media (min-width: 768px) and (max-width: 1024px) {}
 </style>
+
 </html>
