8c1ce1964a47dda412d73e619694b96fc0f1ce3b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-07 16:45:41 +0400

    34343

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -639,7 +639,7 @@
         </section>
     </div>
     <link rel="preconnect" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js" crossorigin="anonymous">
-    <link rel="preconnect" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" crossorigin="anonymous">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
     <link rel="stylesheet" id="elementor-post-2772-css"
