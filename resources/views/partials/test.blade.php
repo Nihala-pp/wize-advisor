875ea016b8e53f828c91b57aab407ee42452bda5
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-07 16:55:03 +0400

    23243

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -36,4 +36,4 @@
 <link rel="preload" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1"
     id="tpgb-fontawesome" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preconnect" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css">
