d7f2336cc2f01375ac6445ff4fb69053a514c8ca
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-07 19:13:04 +0400

    retr

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -955,10 +955,10 @@ div.d-grid.gap-2.col-6.butto {
 #date {
     width: 220px;
     height: 36px;
-    background-color: #000;
+    background-color: #000 !important;
     color: #fff !important;
     font-weight: 200px;
-    border-color: #54b4d3 !important;
+    border-color: #000 !important;
     border-radius: 5px;
     font-size: 0.75rem !important;
     padding-left: 20px !important;
