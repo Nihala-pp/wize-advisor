90c75b08dce7d0da631f64c81d11e3920d1d894b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-07 17:04:58 +0400

    232432

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -43,7 +43,7 @@
     <link rel="icon" type="image/png" href="{{ asset('public/assets/img/PNG-Blue.png') }}">
     <link rel="preload" fetchpriority="high" as="image"
         href="https://wiseadvizor.com/wp-content/uploads/2024/02/{{ $mentors[0]['metaData']['pro_pic_1'] }}" type="image/webp">
-    <link rel="preload" fetchpriority="high" as="image"
+    <link rel="preload" fetchpriority="low" as="image"
         href="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-768x115.webp" type="image/webp">
     <link rel="preload" href="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"
         as="script">
