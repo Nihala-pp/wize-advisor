08d078c0e7538ff913d4ce4ea92cb08ddea02ebb
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 15:51:02 +0400

    454354

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1872,7 +1872,8 @@
                     </div>
                 </div>
         </section>
-        <div class="modal" id="offer_modal" role="dialog" aria-labelledby="offer_modal_label" aria-modal="true" style="display:none">
+        <div class="modal" id="offer_modal" role="dialog" aria-labelledby="offer_modal_label" aria-modal="true"
+            style="display:none">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -1907,7 +1908,44 @@
     <link rel="preload" as="style"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css"
         onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-    @include('partials.home-test')
+    <link rel="preload" as="style" id="elementor-global-css"
+        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/global.css?ver=1699355419"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="elementor-post-8-css"
+        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-8.css?ver=1699364637"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="qi-addons-for-elementor-helper-parts-style-css"
+        href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/helper-parts.min.css?ver=6.4.1"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="qi-addons-for-elementor-style-css"
+        href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/main.min.css?ver=6.4.1"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" href="https://wiseadvizor.com/wp-content/uploads/theplus-addons/theplus.min.css?ver=1699364637"
+        as="style" id="theplus-front-css-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload"
+        href="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/css/frontend.min.css?ver=1.3.85"
+        as="style" id="wpr-addons-css-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" as="style"
+        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="owl_carousel_css-css"
+        href="{{ asset('public/wp-content/plugins/slide-anything/owl-carousel/owl.carousel.min.css?ver=2.2.1.1') }}"
+        onload="this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="owl_theme_css-css"
+        href="{{ asset('public/wp-content/plugins/slide-anything/owl-carousel/sa-owl-theme.css?ver=2.0') }}"
+        onload="this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="owl_animate_css-css"
+        href="{{ asset('public/wp-content/plugins/slide-anything/owl-carousel/animate.min.css?ver=2.0') }}"
+        onload="this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="elementor-post-934-css"
+        href="{{ asset('public/wp-content/uploads/elementor/css/post-934.css?ver=1688647328') }}"
+        onload="this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="elementor-post-p-css"
+        href="{{ asset('public/wp-content/uploads/elementor/css/post-1015.css?ver=1688647329') }}"
+        onload="this.rel='stylesheet'" crossorigin="anonymous">
+    <link rel="preload" as="style" id="elementor-post-941-css"
+        href="{{ asset('public/wp-content/uploads/elementor/css/post-941.css?ver=1688647329') }}"
+        onload="this.rel='stylesheet'" crossorigin="anonymous">
+    @include('partials.test')
     @include('partials.footer-web')
     @include('partials.web-script')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
