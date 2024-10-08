ec0b0a0001e8708cf2c2bd241752aef273ce05e4
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 15:16:12 +0400

    231232

--- resources/views/partials/home-test.blade.php
+++ resources/views/partials/home-test.blade.php
@@ -1048,8 +1048,8 @@ h6 {
 </noscript> -->
 <link rel="preload" as="style"  id="elementor-global-css"
     href="https://wiseadvizor.com/wp-content/uploads/elementor/css/global.css?ver=1699355419"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<!-- <link rel="preload" as="style" id="elementor-post-8-css"
-    href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-8.css?ver=1699364637" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous"> -->
+<link rel="preload" as="style" id="elementor-post-8-css"
+    href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-8.css?ver=1699364637" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
     as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
