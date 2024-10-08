5c8e8fbba1010df7da36dc1c442957d1aad9e771
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 16:48:37 +0400

    343453

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -7,10 +7,10 @@
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
     as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload" as="style"
+<link  rel="preload" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.14.1"
     id="elementor-frontend-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel='preload' as="style" id='elementor-pro-css'
+<link  rel='preload' as="style" id='elementor-pro-css'
     href="https://wiseadvizor.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.8.2"
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel='preload' id='elementor-post-3007-css'
@@ -18,15 +18,6 @@
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" id="elementor-post-32-css"
     href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
-    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload" id="qi-addons-for-elementor-grid-style-css"
-    href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/grid.min.css?ver=6.4.1"
-    as="style" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload" as="style" id="qi-addons-for-elementor-helper-parts-style-css"
-    href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/helper-parts.min.css?ver=6.4.1"
-    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<link rel="preload" as="style" id="qi-addons-for-elementor-style-css"
-    href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/main.min.css?ver=6.4.1"
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="stylesheet" as="style" id="elementor-icons-css"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0">
