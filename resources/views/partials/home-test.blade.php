b69dfb94e0026a90967853ef9d0fb927f3065a24
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 15:27:12 +0400

    324354

--- resources/views/partials/home-test.blade.php
+++ resources/views/partials/home-test.blade.php
@@ -1069,18 +1069,18 @@ h6 {
     <link rel="stylesheet"
         href="https://wiseadvizor.com/wp-content/uploads/theplus-addons/theplus.min.css?ver=1699364637">
 </noscript> -->
-<link rel="preload" as="style" id="twentytwentytwo-style-css"
-    href="https://wiseadvizor.com/wp-content/themes/twentytwentytwo/style.min.css?ver=1.4" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+<!-- <link rel="preload" as="style" id="twentytwentytwo-style-css"
+    href="https://wiseadvizor.com/wp-content/themes/twentytwentytwo/style.min.css?ver=1.4" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous"> -->
 <!-- <link rel="preconnect"
     href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CSail%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&#038;display=block&#038;ver=6.4.1"
     as="font" type="font/woff2" id="google-fonts-1-css" crossorigin> -->
 <link rel="stylesheet"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
     id="elementor-icons-shared-0-css">
-<noscript>
+<!-- <noscript>
     <link rel="stylesheet"
         href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3">
-</noscript>
+</noscript> -->
 <!-- <link rel="preconnect" id="elementor-icons-fa-solid-css"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/solid.min.css?ver=5.15.3" crossorigin="anonymous">
 <link rel="preconnect"  id="elementor-icons-fa-regular-css"
