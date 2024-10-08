30bbe03a4d51a4567be585b876e26ab320f290ff
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 14:44:28 +0400

    4r454

--- resources/views/partials/home-test.blade.php
+++ resources/views/partials/home-test.blade.php
@@ -358,10 +358,8 @@ html :where(.is-position-sticky) {
 <!-- <link rel="preload" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1"
     id="tpgb-fontawesome" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous"> -->
-<!-- <noscript>
     <link rel="stylesheet" id="tpgb-fontawesome-css"
         href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1">
-</noscript> -->
 <!-- <link rel="preconnect"
     href="https://fonts.googleapis.com/css?family=Roboto%3A700%2C500%2C400&#038;subset=latin&#038;&display=block&#038;ver=3.0.2"
     crossorigin="crossorigin"> -->
