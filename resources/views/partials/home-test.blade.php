7a399aba5b6258599d46d129a52a0cb3e5f5c9cb
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 15:15:18 +0400

    3243243

--- resources/views/partials/home-test.blade.php
+++ resources/views/partials/home-test.blade.php
@@ -1046,10 +1046,10 @@ h6 {
     <link rel="stylesheet"
         href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5">
 </noscript> -->
-<link rel="preload" as="style"  id="elementor-global-css"
+<!-- <link rel="preload" as="style"  id="elementor-global-css"
     href="https://wiseadvizor.com/wp-content/uploads/elementor/css/global.css?ver=1699355419"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" as="style" id="elementor-post-8-css"
-    href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-8.css?ver=1699364637" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-8.css?ver=1699364637" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous"> -->
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
     as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
