6f9f37c8c3781455bf37ed9e10d5b9d781f362bf
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-08 15:59:27 +0400

    34534

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,17 +33,6 @@
         as="script" crossorigin="anonymous">
     <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js" as="script"
         crossorigin>
-    <!-- <link rel='stylesheet' id='elementor-post-3007-css'
-        href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.css?ver=1706786161'> -->
-    <!-- <link rel="preload" id="elementor-post-32-css"
-        href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
-        onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous"> -->
-    <!-- <link rel="preload"
-        href="https://wiseadvizor.com/public/wp-content/plugins/elementor/assets/js/frontend-modules.min.js" as="script"
-        crossorigin> -->
-    <!-- <link rel="preload" href="https://wiseadvizor.com/js/main.min.js?ver=6.2.2" as="script" crossorigin> -->
-    <!-- <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js" as="script"
-        crossorigin> -->
     <link rel="preload" href="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" as="script"
         crossorigin>
     <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js">
