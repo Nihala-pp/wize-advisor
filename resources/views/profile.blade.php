4373497b71869d8701492ce93e1f4043d615710b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-09 11:20:34 +0400

    wqewe

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1735,6 +1735,8 @@
     @include('partials.footer-web')
     <script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
         id="hfe-frontend-js-js"></script>
+    <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.14.1') }}"
+        id="elementor-frontend-js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
