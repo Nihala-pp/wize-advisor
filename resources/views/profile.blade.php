e36fc6c87afbebd4d09f1d6e51a960c0e1055d53
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-09 12:17:00 +0400

    ewrer

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1733,144 +1733,138 @@
     @include('partials.test')
 
     @include('partials.footer-web')
-    <script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
+    <!-- <script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
         id="hfe-frontend-js-js"></script>
     <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.14.1') }}"
         id="elementor-frontend-js"></script>
     <script src="{{ asset('public/wp-content/plugins/elementor-pro/assets/js/frontend.min.js?ver=3.5.0') }}"
-        id="elementor-pro-frontend-js"></script>
-    <!-- <script src="{{ asset('public/wp-content/uploads/theplus-addons/theplus.min.js?ver=1688661372') }}"
-        id="theplus-front-js-js"></script>
-    <script src="{{ asset('public/wp-includes/js/jquery/ui/mouse.min.js?ver=1.13.2') }}" id="jquery-ui-mouse-js">
-    </script>
-    <script src="{{ asset('public/wp-includes/js/jquery/ui/slider.min.js?ver=1.13.2') }}" id="jquery-ui-slider-js">
-    </script> -->
+        id="elementor-pro-frontend-js"></script> -->
     <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/webpack.runtime.min.js') }}">
     </script>
     <script async src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend-modules.min.js') }}">
-</script>
-<script id="elementor-frontend-js-before">
-var elementorFrontendConfig = {
-    "environmentMode": {
-        "edit": false,
-        "wpPreview": false,
-        "isScriptDebug": false
-    },
-    "i18n": {
-        "shareOnFacebook": "Share on Facebook",
-        "shareOnTwitter": "Share on Twitter",
-        "pinIt": "Pin it",
-        "download": "Download",
-        "downloadImage": "Download image",
-        "fullscreen": "Fullscreen",
-        "zoom": "Zoom",
-        "share": "Share",
-        "playVideo": "Play Video",
-        "previous": "Previous",
-        "next": "Next",
-        "close": "Close",
-        "a11yCarouselWrapperAriaLabel": "Carousel | Horizontal scrolling: Arrow Left & Right",
-        "a11yCarouselPrevSlideMessage": "Previous slide",
-        "a11yCarouselNextSlideMessage": "Next slide",
-        "a11yCarouselFirstSlideMessage": "This is the first slide",
-        "a11yCarouselLastSlideMessage": "This is the last slide",
-        "a11yCarouselPaginationBulletMessage": "Go to slide"
-    },
-    "is_rtl": false,
-    "breakpoints": {
-        "xs": 0,
-        "sm": 480,
-        "md": 768,
-        "lg": 1025,
-        "xl": 1440,
-        "xxl": 1600
-    },
-    "responsive": {
+    </script>
+    <script id="elementor-frontend-js-before">
+    var elementorFrontendConfig = {
+        "environmentMode": {
+            "edit": false,
+            "wpPreview": false,
+            "isScriptDebug": false
+        },
+        "i18n": {
+            "shareOnFacebook": "Share on Facebook",
+            "shareOnTwitter": "Share on Twitter",
+            "pinIt": "Pin it",
+            "download": "Download",
+            "downloadImage": "Download image",
+            "fullscreen": "Fullscreen",
+            "zoom": "Zoom",
+            "share": "Share",
+            "playVideo": "Play Video",
+            "previous": "Previous",
+            "next": "Next",
+            "close": "Close",
+            "a11yCarouselWrapperAriaLabel": "Carousel | Horizontal scrolling: Arrow Left & Right",
+            "a11yCarouselPrevSlideMessage": "Previous slide",
+            "a11yCarouselNextSlideMessage": "Next slide",
+            "a11yCarouselFirstSlideMessage": "This is the first slide",
+            "a11yCarouselLastSlideMessage": "This is the last slide",
+            "a11yCarouselPaginationBulletMessage": "Go to slide"
+        },
+        "is_rtl": false,
         "breakpoints": {
-            "mobile": {
-                "label": "Mobile Portrait",
-                "value": 767,
-                "default_value": 767,
-                "direction": "max",
-                "is_enabled": true
-            },
-            "mobile_extra": {
-                "label": "Mobile Landscape",
-                "value": 880,
-                "default_value": 880,
-                "direction": "max",
-                "is_enabled": false
-            },
-            "tablet": {
-                "label": "Tablet Portrait",
-                "value": 1024,
-                "default_value": 1024,
-                "direction": "max",
-                "is_enabled": true
-            },
-            "tablet_extra": {
-                "label": "Tablet Landscape",
-                "value": 1200,
-                "default_value": 1200,
-                "direction": "max",
-                "is_enabled": false
-            },
-            "laptop": {
-                "label": "Laptop",
-                "value": 1366,
-                "default_value": 1366,
-                "direction": "max",
-                "is_enabled": false
-            },
-            "widescreen": {
-                "label": "Widescreen",
-                "value": 2400,
-                "default_value": 2400,
-                "direction": "min",
-                "is_enabled": false
+            "xs": 0,
+            "sm": 480,
+            "md": 768,
+            "lg": 1025,
+            "xl": 1440,
+            "xxl": 1600
+        },
+        "responsive": {
+            "breakpoints": {
+                "mobile": {
+                    "label": "Mobile Portrait",
+                    "value": 767,
+                    "default_value": 767,
+                    "direction": "max",
+                    "is_enabled": true
+                },
+                "mobile_extra": {
+                    "label": "Mobile Landscape",
+                    "value": 880,
+                    "default_value": 880,
+                    "direction": "max",
+                    "is_enabled": false
+                },
+                "tablet": {
+                    "label": "Tablet Portrait",
+                    "value": 1024,
+                    "default_value": 1024,
+                    "direction": "max",
+                    "is_enabled": true
+                },
+                "tablet_extra": {
+                    "label": "Tablet Landscape",
+                    "value": 1200,
+                    "default_value": 1200,
+                    "direction": "max",
+                    "is_enabled": false
+                },
+                "laptop": {
+                    "label": "Laptop",
+                    "value": 1366,
+                    "default_value": 1366,
+                    "direction": "max",
+                    "is_enabled": false
+                },
+                "widescreen": {
+                    "label": "Widescreen",
+                    "value": 2400,
+                    "default_value": 2400,
+                    "direction": "min",
+                    "is_enabled": false
+                }
             }
+        },
+        "version": "3.14.1",
+        "is_static": false,
+        "experimentalFeatures": {
+            "e_dom_optimization": true,
+            "e_optimized_assets_loading": true,
+            "e_optimized_css_loading": true,
+            "a11y_improvements": true,
+            "additional_custom_breakpoints": true,
+            "e_swiper_latest": true,
+            "theme_builder_v2": true,
+            "landing-pages": true,
+            "form-submissions": true,
+            "e_scroll_snap": true
+        },
+        "urls": {
+            "assets": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor\/assets\/"
+        },
+        "swiperClass": "swiper",
+        "settings": {
+            "page": [],
+            "editorPreferences": []
+        },
+        "kit": {
+            "active_breakpoints": ["viewport_mobile", "viewport_tablet"],
+            "global_image_lightbox": "yes",
+            "lightbox_enable_counter": "yes",
+            "lightbox_enable_fullscreen": "yes",
+            "lightbox_enable_zoom": "yes",
+            "lightbox_enable_share": "yes",
+            "lightbox_title_src": "title",
+            "lightbox_description_src": "description"
+        },
+        "post": {
+            "id": 8,
+            "title": "Home%20%E2%80%93%20WISE%20ADVIZOR",
+            "excerpt": "",
+            "featuredImage": false
         }
-    },
-    "version": "3.14.1",
-    "is_static": false,
-    "experimentalFeatures": {
-        "e_dom_optimization": true,
-        "e_optimized_assets_loading": true,
-        "e_optimized_css_loading": true,
-        "a11y_improvements": true,
-        "additional_custom_breakpoints": true,
-        "e_swiper_latest": true,
-        "theme_builder_v2": true,
-        "landing-pages": true,
-        "form-submissions": true,
-        "e_scroll_snap": true
-    },
-    "urls": {
-        "assets": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor\/assets\/"
-    },
-    "swiperClass": "swiper",
-    "settings": {
-        "page": [],
-        "editorPreferences": []
-    },
-    "kit": {
-        "active_breakpoints": ["viewport_mobile", "viewport_tablet"],
-        "global_image_lightbox": "yes",
-        "lightbox_enable_counter": "yes",
-        "lightbox_enable_fullscreen": "yes",
-        "lightbox_enable_zoom": "yes",
-        "lightbox_enable_share": "yes",
-        "lightbox_title_src": "title",
-        "lightbox_description_src": "description"
-    },
-    "post": {
-        "id": 8,
-        "title": "Home%20%E2%80%93%20WISE%20ADVIZOR",
-        "excerpt": "",
-        "featuredImage": false
-    }
-};
-</script>
+    };
+    </script>
     <script
         src="{{ asset('public/wp-content/plugins/wpforms-lite/assets/js/integrations/elementor/frontend.min.js?ver=1.8.2.2') }}"
         id="wpforms-elementor-js"></script>
@@ -1880,7 +1874,6 @@ var elementorFrontendConfig = {
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
     </script>
 </body>
-
 </html>
 <style>
 .elementor-32 .elementor-element.elementor-element-60f0ea5:not(.elementorw-motion-effects-element-type-background),
