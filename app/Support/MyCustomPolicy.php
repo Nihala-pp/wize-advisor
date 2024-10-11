c2e9660fac7744bf87d696cbdb502d73ea8100d0
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-11 16:07:36 +0400

    3w4e343

--- app/Support/MyCustomPolicy.php
+++ app/Support/MyCustomPolicy.php
@@ -12,5 +12,9 @@ class MyCustomPolicy extends Basic
         parent::configure();
  
         $this->addDirective(Directive::SCRIPT, 'https://wiseadvizor.com/');
+        $this->addDirective(Directive::SCRIPT, 'self');
+        $this->addDirective(Directive::STYLE, 'self');
+        $this->addNonceForDirective(Directive::SCRIPT);
+        $this->addNonceForDirective(Directive::STYLE);
     }
 }
