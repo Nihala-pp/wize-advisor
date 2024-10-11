cc6ed73ebe117475520e69890e01930e726a842e
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-11 17:42:51 +0400

    324e32432

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -1,39 +1,39 @@
 <link rel="preload" as="style" id="hfe-style-css"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor.min.css?ver=1.6.14"
-    onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin>
+    onload="this.onload=null;this.rel='stylesheet'"  crossorigin>
 <link rel="preload" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
-    id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
-    as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link  rel="preload" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.14.1"
-    id="elementor-frontend-css" onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    id="elementor-frontend-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link  rel='preload' as="style" id='elementor-pro-css'
     href="https://wiseadvizor.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.8.2"
-    onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel='preload' id='elementor-post-3007-css'
     href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.css?ver=1706786161' as="style"
-    onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" id="elementor-post-32-css"
     href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
-    onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="stylesheet" as="style" id="elementor-icons-css"
-    href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0" nonce="{{ csp_nonce() }}">
+    href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0">
 <link rel="stylesheet" as="style" id="elementor-icons-shared-0-css"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
-    onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" as="style" id="elementor-post-5-css"
     href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-5.css?ver=1699355418"
-    onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
-    id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload"
     href="https://wiseadvizor.com/wp-content/plugins/header-footer-elementor/inc/widgets-css/frontend.min.css?ver=1.6.14"
-    as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" nonce="{{ csp_nonce() }}" crossorigin="anonymous">
+    as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" 
-    href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1" nonce="{{ csp_nonce() }}" as="style" onload="this.onload=null;this.rel='stylesheet'"
+    href="https://wiseadvizor.com/wp-content/plugins/the-plus-addons-for-block-editor/assets/css/extra/fontawesome.min.css?ver=6.4.1" as="style" onload="this.onload=null;this.rel='stylesheet'"
     id="tpgb-fontawesome">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" nonce="{{ csp_nonce() }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css">
