30e6b70224e39704d3153131e93b16e15fc4b49b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-14 12:59:11 +0400

    qwewqe

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1873,7 +1873,7 @@
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-    <script nonce="{{ csp_nonce() }}" async type="text/javascript"
+    <script nonce="{{ csp_nonce() }}" defer type="text/javascript"
         src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
     </script>
 </body>
