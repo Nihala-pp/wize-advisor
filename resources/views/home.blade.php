3949ffd19a456572210650d6e50abdc41be59826
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-16 10:50:20 +0400

    wreretre

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2029,7 +2029,7 @@
     }
 
     .elementor-8 .elementor-element.elementor-element-e08eaf8 .elementor-heading-title {
-        color: #001E64;
+        color: #330252;
         font-family: "Helvetica", Sans-serif;
         font-weight: 600;
         font-size: 40px;
