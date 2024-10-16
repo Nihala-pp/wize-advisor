515574954041a7c8aba424774b22686820c0043e
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-16 13:50:44 +0400

    wqeqew

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -651,7 +651,7 @@
 
     @include('partials.test-script')
 
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    <script async src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
