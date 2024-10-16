1838997ee97d3163eda6e13c58097c9440021994
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-16 15:47:45 +0400

    qwqeq

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -345,9 +345,9 @@
                                                     <h5><strong><img loading="lazy" decoding="async"
                                                                 class="alignleft wp-image-5531"
                                                                 src="{{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }}"
-                                                                alt="expert mentorship" width="221" height="221"
+                                                                alt="expert mentorship" width="241" height="300"
                                                                 srcset="{{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 150w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 300w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 450w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 700w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 650w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 1300w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 352w"
-                                                                sizes="(max-width: 221px) 100vw, 221px" /></strong><span
+                                                                sizes="(max-width: 241px) 100vw, 241px" /></strong><span
                                                             class="mentor_name"
                                                             style="color: #000000">{{ $mentor->name }}</span></h5>
                                                     <p><span style="font-size:14px;color: #000000">{{ $mentor->metaData->designation }},
@@ -494,9 +494,9 @@
                                                             <h5><strong><img loading="lazy" decoding="async"
                                                                         class="alignleft wp-image-5531"
                                                                         src="{{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }}"
-                                                                        alt="Business Strategy" width="221" height="221"
+                                                                        alt="Business Strategy" width="241" height="300"
                                                                         srcset="{{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 150w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 300w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 450w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 700w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 650w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 1300w, {{ asset('wp-content/uploads/2024/02/') }}/{{ $mentor->metaData ? $mentor->metaData->pro_pic_1 : '' }} 352w"
-                                                                        sizes="(max-width: 221px) 100vw, 221px" /></strong>
+                                                                        sizes="(max-width: 241px) 100vw, 241px" /></strong>
                                                             </h5>
                                                         </div>
 
