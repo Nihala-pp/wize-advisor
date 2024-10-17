a6cee2e89e1cfc839d4a30cc5da53b7cc8d16fdd
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-17 11:29:59 +0400

    wqeqwe

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -643,7 +643,7 @@
         crossorigin="anonymous">
     <link async rel="preconnect" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         crossorigin="anonymous">
-    <link rel="stylesheet"
+    <link async rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
     <link rel="stylesheet" id="elementor-post-2772-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-2772.min.css?ver=1699364619" media="all">
