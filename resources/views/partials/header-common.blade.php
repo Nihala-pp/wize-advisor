107c8fd321809f468ae3e3d34f23255a455e7314
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-17 15:12:44 +0400

    wewre

--- resources/views/partials/header-common.blade.php
+++ resources/views/partials/header-common.blade.php
@@ -34,10 +34,10 @@
                                 }
                                 </style> <a href="https://wiseadvizor.com">
                                     <img fetchpriority="high" width="932" height="140"
-                                        src="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.png"
+                                        src="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.webp"
                                         class="attachment-large size-large wp-image-624"
                                         alt="startup mentorship platform"
-                                        srcset="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.png 932w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-300x45.png 300w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-768x115.png 768w"
+                                        srcset="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.webp 932w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-300x45.webp 300w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-768x115.webp 768w"
                                         sizes="(max-width: 932px) 100vw, 932px"> </a>
                             </div>
                         </div>
@@ -521,10 +521,10 @@
                                         }
                                         </style> <a href="https://wiseadvizor.com">
                                             <img fetchpriority="high" width="932" height="140"
-                                                src="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.png"
+                                                src="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.webp"
                                                 class="attachment-large size-large wp-image-624"
                                                 alt="startup mentorship platform"
-                                                srcset="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.png 932w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-300x45.png 300w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-768x115.png 768w"
+                                                srcset="https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC.webp 932w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-300x45.webp 300w, https://wiseadvizor.com/wp-content/uploads/2023/06/pngLargeC-768x115.webp 768w"
                                                 sizes="(max-width: 932px) 100vw, 932px"> </a>
                                     </div>
                                 </div>
