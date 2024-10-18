6e03668d18dabb5f1cb58547f5fb6f5c2a587b71
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-18 12:41:35 +0400

    ereret

--- resources/views/browsers.blade.php
+++ resources/views/browsers.blade.php
@@ -640,7 +640,7 @@
             </div>
         </section>
     </div>
-    <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"
+    <link async rel="dns-prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"
         crossorigin="anonymous">
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         crossorigin="anonymous">
