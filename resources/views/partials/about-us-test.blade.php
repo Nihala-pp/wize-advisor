400ac0adadc1f9f13ca6207b405aeb7833e26271
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-22 11:39:23 +0400

    34353

--- resources/views/partials/about-us-test.blade.php
+++ resources/views/partials/about-us-test.blade.php
@@ -66,10 +66,6 @@
 <link rel="preload"  as="style"
     href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5"
     id="she-header-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
-<noscript>
-    <link rel="stylesheet"
-        href="https://wiseadvizor.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.min.css?ver=1.6.5">
-</noscript>
 <link rel="preload" as="style"  id="elementor-global-css"
     href="https://wiseadvizor.com/wp-content/uploads/elementor/css/global.css?ver=1699355419"  onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" as="style" id="elementor-post-8-css"
@@ -91,7 +87,7 @@
     <link rel="stylesheet"
         href="https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/main.min.css?ver=6.4.1">
 </noscript> -->
-<link rel="preload" href="https://wiseadvizor.com/wp-content/uploads/theplus-addons/theplus.min.css?ver=1699364637"
+<!-- <link rel="preload" href="https://wiseadvizor.com/wp-content/uploads/theplus-addons/theplus.min.css?ver=1699364637"
     as="style" id="theplus-front-css-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <noscript>
     <link rel="stylesheet"
@@ -101,7 +97,7 @@
     href="https://wiseadvizor.com/wp-content/themes/twentytwentytwo/style.min.css?ver=1.4" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preconnect"
     href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CSail%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&#038;display=block&#038;ver=6.4.1"
-    as="font" type="font/woff2" id="google-fonts-1-css" crossorigin>
+    as="font" type="font/woff2" id="google-fonts-1-css" crossorigin> -->
 <link rel="stylesheet"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css?ver=5.15.3"
     id="elementor-icons-shared-0-css">
