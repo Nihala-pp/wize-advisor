294fd0a3fad279ea76f9396d90e4bb5cf132f6fd
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-23 10:54:53 +0400

    ewqewrew

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -596,7 +596,7 @@
             </div>
         </section>
     </div>
-    <link async rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
