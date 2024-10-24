c1d88a0fda764786f9937bb0083483d803e6d4af
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-24 11:11:54 +0400

    dwew

--- resources/views/partials/test-script.blade.php
+++ resources/views/partials/test-script.blade.php
@@ -129,7 +129,7 @@ var elementorFrontendConfig = {
 </script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.14.1') }}"
     id="elementor-frontend-js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
+<script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js">
 </script>
 <script defer src="{{ asset('public/assets/js/lazysizes.min.js') }}"></script>
 <!--Start of Tawk.to Script-->
