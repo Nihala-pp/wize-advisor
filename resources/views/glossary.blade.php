fc1d01e60708f2dab6aee2eff06840d65910b113
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-24 16:59:01 +0400

    454354

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -149,7 +149,7 @@
 </head>
 
 <body
-    class="page-template page-template-elementor_header_footer page page-id-5666 logged-in admin-bar no-customize-support wp-embed-responsive ehf-footer ehf-template-twentytwentyone ehf-stylesheet-twentytwentyone qodef-qi--no-touch qi-addons-for-elementor-1.7.1 is-light-theme no-js singular elementor-default elementor-template-full-width elementor-kit-5 elementor-page elementor-page-5666">
+    class="page-template page-template-elementor_canvas page page-id-5666 wp-embed-responsive ehf-footer ehf-template-twentytwentyone ehf-stylesheet-twentytwentyone qodef-qi--no-touch qi-addons-for-elementor-1.7.1 is-light-theme no-js singular elementor-default elementor-template-full-width elementor-kit-5 elementor-page elementor-page-5666">
     @include('partials.header-common')
     <div data-elementor-type="wp-page" data-elementor-id="5666" class="elementor elementor-5666">
         <section
