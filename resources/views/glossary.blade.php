5f3b96affcffabf56cb2a997c2537d871250af1b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-24 18:16:19 +0400

    erete

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -31,9 +31,9 @@
         media='all' />
     <!-- <link rel='stylesheet' id='elementor-post-5666-css'
         href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-5666.css?ver=1729768001' media='all' /> -->
-    <link rel='stylesheet' id='qi-addons-for-elementor-grid-style-css'
+    <!-- <link rel='stylesheet' id='qi-addons-for-elementor-grid-style-css'
         href='https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/grid.min.css?ver=1.7.1'
-        media='all' />
+        media='all' /> -->
     <link rel='stylesheet' id='qi-addons-for-elementor-style-css'
         href='https://wiseadvizor.com/wp-content/plugins/qi-addons-for-elementor/assets/css/main.min.css?ver=1.7.1'
         media='all' />
