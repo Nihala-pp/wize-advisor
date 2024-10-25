e324b088455ea2f7d12466a244fb1b83e0826d41
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-25 17:26:41 +0400

    qweqwew

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -84,7 +84,7 @@
                                 <h6 class="elementor-heading-title elementor-size-default">
                                     <a href="https://wiseadvizor.com/">Home</a> > <a
                                         href="{{ route('glossary') }}">Glossary</a> > <a
-                                        href="{{ route('glossary-term', [$data->terms]) }}">Glossary</a>
+                                        href="{{ route('glossary-term', [$data->terms]) }}">{{ $data->terms }}</a>
                                 </h6>
                             </div>
                         </div>
