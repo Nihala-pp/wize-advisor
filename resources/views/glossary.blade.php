2da72e1d05b64e002b3864fee00a1cb575eb7cab
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-25 13:50:02 +0400

    wewqe

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -120,7 +120,7 @@
             </div>
         </section>
         @foreach($glossaries as $glossary)
-        <section
+        <section id="{{ $glossary->letter }}"
             class="elementor-section elementor-top-section elementor-element elementor-element-b8034db elementor-section-boxed elementor-section-height-default elementor-section-height-default wpr-particle-no wpr-jarallax-no wpr-parallax-no wpr-sticky-section-no"
             data-id="b8034db" data-element_type="section"
             data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
