6dd33c6d60efc3aef22d81ea017e8bf253e9ae0a
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-25 18:27:01 +0400

    ewqeq

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -424,7 +424,7 @@
 }
 
 .elementor-4349 .elementor-element.elementor-element-4af5308>.elementor-widget-container {
-    margin-right: -980px !important;
+    margin-right: -960px !important;
     /* margin: 0; */
     padding: 0;
 }
