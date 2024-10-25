4ffa8f8964a1ab63aadcee5c218460e4e486c597
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-25 18:53:48 +0400

    wewerw

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -423,7 +423,7 @@
 
 .elementor-4349 .elementor-element.elementor-element-1abaa4f>.elementor-widget-container {
     text-align: center !important;
-    padding: 10px 0 0;
+    padding: 0px 0 0;
 }
 
 .elementor-4349 .elementor-element.elementor-element-4af5308 img {
