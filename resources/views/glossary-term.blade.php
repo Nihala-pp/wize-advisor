7a3740343d85eeea53cf4ab23508ece241be81ea
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-28 13:48:56 +0400

    23242

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -33,7 +33,7 @@
 </head>
 
 <body
-    class="page-template page-template-elementor_canvas page page-id-4349 page-child parent-pageid-18 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-4349">
+    class="page-template page-template-elementor_canvas page page-id-4349 page-child parent-pageid-18 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-4349" data-spy="scroll" data-target="#toc">
     @include('partials.header-common')
     <div data-elementor-type="wp-page" data-elementor-id="4349" class="elementor elementor-4349">
         <section
