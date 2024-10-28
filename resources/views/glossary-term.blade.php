561e7a49eeb60b54f134d8c210ac3e629a7c8a3b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-28 17:42:39 +0400

    werewr

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -210,7 +210,7 @@
                                                 <div
                                                     class="qodef-shortcode qodef-m  qodef-qi-table-of-contents qodef-list-position--inside qodef-list-underline ">
                                                     <div class="qodef-e-title-holder">
-                                                        <h4 class="qodef-m-title qodef-exclude" style="padding-left: 15px;">
+                                                        <h4 class="qodef-m-title qodef-exclude" style="padding-left: 15px !important;">
                                                             Table of Content </h4>
                                                     </div>
                                                     <nav id="toc" data-toggle="toc"></nav>
