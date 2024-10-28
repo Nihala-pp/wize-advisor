4f73c6b2c9613d85f3441a948e31f45c1aa18f82
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-28 18:45:01 +0400

    wewqre

--- resources/views/admin/glossary.blade.php
+++ resources/views/admin/glossary.blade.php
@@ -241,13 +241,13 @@
                                     enctype="multipart/form-data">
                                     @csrf
                                     <div class="input-group input-group-dynamic is-filled">
-                                        <label for="exampleFormControlInput1" class="form-label">Name</label>
-                                        <input class="multisteps-form__input form-control" type="text" name="expertise"
+                                        <label for="exampleFormControlInput1" class="form-label">Letter</label>
+                                        <input class="multisteps-form__input form-control" type="text" name="letter"
                                             onfocus="focused(this)" onfocusout="defocused(this)" required>
                                     </div>
                                     <div class="input-group input-group-static my-3">
-                                        <label class="form-label"></label>
-                                        <input type="file" name="profile_pic" class="form-control" required>
+                                    <label for="exampleFormControlInput1" class="form-label">Terms</label>
+                                    <input type="file" name="terms" class="form-control" required>
                                     </div>
                                     <button type="submit" name="submitform" id="submitform" class="btn btn-primary"><i
                                             class="fa-solid mx-1 fa-floppy-disk"></i>{{ __('Save') }}</button>
