47922174ffcd2ae5cd37157d0403b61c0543f60b
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-29 10:45:37 +0400

    ewre

--- resources/views/admin/glossary.blade.php
+++ resources/views/admin/glossary.blade.php
@@ -164,12 +164,6 @@
                                         + New Content
                                     </button>
                                 </div>
-                                <!-- <div class="col-6 text-end">
-                                    <button type="button" class="btn bg-gradient-primary" data-bs-toggle="modal"
-                                        data-bs-target="#exampleModal">
-                                        + New Content
-                                    </button>
-                                </div> -->
                             </div>
                         </div>
                         <div class="card-body px-0 pb-2">
