31e0e8f40212f10cb88f96c633297e78ab2eca9e
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-29 19:11:06 +0400

    aewq

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -444,7 +444,7 @@
                                             data-id="9f5ff1a" data-element_type="widget"
                                             data-widget_type="text-editor.default">
                                             <div class="elementor-widget-container">
-                                                <p>{{ Str::of(strip_tags($related_mentor->metaData->bio_1))->words(15, ' ....') }}
+                                                <p>{{ Str::of(strip_tags($related_mentor->metaData->bio_1))->words(12, ' ....') }}
                                                 </p>
                                             </div>
                                         </div>
