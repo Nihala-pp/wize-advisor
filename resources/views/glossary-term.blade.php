99f8e8804d9388facde3d84fb7362da752fe022e
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-29 18:20:48 +0400

    qwqe

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -126,10 +126,10 @@
                                                     display: inline-block
                                                 }
                                                 </style> <img decoding="async" width="150" height="150"
-                                                    src="{{ asset('wp-content/uploads/2024/05/BluePNG.jpg') }}"
+                                                    src="{{ asset('wp-content/uploads/2024/06/Ankur.jpg') }}"
                                                     class="attachment-thumbnail size-thumbnail wp-image-5883"
                                                     alt="startup success"
-                                                    srcset="{{ asset('wp-content/uploads/2024/05/BluePNG.jpg') }} 150w, {{ asset('wp-content/uploads/2024/05/BluePNG.jpg') }} 700w, {{ asset('wp-content/uploads/2024/05/BluePNG.jpg') }} 650w, {{ asset('wp-content/uploads/2024/05/BluePNG.jpg') }} 1300w, {{ asset('wp-content/uploads/2024/05/BluePNG.jpg') }} 98w"
+                                                    srcset="{{ asset('wp-content/uploads/2024/06/Ankur.jpg') }} 150w, {{ asset('wp-content/uploads/2024/06/Ankur.jpg') }} 700w, {{ asset('wp-content/uploads/2024/06/Ankur.jpg') }} 650w, {{ asset('wp-content/uploads/2024/06/Ankur.jpg') }} 1300w, {{ asset('wp-content/uploads/2024/06/Ankur.jpg') }} 98w"
                                                     sizes="(max-width: 150px) 100vw, 150px"
                                                     style="width:100%;height:100%;max-width:40px" />
                                             </div>
