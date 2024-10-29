3c1fb233aeda285d5e1609f444f6edf00ab54f62
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-29 14:27:28 +0400

    erfre

--- resources/views/admin/glossary.blade.php
+++ resources/views/admin/glossary.blade.php
@@ -249,7 +249,7 @@
                                     </div>
                                     <div>
                                         <label class="form-label">Terms (Type and Press Enter)</label>
-                                        <input name="terms[]" class="form-control" id="terms-tags" data-color="dark"
+                                        <input name="glossary[0][terms]" class="form-control" id="terms-tags" data-color="dark"
                                             type="text" required>
                                     </div>
                                     <button type="submit" name="submitform" id="submitform" class="btn btn-primary"><i
