6ec8ee437a950a9f5280987d792410b10b6a8070
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-29 12:49:10 +0400

    erewr

--- resources/views/admin/glossary.blade.php
+++ resources/views/admin/glossary.blade.php
@@ -360,11 +360,10 @@
                 },
                 success: function(response) {
                     response = JSON.parse(response);
-                    response['terms'].forEach(function(value, key) {
-                        response.forEach()
+                    response.forEach(function(value, key) {
                         $(".terms").append(
-                            '<option style="color:black;" value="' + value +
-                            '">' + value + '</option>');
+                            '<option style="color:black;" value="' + value['terms'] +
+                            '">' +  value['terms'] + '</option>');
                     });
                 }
             });
