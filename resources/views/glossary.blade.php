e46f48c4881b94b3b2bf4489974052135471991e
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-30 13:24:13 +0400

    wqewrew

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -316,7 +316,7 @@
 @media (max-width: 767px) {
 
    .elementor-column .elementor-col-25 .elementor-top-column .elementor-element .elementor-element-53afbe6 {
-        width:50%;
+        width:50% !important;
     }
 
     .elementor-5666 .elementor-element.elementor-element-6a08fa2>.elementor-widget-container {
