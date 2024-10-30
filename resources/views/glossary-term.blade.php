2d4424ff8adbe6046ce713cf5817b6bed28e1af6
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-30 17:28:09 +0400

    ewqe

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -5,14 +5,11 @@
     <meta charset="UTF-8">
     <title>{{ $data->terms  }}.</title>
     <meta name="description" content="{{ $data->meta_desc }}">
-    <meta name="keywords" content="
-    @php
-      $keywords = json_decode($data->keywords);
-    @endphp
+    <meta name="keywords" content="@php $keywords = json_decode($data->keywords); @endphp
     @foreach ($keywords as $keyword)
       {{ $keyword }}
     @endforeach
-    ">
+">
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Book 1:1 call with top startup mentors on wiseAdvizor">
