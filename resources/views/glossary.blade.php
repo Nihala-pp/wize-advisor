7c882d008c72f1aa58a6265a912df26b3d229a21
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-31 13:20:55 +0400

    qwqeew

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -385,6 +385,7 @@
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js">
     </script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script type="text/javascript">
     (function($) {
         $(document).ready(function() {
