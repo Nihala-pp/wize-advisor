37c258b332385e1642f8a090ebe598f4d5a60d05
nihala-pp <nihala-pp@pransainvestment.com>
2024-10-31 12:53:08 +0400

    qweq

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -405,7 +405,7 @@
                 // alert(text);
             });
 
-            $("#searchTerm").keyup(function() {
+            $("#searchTerm").click(function() {
                 var txt = $(this).val();
                 alert(txt);
                 var resultDropdown = $(".result");
