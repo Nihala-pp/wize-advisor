5e0c0178bf48d41552190a8b0c8ed6da815118aa
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-01 11:13:09 +0400

    fdrtftr

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -435,7 +435,7 @@
             //set input id "id" value 
             // $("#id").val(id);
             //clear search data
-            window.location.href = ("https://wiseadvizor.com/glossary/" + expertise,'_blank');
+            window.location.href = ("https://wiseadvizor.com/glossary/" + expertise);
         });
     });
     </script>
