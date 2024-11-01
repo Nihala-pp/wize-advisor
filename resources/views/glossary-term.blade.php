d74d7cf011d8ec845539b42465f77521aebe74aa
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-01 13:19:00 +0400

    qwqewq

--- resources/views/glossary-term.blade.php
+++ resources/views/glossary-term.blade.php
@@ -447,7 +447,7 @@
                                             data-id="9f5ff1a" data-element_type="widget"
                                             data-widget_type="text-editor.default">
                                             <div class="elementor-widget-container">
-                                                <p>{{ Str::limit($related_mentor->metaData->bio_1, 113, (' ....')) }}
+                                                <p>{{ Str::limit($related_mentor->metaData->bio_1, 103, (' ....')) }}
                                                 </p>
                                             </div>
                                         </div>
