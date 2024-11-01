685b12c34a068c85d7582336a7dac63fd1918308
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-01 17:18:10 +0400

    qwqe

--- resources/views/admin/edit-glossary.blade.php
+++ resources/views/admin/edit-glossary.blade.php
@@ -176,7 +176,7 @@
                                 <div>
                                     <label class="form-label">Terms (Type and Press Enter)</label>
                                     @php
-                                      $terms = json_decode($glossary->terms['terms'], true);
+                                      $terms = json_decode($glossary->terms[0], true);
                                     @endphp
                                     @foreach ($terms as $key => $term )
 
