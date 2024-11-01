3f4f2c9ab118b1c0ddba731b9c146b04fe41bb69
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-01 18:06:36 +0400

    qw231

--- routes/web.php
+++ routes/web.php
@@ -182,8 +182,8 @@ Route::controller(AdminController::class)
         Route::get('/mentors/resources', 'resources')->name('mentors.resources');
         Route::get('/mentors/events', 'events')->name('mentors.events');
         Route::get('/glossary', 'glossary')->name('mentors.glossary');
-        Route::get('/glossary/content_edit/{id?}', 'delete_glossary')->name('mentors.glossary.content.edit');
-        Route::get('/glossary/edit/{id?}', 'edit_glossary')->name('mentors.glossary.edit');
+        Route::get('/glossary/content_edit/{id?}', 'content_edit')->name('mentors.glossary.content.edit');
+        Route::get('/glossary/edit/{id?}', 'edit_glossary')->name(name: 'mentors.glossary.edit');
         Route::post('/glossary/save', 'save_glossary')->name('mentors.glossary.save');
         Route::post('/glossary/content/save', 'save_glossary_content')->name('mentors.glossary.content.save');
         Route::get('/glossary/getTerms', 'getTerms')->name('mentors.glossary.getTerms');
