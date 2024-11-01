54d154a177d77adafb60ae6ed39c9bb46105be0e
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-01 16:12:55 +0400

    asad

--- resources/views/admin/edit-glossary.blde.php
+++ resources/views/admin/edit-glossary.blde.php
@@ -176,7 +176,11 @@
                                 <div>
                                     <label class="form-label">Terms (Type and Press Enter)</label>
                                     <input name="glossary[0][terms]" class="form-control" id="terms-tags"
-                                        data-color="dark" type="text" required>
+                                        data-color="dark" type="text"  required value="
+                                          @foreach($glossary->terms as $terms)
+                                            {{ $terms ?? '' }}
+                                          @endforeach
+                                        ">
                                 </div>
                                 <button type="submit" name="submitform" id="submitform" class="btn btn-primary"><i
                                         class="fa-solid mx-1 fa-floppy-disk"></i>{{ __('Save') }}</button>
