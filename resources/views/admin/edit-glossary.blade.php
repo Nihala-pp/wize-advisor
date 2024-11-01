119a2abd6398e52881ee64857a60207ec12b86ec
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-01 17:12:43 +0400

    q

--- resources/views/admin/edit-glossary.blade.php
+++ resources/views/admin/edit-glossary.blade.php
@@ -175,7 +175,7 @@
                                 </div>
                                 <div>
                                     <label class="form-label">Terms (Type and Press Enter)</label>
-                                    @foreach ( $glossary->terms as $key => $term )
+                                    @foreach (json_decode($glossary->terms) as $key => $term )
 
                                     <input name="glossary[0][terms]" class="form-control" id="terms-tags"
                                         data-color="dark" type="text" required value="{{ $term ?? '' }}">
