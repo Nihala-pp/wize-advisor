a55a217a0e3170720e429d20d6785d63dff0275b
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-07 17:45:22 +0400

    wewq

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -543,7 +543,7 @@
                                                     <a href="{{ route('register', [$token, $id]) }}"
                                                         class="text-primary text-gradient font-weight-bold">
                                                         @if(!empty($name))
-                                                        Register Here!<span class="text">and Schedule call with
+                                                        Register Here! <span class="text">and Schedule call with
                                                             {{ $name }}</span>
                                                         @elseif(!empty($token))
                                                         Create Account <span class="text">and Schedule call with
