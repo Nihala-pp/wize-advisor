4f0a03d5c5d8259cec4dd422a1928c6df18a0677
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-07 15:46:02 +0400

    rwerwe

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -17,7 +17,7 @@
     href='https://wiseadvizor.com/wp-content/uploads/elementor/css/post-3007.min.css?ver=1706786161' as="style"
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="preload" id="elementor-post-32-css"
-    href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.css?ver=1699355419" as="style"
+    href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-32.min.css?ver=1699355419" as="style"
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="stylesheet" id='elementor-icons-css' href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.31.0">
 <link rel="stylesheet" as="style" id="elementor-icons-shared-0-css"
