e00ec022b0e4804286e5953beab760b25e28ed0e
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-14 18:21:28 +0400

    wqewq

--- resources/views/partials/test-script.blade.php
+++ resources/views/partials/test-script.blade.php
@@ -2,6 +2,10 @@
 <script async
     src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/frontend.min.js?ver=1.3.85"
     id="wpr-addons-js-js"></script>
+<script defer src="{{ asset('public/wp-content/uploads/theplus-addons/theplus.min.js?ver=1688661372') }}"
+    id="theplus-front-js-js"></script>
+<script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
+    id="hfe-frontend-js-js"></script>
 <script src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/frontend.min.js?ver=1.3.85"
     id="wpr-addons-js-js"></script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/webpack.runtime.min.js') }}">
