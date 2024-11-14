2672391f39cdc7588fd837f237f327b10b64c8be
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-14 19:27:38 +0400

    qwqe

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1715,7 +1715,7 @@ i.fas.fa-bell.fa-2xl {
         border-width: 1px 1px 1px 1px;
         border-radius: 0px 0px 0px 0px !important;
         padding: 15px 0px 15px 10px;
-        min-height: 43px;
+        min-height: 50px !important;
     }
 
     .elementor-7754 .elementor-element.elementor-element-49e1889:not(.elementor-motion-effects-element-type-background)>.elementor-widget-wrap,
