a02947cedb376fa9f358335815e6aac60f3be9e4
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-14 18:26:16 +0400

    ewqew

--- resources/views/partials/test-script.blade.php
+++ resources/views/partials/test-script.blade.php
@@ -4,6 +4,8 @@
     id="wpr-addons-js-js"></script>
 <script defer src="{{ asset('public/wp-content/uploads/theplus-addons/theplus.min.js?ver=1688661372') }}"
     id="theplus-front-js-js"></script>
+<script src="{{ asset('public/wp-content/plugins/header-footer-elementor/inc/js/frontend.min.js?ver=1.6.14') }}"
+    id="hfe-frontend-js-js"></script>
 <script src="https://wiseadvizor.com/wp-content/plugins/royal-elementor-addons/assets/js/frontend.min.js?ver=1.3.85"
     id="wpr-addons-js-js"></script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/webpack.runtime.min.js') }}">
@@ -11,213 +13,213 @@
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend-modules.min.js') }}">
 </script>
 <script id="elementor-frontend-js-before">
-    var elementorFrontendConfig = {
-        "environmentMode": {
-            "edit": false,
-            "wpPreview": false,
-            "isScriptDebug": false
-        },
-        "i18n": {
-            "shareOnFacebook": "Share on Facebook",
-            "shareOnTwitter": "Share on Twitter",
-            "pinIt": "Pin it",
-            "download": "Download",
-            "downloadImage": "Download image",
-            "fullscreen": "Fullscreen",
-            "zoom": "Zoom",
-            "share": "Share",
-            "playVideo": "Play Video",
-            "previous": "Previous",
-            "next": "Next",
-            "close": "Close",
-            "a11yCarouselWrapperAriaLabel": "Carousel | Horizontal scrolling: Arrow Left & Right",
-            "a11yCarouselPrevSlideMessage": "Previous slide",
-            "a11yCarouselNextSlideMessage": "Next slide",
-            "a11yCarouselFirstSlideMessage": "This is the first slide",
-            "a11yCarouselLastSlideMessage": "This is the last slide",
-            "a11yCarouselPaginationBulletMessage": "Go to slide"
-        },
-        "is_rtl": false,
+var elementorFrontendConfig = {
+    "environmentMode": {
+        "edit": false,
+        "wpPreview": false,
+        "isScriptDebug": false
+    },
+    "i18n": {
+        "shareOnFacebook": "Share on Facebook",
+        "shareOnTwitter": "Share on Twitter",
+        "pinIt": "Pin it",
+        "download": "Download",
+        "downloadImage": "Download image",
+        "fullscreen": "Fullscreen",
+        "zoom": "Zoom",
+        "share": "Share",
+        "playVideo": "Play Video",
+        "previous": "Previous",
+        "next": "Next",
+        "close": "Close",
+        "a11yCarouselWrapperAriaLabel": "Carousel | Horizontal scrolling: Arrow Left & Right",
+        "a11yCarouselPrevSlideMessage": "Previous slide",
+        "a11yCarouselNextSlideMessage": "Next slide",
+        "a11yCarouselFirstSlideMessage": "This is the first slide",
+        "a11yCarouselLastSlideMessage": "This is the last slide",
+        "a11yCarouselPaginationBulletMessage": "Go to slide"
+    },
+    "is_rtl": false,
+    "breakpoints": {
+        "xs": 0,
+        "sm": 480,
+        "md": 768,
+        "lg": 1025,
+        "xl": 1440,
+        "xxl": 1600
+    },
+    "responsive": {
         "breakpoints": {
-            "xs": 0,
-            "sm": 480,
-            "md": 768,
-            "lg": 1025,
-            "xl": 1440,
-            "xxl": 1600
-        },
-        "responsive": {
-            "breakpoints": {
-                "mobile": {
-                    "label": "Mobile Portrait",
-                    "value": 767,
-                    "default_value": 767,
-                    "direction": "max",
-                    "is_enabled": true
-                },
-                "mobile_extra": {
-                    "label": "Mobile Landscape",
-                    "value": 880,
-                    "default_value": 880,
-                    "direction": "max",
-                    "is_enabled": false
-                },
-                "tablet": {
-                    "label": "Tablet Portrait",
-                    "value": 1024,
-                    "default_value": 1024,
-                    "direction": "max",
-                    "is_enabled": true
-                },
-                "tablet_extra": {
-                    "label": "Tablet Landscape",
-                    "value": 1200,
-                    "default_value": 1200,
-                    "direction": "max",
-                    "is_enabled": false
-                },
-                "laptop": {
-                    "label": "Laptop",
-                    "value": 1366,
-                    "default_value": 1366,
-                    "direction": "max",
-                    "is_enabled": false
-                },
-                "widescreen": {
-                    "label": "Widescreen",
-                    "value": 2400,
-                    "default_value": 2400,
-                    "direction": "min",
-                    "is_enabled": false
-                }
+            "mobile": {
+                "label": "Mobile Portrait",
+                "value": 767,
+                "default_value": 767,
+                "direction": "max",
+                "is_enabled": true
+            },
+            "mobile_extra": {
+                "label": "Mobile Landscape",
+                "value": 880,
+                "default_value": 880,
+                "direction": "max",
+                "is_enabled": false
+            },
+            "tablet": {
+                "label": "Tablet Portrait",
+                "value": 1024,
+                "default_value": 1024,
+                "direction": "max",
+                "is_enabled": true
+            },
+            "tablet_extra": {
+                "label": "Tablet Landscape",
+                "value": 1200,
+                "default_value": 1200,
+                "direction": "max",
+                "is_enabled": false
+            },
+            "laptop": {
+                "label": "Laptop",
+                "value": 1366,
+                "default_value": 1366,
+                "direction": "max",
+                "is_enabled": false
+            },
+            "widescreen": {
+                "label": "Widescreen",
+                "value": 2400,
+                "default_value": 2400,
+                "direction": "min",
+                "is_enabled": false
             }
-        },
-        "version": "3.14.1",
-        "is_static": false,
-        "experimentalFeatures": {
-            "e_dom_optimization": true,
-            "e_optimized_assets_loading": true,
-            "e_optimized_css_loading": true,
-            "a11y_improvements": true,
-            "additional_custom_breakpoints": true,
-            "e_swiper_latest": true,
-            "theme_builder_v2": true,
-            "landing-pages": true,
-            "form-submissions": true,
-            "e_scroll_snap": true
-        },
-        "urls": {
-            "assets": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor\/assets\/"
-        },
-        "swiperClass": "swiper",
-        "settings": {
-            "page": [],
-            "editorPreferences": []
-        },
-        "kit": {
-            "active_breakpoints": ["viewport_mobile", "viewport_tablet"],
-            "global_image_lightbox": "yes",
-            "lightbox_enable_counter": "yes",
-            "lightbox_enable_fullscreen": "yes",
-            "lightbox_enable_zoom": "yes",
-            "lightbox_enable_share": "yes",
-            "lightbox_title_src": "title",
-            "lightbox_description_src": "description"
-        },
-        "post": {
-            "id": 8,
-            "title": "Home%20%E2%80%93%20WISE%20ADVIZOR",
-            "excerpt": "",
-            "featuredImage": false
         }
-    };
+    },
+    "version": "3.14.1",
+    "is_static": false,
+    "experimentalFeatures": {
+        "e_dom_optimization": true,
+        "e_optimized_assets_loading": true,
+        "e_optimized_css_loading": true,
+        "a11y_improvements": true,
+        "additional_custom_breakpoints": true,
+        "e_swiper_latest": true,
+        "theme_builder_v2": true,
+        "landing-pages": true,
+        "form-submissions": true,
+        "e_scroll_snap": true
+    },
+    "urls": {
+        "assets": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor\/assets\/"
+    },
+    "swiperClass": "swiper",
+    "settings": {
+        "page": [],
+        "editorPreferences": []
+    },
+    "kit": {
+        "active_breakpoints": ["viewport_mobile", "viewport_tablet"],
+        "global_image_lightbox": "yes",
+        "lightbox_enable_counter": "yes",
+        "lightbox_enable_fullscreen": "yes",
+        "lightbox_enable_zoom": "yes",
+        "lightbox_enable_share": "yes",
+        "lightbox_title_src": "title",
+        "lightbox_description_src": "description"
+    },
+    "post": {
+        "id": 8,
+        "title": "Home%20%E2%80%93%20WISE%20ADVIZOR",
+        "excerpt": "",
+        "featuredImage": false
+    }
+};
 </script>
 <script src="{{ asset('public/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.14.1') }}"
     id="elementor-frontend-js"></script>
 <script id="elementor-pro-frontend-js-before">
-    var ElementorProFrontendConfig = {
-        "ajaxurl": "https:\/\/wiseadvizor.com\/wp-admin\/admin-ajax.php",
-        "nonce": "3a5ad37dc1",
-        "urls": {
-            "assets": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor-pro\/assets\/",
-            "rest": "https:\/\/wiseadvizor.com\/wp-json\/"
+var ElementorProFrontendConfig = {
+    "ajaxurl": "https:\/\/wiseadvizor.com\/wp-admin\/admin-ajax.php",
+    "nonce": "3a5ad37dc1",
+    "urls": {
+        "assets": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor-pro\/assets\/",
+        "rest": "https:\/\/wiseadvizor.com\/wp-json\/"
+    },
+    "i18n": {
+        "toc_no_headings_found": "No headings were found on this page."
+    },
+    "shareButtonsNetworks": {
+        "facebook": {
+            "title": "Facebook",
+            "has_counter": true
         },
-        "i18n": {
-            "toc_no_headings_found": "No headings were found on this page."
+        "twitter": {
+            "title": "Twitter"
         },
-        "shareButtonsNetworks": {
-            "facebook": {
-                "title": "Facebook",
-                "has_counter": true
-            },
-            "twitter": {
-                "title": "Twitter"
-            },
-            "linkedin": {
-                "title": "LinkedIn",
-                "has_counter": true
-            },
-            "pinterest": {
-                "title": "Pinterest",
-                "has_counter": true
-            },
-            "reddit": {
-                "title": "Reddit",
-                "has_counter": true
-            },
-            "vk": {
-                "title": "VK",
-                "has_counter": true
-            },
-            "odnoklassniki": {
-                "title": "OK",
-                "has_counter": true
-            },
-            "tumblr": {
-                "title": "Tumblr"
-            },
-            "digg": {
-                "title": "Digg"
-            },
-            "skype": {
-                "title": "Skype"
-            },
-            "stumbleupon": {
-                "title": "StumbleUpon",
-                "has_counter": true
-            },
-            "mix": {
-                "title": "Mix"
-            },
-            "telegram": {
-                "title": "Telegram"
-            },
-            "pocket": {
-                "title": "Pocket",
-                "has_counter": true
-            },
-            "xing": {
-                "title": "XING",
-                "has_counter": true
-            },
-            "whatsapp": {
-                "title": "WhatsApp"
-            },
-            "email": {
-                "title": "Email"
-            },
-            "print": {
-                "title": "Print"
-            }
+        "linkedin": {
+            "title": "LinkedIn",
+            "has_counter": true
         },
-        "facebook_sdk": {
-            "lang": "en_US",
-            "app_id": ""
+        "pinterest": {
+            "title": "Pinterest",
+            "has_counter": true
         },
-        "lottie": {
-            "defaultAnimationUrl": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor-pro\/modules\/lottie\/assets\/animations\/default.json"
+        "reddit": {
+            "title": "Reddit",
+            "has_counter": true
+        },
+        "vk": {
+            "title": "VK",
+            "has_counter": true
+        },
+        "odnoklassniki": {
+            "title": "OK",
+            "has_counter": true
+        },
+        "tumblr": {
+            "title": "Tumblr"
+        },
+        "digg": {
+            "title": "Digg"
+        },
+        "skype": {
+            "title": "Skype"
+        },
+        "stumbleupon": {
+            "title": "StumbleUpon",
+            "has_counter": true
+        },
+        "mix": {
+            "title": "Mix"
+        },
+        "telegram": {
+            "title": "Telegram"
+        },
+        "pocket": {
+            "title": "Pocket",
+            "has_counter": true
+        },
+        "xing": {
+            "title": "XING",
+            "has_counter": true
+        },
+        "whatsapp": {
+            "title": "WhatsApp"
+        },
+        "email": {
+            "title": "Email"
+        },
+        "print": {
+            "title": "Print"
         }
-    };
+    },
+    "facebook_sdk": {
+        "lang": "en_US",
+        "app_id": ""
+    },
+    "lottie": {
+        "defaultAnimationUrl": "https:\/\/wiseadvizor.com\/wp-content\/plugins\/elementor-pro\/modules\/lottie\/assets\/animations\/default.json"
+    }
+};
 </script>
 <script src="{{ asset('public/wp-content/plugins/elementor-pro/assets/js/frontend.min.js?ver=3.5.0') }}"
     id="elementor-pro-frontend-js"></script>
@@ -233,15 +235,15 @@
 <script defer src="{{ asset('public/assets/js/lazysizes.min.js') }}"></script>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.defer = true;
-        s1.src = 'https://embed.tawk.to/65046147b1aaa13b7a770b04/1hacho0lm';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+(function() {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.defer = true;
+    s1.src = 'https://embed.tawk.to/65046147b1aaa13b7a770b04/1hacho0lm';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
 </script>
