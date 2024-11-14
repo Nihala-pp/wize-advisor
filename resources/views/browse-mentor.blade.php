9533cda7f1a995ac26e07215d6bf5c980acca990
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-14 18:05:31 +0400

    fdgfhyt

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -61,7 +61,6 @@
     <meta name="twitter:url" content="https://wiseadvizor.com/BrowseMentors">
     @endif
 </head>
-
 <body
     class="page-template page-template-elementor_canvas page page-id-2772 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-2772">
 
