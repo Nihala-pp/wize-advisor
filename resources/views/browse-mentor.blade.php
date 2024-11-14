7eddd7c39e649a33f85c435a15b0fac500dd3f42
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-14 18:19:38 +0400

    wqewq

--- resources/views/browse-mentor.blade.php
+++ resources/views/browse-mentor.blade.php
@@ -61,6 +61,7 @@
     <meta name="twitter:url" content="https://wiseadvizor.com/BrowseMentors">
     @endif
 </head>
+
 <body
     class="page-template page-template-elementor_canvas page page-id-2772 wp-embed-responsive ehf-header ehf-footer ehf-template-twentytwentytwo ehf-stylesheet-twentytwentytwo qodef-qi--no-touch qi-addons-for-elementor-1.6.2 elementor-default elementor-template-canvas elementor-kit-5 elementor-page elementor-page-2772">
 
@@ -594,18 +595,19 @@
             </div>
         </section>
     </div>
-    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
     <link rel="stylesheet" id="elementor-post-2772-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/post-2772.min.css?ver=1699364619" media="all">
-    <link  rel="stylesheet" id="twenty-twenty-one-style-css"
+    <link rel="stylesheet" id="twenty-twenty-one-style-css"
         href="https://wiseadvizor.com/wp-content/themes/twentytwentyone/style.min.css?ver=2.0" media="all">
-    @include('partials.test')
-
     <script src="https://wiseadvizor.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js">
     </script>
+
+    @include('partials.test')
+
 
     @include('partials.footer-web')
 
@@ -1785,4 +1787,5 @@ i.fas.fa-bell.fa-2xl {
 
 @media (min-width: 768px) and (max-width: 1024px) {}
 </style>
+
 </html>
