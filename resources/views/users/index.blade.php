8c27e50e75036a75cf1a775e16299fea54175a9e
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-14 19:31:43 +0400

    wewq

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1701,7 +1701,7 @@ i.fas.fa-bell.fa-2xl {
         background-color: #605BE5;
         border-color: #E8E8E8;
         min-width: 42px;
-        min-height: 30px;
+        min-height: 20px;
         border-width: 0px 0px 0px 0px;
         border-radius: 0px 0px 0px 0px;
     }
