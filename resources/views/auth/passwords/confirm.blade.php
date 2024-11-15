9cb998a6587644063d2b64f0e1b5b9f34054cd6a
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-15 15:19:00 +0400

    ww4e34

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -10,7 +10,7 @@
     </div>
   </div> -->
     <main class="main-content  mt-0">
-        <div class="page-header align-items-start min-vh-100" style="background-color: #000;">
+        <div class="page-header align-items-start min-vh-100" style="background-color:#000;">
             <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container my-auto">
                 <div class="row">
