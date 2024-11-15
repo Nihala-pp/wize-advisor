cc8546aa77dd263615a83decab13c2583267b82e
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-15 18:03:45 +0400

    edrde

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -9,7 +9,7 @@
     as="style" id="hfe-widgets-style-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
 <link rel="stylesheet" as="style"
     href="https://wiseadvizor.com/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.14.1"
-    id="elementor-frontend-css" onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
+    id="elementor-frontend-css">
 <link rel='preload' as="style" id='elementor-pro-css'
     href="https://wiseadvizor.com/wp-content/plugins/elementor-pro/assets/css/frontend-lite.min.css?ver=3.8.2"
     onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
