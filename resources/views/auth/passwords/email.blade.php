b8bae41ae41a7888cd4d33b3463bcc6a6297bb3a
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-15 15:46:45 +0400

    wew343

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -46,7 +46,7 @@
                                 </div>
                             </div> -->
                             <div class="card-body" style="background-color: transparent;background-image: linear-gradient(180deg, #171414 0%, #0F071C 100%);">
-                                <h1 class="text-white font-weight-bolder text-center mt-2 mb-0" style="color:#000 !important;">
+                                <h1 class="text-white font-weight-bolder text-center mt-2 mb-0">
                                     {{ __('Recover Your wiseAdvizor Password') }}</h1>
                                 @if (session('status'))
                                 <div class="alert alert-success" role="alert">
