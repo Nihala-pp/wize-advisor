ea5b04f16c19d00e8063bdbf7dc7a49ae17ffe64
nihala-pp <nihala-pp@pransainvestment.com>
2024-11-20 15:08:24 +0400

    trttrytr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,6 +39,7 @@
     <link async rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     @include('partials.test')
+    <link rel="preconnect" href="https://www.googletagmanager.com/gtag/js?id=G-QC47PPTD6T">
     <link rel="preload" as="style" id="elementor-global-css"
         href="https://wiseadvizor.com/wp-content/uploads/elementor/css/global.css?ver=1699355419"
         onload="this.onload=null;this.rel='stylesheet'" crossorigin="anonymous">
